65f0ab29cbd28f626ec814f5b5a013072c2b51ef
Thomas Jacquin <jacquin.thomas@gmail.com>
2016-11-10 02:48:49 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -10,17 +10,16 @@ echo "Sunwait installation"
 cp sunwait /usr/local/bin
 echo -en '\n'
 echo -en '\n'
-apt-get update
-apt-get install "libusb-dev libav-tools gawk lftp entr imagemagik" -y
+apt-get update && apt-get install libusb-dev libav-tools gawk lftp entr imagemagik -y
 echo -en '\n'
 echo -en '\n'
 echo -en 'Using the camera without root access'
-install asi.rules /lib/udev/rules.d
+install lib/asi.rules /lib/udev/rules.d
 echo -en '\n'
 echo -en '\n'
 echo -en 'Copying shared libraries to user library'
-cp /lib/armv7/libASICamera2* /usr/local/lib
+cp lib/armv7/libASICamera2* /usr/local/lib
 echo -en '\n'
 echo -en '\n'
 echo -en 'Autostart script'
-echo "@xterm -hold -e ~/allsky/allsky.sh" >> ~/.config/lxsession/LXDE-pi/autostart
+echo "@xterm -hold -e /home/pi/allsky/allsky.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart

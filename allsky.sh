459cd62e1a3675d465d4823559fef2d2a28d980f
Thomas Jacquin <jacquin.thomas@gmail.com>
2017-02-22 18:36:31 -0800

    Added comments to allsky.sh

--- allsky.sh
+++ allsky.sh
@@ -4,9 +4,12 @@ echo "Starting allsky camera..."
 cd /home/pi/allsky
 
 FILENAME="gibbons.jpg"
+
+# Set upload to false if you don't want to upload the latest image to your website
 UPLOAD=true
 
 # Set -help to 1 for full list of arguments
+# Set -nodisplay to 1 on Raspbian Lite and other OS without Desktop environments
 
 ls "$FILENAME" | entr ./saveImage.sh "$FILENAME" "$UPLOAD" & \
 ./capture \
@@ -14,12 +17,12 @@ ls "$FILENAME" | entr ./saveImage.sh "$FILENAME" "$UPLOAD" & \
   -exposure 	5000000 \
   -filename 	"$FILENAME" \
   -latitude 	"60.7N" \
-  -longitude 	"135.05E" \
+  -longitude 	"135.05W" \
   -wbr	 	53 \
   -wbb	 	80 \
   -textx	20 \
   -fontcolor	255 255 255 \
   -time 	1 \
   -help 	0 \
-  -nodisplay 	1 \
+  -nodisplay 	0 \
   -timelapse 	1 

eddc23a4e9098863f7c332f26302a5be054b3f69
Thomas Jacquin <thomas.jacquin@makeit.com>
2017-11-27 10:33:11 -0800

    Fixed broken timelapse code

--- timelapse.sh
+++ timelapse.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-source /home/pi/allsky/config.sh
+source /home/thomas/dev/allsky/config.sh
 
 if [ "$UPLOAD_VIDEO" = true ] ; then
 	# find images, rename images sequentially and start avconv to build mp4; upload mp4 and move current dir
 	ls -rt images/current |
 	gawk 'BEGIN{ a=1 }{ printf "mv -v ./images/current/%s images/current/%04d.'$EXTENSION'\n", $0, a++ }' |
-	bash &
+	bash
 	avconv -y -f image2 -r 25 -i images/current/%04d.$EXTENSION -vcodec libx264 -b:v 2000k images/current/allsky-$(date -d "yesterday 13:00" '+%Y%m%d').mp4 && \
 	lftp sftp://"$USER":"$PASSWORD"@"$HOST":"$MP4DIR" -e "set net:max-retries 1; put images/current/allsky-`date -d "yesterday 13:00" '+%Y%m%d'`.mp4; bye" && \
 	mv ./images/current images/$(date -d "yesterday 13:00" '+%Y%m%d')
-else 
+else
 	# find images, rename images sequentially and start avconv to build mp4; move current dir
 	ls -rt images/current |
 	gawk 'BEGIN{ a=1 }{ printf "mv -v ./images/current/%s images/current/%04d.'$EXTENSION'\n", $0, a++ }' |

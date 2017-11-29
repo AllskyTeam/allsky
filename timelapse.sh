353b90526f8e6fd9da314760a01b96938856e83e
Thomas Jacquin <jacquin.thomas@gmail.com>
2017-11-29 11:09:09 -0800

    Fixed typo in timelapse.sh

--- timelapse.sh
+++ timelapse.sh
@@ -13,7 +13,7 @@ else
 	# find images, rename images sequentially and start avconv to build mp4; move current dir
 	ls -rt images/current |
 	gawk 'BEGIN{ a=1 }{ printf "mv -v ./images/current/%s images/current/%04d.'$EXTENSION'\n", $0, a++ }' |
-	bash &
+	bash
 	avconv -y -f image2 -r 25 -i images/current/%04d.$EXTENSION -vcodec libx264 -b:v 2000k images/current/allsky-$(date -d "yesterday 13:00" '+%Y%m%d').mp4 && \
 	mv ./images/current images/$(date -d "yesterday 13:00" '+%Y%m%d')
 fi

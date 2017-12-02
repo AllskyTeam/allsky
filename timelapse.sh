25b40912cb9e393bd95817b247d1c27ee777bce6
Thomas Jacquin <jacquin.thomas@gmail.com>
2017-12-02 14:55:28 -0800

    ADDED protocol setting to ftp transfer

--- timelapse.sh
+++ timelapse.sh
@@ -7,7 +7,7 @@ if [ "$UPLOAD_VIDEO" = true ] ; then
 	gawk 'BEGIN{ a=1 }{ printf "mv -v ./images/current/%s images/current/%04d.'$EXTENSION'\n", $0, a++ }' |
 	bash
 	avconv -y -f image2 -r 25 -i images/current/%04d.$EXTENSION -vcodec libx264 -b:v 2000k images/current/allsky-$(date -d "yesterday 13:00" '+%Y%m%d').mp4 && \
-	lftp sftp://"$USER":"$PASSWORD"@"$HOST":"$MP4DIR" -e "set net:max-retries 1; put images/current/allsky-`date -d "yesterday 13:00" '+%Y%m%d'`.mp4; bye" && \
+	lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$MP4DIR" -e "set net:max-retries 1; put images/current/allsky-`date -d "yesterday 13:00" '+%Y%m%d'`.mp4; bye" && \
 	mv ./images/current images/$(date -d "yesterday 13:00" '+%Y%m%d')
 else
 	# find images, rename images sequentially and start avconv to build mp4; move current dir

4807fbd9dbe3feb0ab5d71db4dde2f9b88e47378
Thomas Jacquin <jacquin.thomas@gmail.com>
2018-01-12 15:16:07 -0800

    Added variables in postData.sh

--- postData.sh
+++ postData.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+source /home/pi/allsky/config.sh
+
 echo "Posting Next Twilight Time"
 today=`date +%Y-%m-%d`
 time="$(sunwait list set civil 60.7N 135.02W)"
 timeNoZone=${time:0:5}
 echo {\"sunset\": \"$today"T"$timeNoZone":00.000-0800"\"} > data.json
 echo "Uploading data.json"
-lftp sftp://username:password@server:/location_of_allsky_directory -e "put data.json; bye"
+lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$IMGDIR" -e "set net:max-retries 1; set net:timeout 20; put data.json; bye"

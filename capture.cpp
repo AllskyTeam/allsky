1ddcf7c108da8a68d3fe7ad905337707a90adcfc
Thomas Jacquin <jacquin.thomas@gmail.com>
2018-02-28 20:02:25 -0800

    Updated version number to 0.5

--- capture.cpp
+++ capture.cpp
@@ -153,7 +153,7 @@ int  main(int argc, char* argv[])
 //-------------------------------------------------------------------------------------------------------
   printf("\n");  
   printf("%s ******************************************\n", KGRN);  
-  printf("%s *** Allsky Camera Software v0.4 | 2018 ***\n", KGRN);
+  printf("%s *** Allsky Camera Software v0.5 | 2018 ***\n", KGRN);
   printf("%s ******************************************\n\n", KGRN);
   printf("\%sCapture images of the sky with a Raspberry Pi and an ASI Camera\n",KGRN); 
   printf("\n");  

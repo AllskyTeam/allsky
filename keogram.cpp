eeb617bdeb017cce6280e00838dabeb27a2a56f3
David Bainbridge <dbainbridge@mac.com>
2018-12-22 12:24:40 -0600

    Fix code formatting so that it is using same formatting across all three cpp files. Includes clang-format config that can be used by installing clang-format via sudo apt-get install clang-format.

--- keogram.cpp
+++ keogram.cpp
@@ -6,10 +6,10 @@
 #include <cstdlib>
 #include <glob.h>
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <sys/stat.h>
 #include <vector>
-#include <stdio.h>
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -26,155 +26,129 @@
 //-------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------
 
-int main(int argc, char *argv[])
-{
-    if (argc < 4)
-    {
-        std::cout << KRED << "You need to pass 3 arguments: source directory, "
-                             "image extension, output file"
-                  << std::endl;
-        std::cout << "Optionally you can pass after them: " << std::endl;
-        std::cout << " -no-label                    - Disable hour labels" << std::endl;
-        std::cout << " -fontname = Font Name        - Default = 0    - Font Types "
-                     "(0-7), Ex. 0 = simplex, 4 = triplex, 7 = script"
-                  << std::endl;
-        std::cout << " -fontcolor = Font Color      - Default = 255 0 0  - Text "
-                     "blue (BRG)"
-                  << std::endl;
-        std::cout << " -fonttype = Font Type        - Default = 8    - Font Line "
-                     "Type,(0-2), 0 = AA, 1 = 8, 2 = 4"
-                  << std::endl;
-        std::cout << " -fontsize                    - Default = 2.0  - Text Font Size" << std::endl;
-        std::cout << " -fontline                    - Default = 3    - Text Font "
-                     "Line Thickness"
-                  << std::endl;
-        std::cout << "    ex: keogram ../images/current/ jpg keogram.jpg -fontsize 2" << std::endl;
-        std::cout << "    ex: keogram . png /home/pi/allsky/keogram.jpg -no-label" << KNRM << std::endl;
-        return 3;
+int main(int argc, char *argv[]) {
+  if (argc < 4) {
+    std::cout << KRED << "You need to pass 3 arguments: source directory, "
+                         "image extension, output file"
+              << std::endl;
+    std::cout << "Optionally you can pass after them: " << std::endl;
+    std::cout << " -no-label                    - Disable hour labels" << std::endl;
+    std::cout << " -fontname = Font Name        - Default = 0    - Font Types "
+                 "(0-7), Ex. 0 = simplex, 4 = triplex, 7 = script"
+              << std::endl;
+    std::cout << " -fontcolor = Font Color      - Default = 255 0 0  - Text "
+                 "blue (BRG)"
+              << std::endl;
+    std::cout << " -fonttype = Font Type        - Default = 8    - Font Line "
+                 "Type,(0-2), 0 = AA, 1 = 8, 2 = 4"
+              << std::endl;
+    std::cout << " -fontsize                    - Default = 2.0  - Text Font Size" << std::endl;
+    std::cout << " -fontline                    - Default = 3    - Text Font "
+                 "Line Thickness"
+              << std::endl;
+    std::cout << "    ex: keogram ../images/current/ jpg keogram.jpg -fontsize 2" << std::endl;
+    std::cout << "    ex: keogram . png /home/pi/allsky/keogram.jpg -no-label" << KNRM << std::endl;
+    return 3;
+  }
+
+  std::string directory = argv[1];
+  std::string extension = argv[2];
+  std::string outputfile = argv[3];
+
+  bool labelsEnabled = true;
+  int fontFace = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
+  double fontScale = 2;
+  int fontType = 8;
+  int thickness = 3;
+  char fontColor[3] = {255, 0, 0};
+
+  // Handle optional parameters
+  for (int a = 4; a < argc; ++a) {
+    if (!strcmp(argv[a], "-no-label")) {
+      labelsEnabled = false;
+    } else if (!strcmp(argv[a], "-fontname")) {
+      fontFace = atoi(argv[++a]);
+    } else if (!strcmp(argv[a], "-fonttype")) {
+      fontType = atoi(argv[++a]);
+    } else if (!strcmp(argv[a], "-fontsize")) {
+      fontScale = atof(argv[++a]);
+    } else if (!strcmp(argv[a], "-fontline")) {
+      thickness = atoi(argv[++a]);
+    } else if (!strcmp(argv[a], "-fontcolor")) {
+      fontColor[0] = atoi(argv[++a]);
+      fontColor[1] = atoi(argv[++a]);
+      fontColor[2] = atoi(argv[++a]);
+    }
+  }
+
+  glob_t files;
+  std::string wildcard = directory + "/*." + extension;
+  glob(wildcard.c_str(), 0, NULL, &files);
+
+  cv::Mat accumulated;
+
+  int prevHour = -1;
+
+  for (size_t f = 0; f < files.gl_pathc; f++) {
+    cv::Mat image = cv::imread(files.gl_pathv[f], cv::IMREAD_UNCHANGED);
+    if (!image.data) {
+      std::cout << "Error reading file " << basename(files.gl_pathv[f]) << std::endl;
+      continue;
     }
 
-    std::string directory  = argv[1];
-    std::string extension  = argv[2];
-    std::string outputfile = argv[3];
+    std::cout << "[" << f + 1 << "/" << files.gl_pathc << "] " << basename(files.gl_pathv[f]) << std::endl;
 
-    bool labelsEnabled = true;
-    int fontFace       = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
-    double fontScale   = 2;
-    int fontType       = 8;
-    int thickness      = 3;
-    char fontColor[3]  = { 255, 0, 0 };
-
-    // Handle optional parameters
-    for (int a = 4; a < argc; ++a)
-    {
-        if (!strcmp(argv[a], "-no-label"))
-        {
-            labelsEnabled = false;
-        }
-        else if (!strcmp(argv[a], "-fontname"))
-        {
-            fontFace = atoi(argv[++a]);
-        }
-        else if (!strcmp(argv[a], "-fonttype"))
-        {
-            fontType = atoi(argv[++a]);
-        }
-        else if (!strcmp(argv[a], "-fontsize"))
-        {
-            fontScale = atof(argv[++a]);
-        }
-        else if (!strcmp(argv[a], "-fontline"))
-        {
-            thickness = atoi(argv[++a]);
-        }
-        else if (!strcmp(argv[a], "-fontcolor"))
-        {
-            fontColor[0] = atoi(argv[++a]);
-            fontColor[1] = atoi(argv[++a]);
-            fontColor[2] = atoi(argv[++a]);
-        }
+    // If we don't have image yet, create one using height and format from
+    // the source image and width from number of files
+    if (accumulated.empty()) {
+      accumulated.create(image.rows, files.gl_pathc, image.type());
     }
+    // Copy middle column to destination
+    image.col(image.cols / 2).copyTo(accumulated.col(f));
 
-    glob_t files;
-    std::string wildcard = directory + "/*." + extension;
-    glob(wildcard.c_str(), 0, NULL, &files);
+    if (labelsEnabled) {
+      struct stat s;
+      stat(files.gl_pathv[f], &s);
 
-    cv::Mat accumulated;
-
-    int prevHour = -1;
-
-    for (size_t f = 0; f < files.gl_pathc; f++)
-    {
-        cv::Mat image = cv::imread(files.gl_pathv[f], cv::IMREAD_UNCHANGED);
-        if (!image.data)
-        {
-            std::cout << "Error reading file " << basename(files.gl_pathv[f]) << std::endl;
-            continue;
-        }
-
-        std::cout << "[" << f + 1 << "/" << files.gl_pathc << "] " << basename(files.gl_pathv[f]) << std::endl;
-
-        // If we don't have image yet, create one using height and format from
-        // the source image and width from number of files
-        if (accumulated.empty())
-        {
-            accumulated.create(image.rows, files.gl_pathc, image.type());
-        }
-        // Copy middle column to destination
-        image.col(image.cols / 2).copyTo(accumulated.col(f));
-
-        if (labelsEnabled)
-        {
-            struct stat s;
-            stat(files.gl_pathv[f], &s);
-
-            struct tm *t = localtime(&s.st_mtime);
-            if (t->tm_hour != prevHour)
-            {
-                if (prevHour != -1)
-                {
-                    // Draw a dashed line and label for hour
-                    cv::LineIterator it(accumulated, cv::Point(f, 0), cv::Point(f, accumulated.rows));
-                    for (int i = 0; i < it.count; i++, ++it)
-                    {
-                        // 4 pixel dashed line
-                        if (i & 4)
-                        {
-                            uchar *p = *it;
-                            for (int c = 0; c < it.elemSize; c++)
-                            {
-                                *p = ~(*p);
-                                p++;
-                            }
-                        }
-                    }
-
-                    // Draw text label to the left of the dash
-                    char hour[3];
-                    snprintf(hour, 3, "%02d", t->tm_hour);
-                    std::string text(hour);
-                    int baseline      = 0;
-                    cv::Size textSize = cv::getTextSize(text, fontFace, fontScale, thickness, &baseline);
-
-                    if (f - textSize.width >= 0)
-                    {
-                        cv::putText(accumulated, text,
-                                    cv::Point(f - textSize.width, accumulated.rows - textSize.height), fontFace,
-                                    fontScale, cv::Scalar(fontColor[0], fontColor[1], fontColor[2]), thickness,
-                                    fontType);
-                    }
-                }
-                prevHour = t->tm_hour;
+      struct tm *t = localtime(&s.st_mtime);
+      if (t->tm_hour != prevHour) {
+        if (prevHour != -1) {
+          // Draw a dashed line and label for hour
+          cv::LineIterator it(accumulated, cv::Point(f, 0), cv::Point(f, accumulated.rows));
+          for (int i = 0; i < it.count; i++, ++it) {
+            // 4 pixel dashed line
+            if (i & 4) {
+              uchar *p = *it;
+              for (int c = 0; c < it.elemSize; c++) {
+                *p = ~(*p);
+                p++;
+              }
             }
+          }
+
+          // Draw text label to the left of the dash
+          char hour[3];
+          snprintf(hour, 3, "%02d", t->tm_hour);
+          std::string text(hour);
+          int baseline = 0;
+          cv::Size textSize = cv::getTextSize(text, fontFace, fontScale, thickness, &baseline);
+
+          if (f - textSize.width >= 0) {
+            cv::putText(accumulated, text, cv::Point(f - textSize.width, accumulated.rows - textSize.height), fontFace,
+                        fontScale, cv::Scalar(fontColor[0], fontColor[1], fontColor[2]), thickness, fontType);
+          }
         }
+        prevHour = t->tm_hour;
+      }
     }
-    globfree(&files);
+  }
+  globfree(&files);
 
-    std::vector<int> compression_params;
-    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    compression_params.push_back(9);
-    compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-    compression_params.push_back(95);
+  std::vector<int> compression_params;
+  compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+  compression_params.push_back(9);
+  compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+  compression_params.push_back(95);
 
-    cv::imwrite(outputfile, accumulated, compression_params);
+  cv::imwrite(outputfile, accumulated, compression_params);
 }
--- startrails.cpp
+++ startrails.cpp
@@ -3,15 +3,15 @@
 // Based on script by Thomas Jacquin
 // SPDX-License-Identifier: MIT
 
+#include <algorithm>
 #include <cstdlib>
 #include <glob.h>
-#include <string>
 #include <iostream>
+#include <string>
 #include <vector>
-#include <algorithm>
 
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #define KNRM "\x1B[0m"
 #define KRED "\x1B[31m"
@@ -25,112 +25,101 @@
 //-------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------
 
-int main(int argc, char *argv[])
-{
-    if (argc != 5)
-    {
-        std::cout << KRED
-                  << "You need to pass 4 arguments: source directory, file extension, brightness treshold, output file"
-                  << KNRM << std::endl;
-        std::cout << "    ex: startrails ../images/20180208/ jpg 0.07 startrails.jpg" << std::endl;
-        std::cout << "    brightness ranges from 0 (black) to 1 (white)" << std::endl;
-        std::cout << "    A moonless sky is around 0.05 while full moon can be as high as 0.4" << std::endl;
-        return 3;
-    }
-    std::string directory  = argv[1];
-    std::string extension  = argv[2];
-    double threshold       = atof(argv[3]);
-    std::string outputfile = argv[4];
+int main(int argc, char *argv[]) {
+  if (argc != 5) {
+    std::cout << KRED
+              << "You need to pass 4 arguments: source directory, file extension, brightness treshold, output file"
+              << KNRM << std::endl;
+    std::cout << "    ex: startrails ../images/20180208/ jpg 0.07 startrails.jpg" << std::endl;
+    std::cout << "    brightness ranges from 0 (black) to 1 (white)" << std::endl;
+    std::cout << "    A moonless sky is around 0.05 while full moon can be as high as 0.4" << std::endl;
+    return 3;
+  }
+  std::string directory = argv[1];
+  std::string extension = argv[2];
+  double threshold = atof(argv[3]);
+  std::string outputfile = argv[4];
 
-    // Find files
-    glob_t files;
-    std::string wildcard = directory + "/*." + extension;
-    glob(wildcard.c_str(), 0, NULL, &files);
+  // Find files
+  glob_t files;
+  std::string wildcard = directory + "/*." + extension;
+  glob(wildcard.c_str(), 0, NULL, &files);
 
-    cv::Mat accumulated;
+  cv::Mat accumulated;
 
-    // Create space for statistics
-    cv::Mat stats;
-    stats.create(1, files.gl_pathc, CV_64F);
+  // Create space for statistics
+  cv::Mat stats;
+  stats.create(1, files.gl_pathc, CV_64F);
 
-    for (size_t f = 0; f < files.gl_pathc; f++)
-    {
-        cv::Mat image = cv::imread(files.gl_pathv[f], cv::IMREAD_UNCHANGED);
-        if (!image.data)
-        {
-            std::cout << "Error reading file " << basename(files.gl_pathv[f]) << std::endl;
-            continue;
-        }
-
-        cv::Scalar mean_scalar = cv::mean(image);
-        double mean;
-        switch (image.channels())
-        {
-            default: // mono case
-                mean = mean_scalar.val[0];
-                break;
-            case 3: // for color choose maximum channel
-            case 4:
-                mean = cv::max(mean_scalar[0], cv::max(mean_scalar[1], mean_scalar[2]));
-                break;
-        }
-        // Scale to 0-1 range
-        switch (image.depth())
-        {
-            case CV_8U:
-                mean /= 255.0;
-                break;
-            case CV_16U:
-                mean /= 65535.0;
-                break;
-        }
-        std::cout << "[" << f + 1 << "/" << files.gl_pathc << "] " << basename(files.gl_pathv[f]) << " " << mean
-                  << std::endl;
-
-        stats.col(f) = mean;
-
-        if (mean <= threshold)
-        {
-            if (accumulated.empty())
-            {
-                image.copyTo(accumulated);
-            }
-            else
-            {
-                accumulated = cv::max(accumulated, image);
-            }
-        }
+  for (size_t f = 0; f < files.gl_pathc; f++) {
+    cv::Mat image = cv::imread(files.gl_pathv[f], cv::IMREAD_UNCHANGED);
+    if (!image.data) {
+      std::cout << "Error reading file " << basename(files.gl_pathv[f]) << std::endl;
+      continue;
     }
 
-    // Calculate some statistics
-    double min_mean, max_mean;
-    cv::Point min_loc;
-    cv::minMaxLoc(stats, &min_mean, &max_mean, &min_loc);
-    double mean_mean = cv::mean(stats)[0];
-
-    // For median, do partial sort and take middle value
-    std::vector<double> vec;
-    stats.copyTo(vec);
-    std::nth_element(vec.begin(), vec.begin() + (vec.size() / 2), vec.end());
-    double median_mean = vec[vec.size() / 2];
-
-    std::cout << "Minimum: " << min_mean << " maximum: " << max_mean << " mean: " << mean_mean
-              << " median: " << median_mean << std::endl;
-
-    // If we still don't have an image (no images below threshold), copy the minimum mean image so we see why
-    if (accumulated.empty())
-    {
-        std::cout << "No images below threshold, writing the minimum image only" << std::endl;
-        accumulated = cv::imread(files.gl_pathv[min_loc.x], cv::IMREAD_UNCHANGED);
+    cv::Scalar mean_scalar = cv::mean(image);
+    double mean;
+    switch (image.channels()) {
+    default: // mono case
+      mean = mean_scalar.val[0];
+      break;
+    case 3: // for color choose maximum channel
+    case 4:
+      mean = cv::max(mean_scalar[0], cv::max(mean_scalar[1], mean_scalar[2]));
+      break;
     }
-    globfree(&files);
+    // Scale to 0-1 range
+    switch (image.depth()) {
+    case CV_8U:
+      mean /= 255.0;
+      break;
+    case CV_16U:
+      mean /= 65535.0;
+      break;
+    }
+    std::cout << "[" << f + 1 << "/" << files.gl_pathc << "] " << basename(files.gl_pathv[f]) << " " << mean
+              << std::endl;
 
-    std::vector<int> compression_params;
-    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    compression_params.push_back(9);
-    compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-    compression_params.push_back(95);
+    stats.col(f) = mean;
 
-    cv::imwrite(outputfile, accumulated, compression_params);
-    return 0;
+    if (mean <= threshold) {
+      if (accumulated.empty()) {
+        image.copyTo(accumulated);
+      } else {
+        accumulated = cv::max(accumulated, image);
+      }
+    }
+  }
+
+  // Calculate some statistics
+  double min_mean, max_mean;
+  cv::Point min_loc;
+  cv::minMaxLoc(stats, &min_mean, &max_mean, &min_loc);
+  double mean_mean = cv::mean(stats)[0];
+
+  // For median, do partial sort and take middle value
+  std::vector<double> vec;
+  stats.copyTo(vec);
+  std::nth_element(vec.begin(), vec.begin() + (vec.size() / 2), vec.end());
+  double median_mean = vec[vec.size() / 2];
+
+  std::cout << "Minimum: " << min_mean << " maximum: " << max_mean << " mean: " << mean_mean
+            << " median: " << median_mean << std::endl;
+
+  // If we still don't have an image (no images below threshold), copy the minimum mean image so we see why
+  if (accumulated.empty()) {
+    std::cout << "No images below threshold, writing the minimum image only" << std::endl;
+    accumulated = cv::imread(files.gl_pathv[min_loc.x], cv::IMREAD_UNCHANGED);
+  }
+  globfree(&files);
+
+  std::vector<int> compression_params;
+  compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+  compression_params.push_back(9);
+  compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+  compression_params.push_back(95);
+
+  cv::imwrite(outputfile, accumulated, compression_params);
+  return 0;
 }

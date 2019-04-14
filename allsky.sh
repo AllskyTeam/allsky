eb758df56af8a25c4cab24ec651d95e861fdb69a
Michael J. Kidd <linuxkidd@gmail.com>
2019-04-14 16:43:01 -0700

    Added check in allsky.sh to ensure a ZWO device is present

--- allsky.sh
+++ allsky.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ $(lsusb | grep 03c3: -c) -eq 0 ]; then
+        echo ZWO Camera not found.  Exiting. >&2
+        sudo systemctl stop allsky
+        exit 0
+fi
+
 source /home/pi/allsky/config.sh
 source /home/pi/allsky/scripts/filename.sh
 

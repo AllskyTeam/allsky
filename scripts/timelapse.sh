995d868bf6a91e61ae5c314460b87004317ceb02
Thomas Jacquin <jacquin.thomas@gmail.com>
2019-08-22 12:39:26 -0700

    Changed timelapse script to preserve timestamp in image name

--- scripts/timelapse.sh
+++ scripts/timelapse.sh
@@ -15,21 +15,20 @@ if [ $# -lt 1 ]
         exit 3
 fi
 
-# find thumbnails and rename them to match full images names
-find "images/$1/thumbnails" -name "*.$EXTENSION" -size 0 -delete
-ls -rt images/$1/thumbnails/*.$EXTENSION |
-gawk 'BEGIN{ a=1 }{ printf "mv -v %s images/'$1'/thumbnails/%04d.'$EXTENSION'\n", $0, a++ }' |
+echo -en "* ${GREEN}Creating symlinks to generate timelapse${NC}\n"
+mkdir /home/pi/allsky/images/$1/sequence/
+# find images, make symlinks sequentially and start avconv to build mp4; upload mp4 and move directory
+find "/home/pi/allsky/images/$1" -name "*.$EXTENSION" -size 0 -delete
+ls -rt /home/pi/allsky/images/$1/*.$EXTENSION |
+gawk 'BEGIN{ a=1 }{ printf "ln -sv %s /home/pi/allsky/images/'$1'/sequence/%04d.'$EXTENSION'\n", $0, a++ }' |
 bash
-
-# find images, rename images sequentially and start avconv to build mp4; upload mp4 and move directory
-find "images/$1" -name "*.$EXTENSION" -size 0 -delete
-ls -rt images/$1/*.$EXTENSION |
-gawk 'BEGIN{ a=1 }{ printf "mv -v %s images/'$1'/%04d.'$EXTENSION'\n", $0, a++ }' |
-bash
-ffmpeg -y -f image2 -r 25 -i images/$1/%04d.$EXTENSION -vcodec libx264 -b:v 2000k -pix_fmt yuv420p images/$1/allsky-$1.mp4
+avconv -y -f image2 -r 25 -i images/$1/sequence/%04d.$EXTENSION -vcodec libx264 -b:v 2000k -pix_fmt yuv420p images/$1/allsky-$1.mp4
 
 if [ "$UPLOAD_VIDEO" = true ] ; then
 	lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$MP4DIR" -e "set net:max-retries 1; put images/$1/allsky-$1.mp4; bye"
 fi
+
+echo -en "* ${GREEN}Deleting sequence${NC}\n"
+rm -rf /home/pi/allsky/images/$1/sequence
 
 echo -en "* ${GREEN}Timelapse was created${NC}\n"

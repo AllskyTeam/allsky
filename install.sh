6beb91b631d108c1698a37628f4b5ed696706f5e
Chris Kuethe <chris.kuethe+github@gmail.com>
2019-10-04 09:10:01 -0700

    A little bit of additional documentation

--- install.sh
+++ install.sh
@@ -7,21 +7,32 @@ echo -e "${RED}**********************************************"
 echo    "*** Welcome to the Allsky Camera installer ***"
 echo -e "**********************************************${NC}"
 echo -en '\n'
-echo -en "${GREEN}* Sunwait installation"
-cp sunwait /usr/local/bin
-echo -en '\n'
+
 echo -en "${GREEN}* Dependencies installation\n${NC}"
 apt-get update && apt-get install libopencv-dev libusb-dev ffmpeg gawk lftp jq imagemagick -y
 echo -en '\n'
+
+echo -en "${GREEN}* Compile allsky software\n${NC}"
+make all
+echo -en '\n'
+
+echo -en "${GREEN}* Sunwait installation"
+cp sunwait /usr/local/bin
+echo -en '\n'
+
 echo -en "${GREEN}* Using the camera without root access\n${NC}"
 install asi.rules /etc/udev/rules.d
 udevadm control -R
 echo -en '\n'
+
 echo -en "${GREEN}* Autostart script\n${NC}"
 sed -i '/allsky.sh/d' /etc/xdg/lxsession/LXDE-pi/autostart
 cp autostart/allsky.service /lib/systemd/system/
 chown root:root /lib/systemd/system/allsky.service
 chmod 0644 /lib/systemd/system/allsky.service
+echo -en '\n'
+
+echo -en "${GREEN}* Configure log rotation\n${NC}"
 cp autostart/allsky /etc/logrotate.d/
 chown root:root /etc/logrotate.d/allsky
 chmod 0644 /etc/logrotate.d/allsky
@@ -29,9 +40,7 @@ cp autostart/allsky.conf /etc/rsyslog.d/
 chown root:root /etc/rsyslog.d/allsky.conf
 chmod 0644 /etc/rsyslog.d/allsky.conf
 echo -en '\n'
-echo -en "${GREEN}* Compile allsky software\n${NC}"
-make all
-echo -en '\n'
+
 echo -en "${GREEN}* Copy camera settings files\n${NC}"
 cp settings.json.repo settings.json
 cp config.sh.repo config.sh
@@ -40,6 +49,7 @@ chown -R pi:pi ../allsky
 systemctl daemon-reload
 systemctl enable allsky.service
 echo -en '\n'
+
 echo -en '\n'
 echo -en "The Allsky Software is now installed. You should reboot the Raspberry Pi to finish the installation\n"
 echo -en '\n'

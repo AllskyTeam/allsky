d7df2a75fc4b0d9ba9e594c32a32b25216588546
Dale Ghent <daleg@elemental.org>
2020-08-11 19:29:29 -0400

    Fold ffmpeg command line for legibility

--- scripts/timelapse.sh
+++ scripts/timelapse.sh
@@ -17,12 +17,20 @@ fi
 
 echo -en "* ${GREEN}Creating symlinks to generate timelapse${NC}\n"
 mkdir /home/pi/allsky/images/$1/sequence/
+
 # find images, make symlinks sequentially and start avconv to build mp4; upload mp4 and move directory
 find "/home/pi/allsky/images/$1" -name "*.$EXTENSION" -size 0 -delete
 ls -rt /home/pi/allsky/images/$1/*.$EXTENSION |
 gawk 'BEGIN{ a=1 }{ printf "ln -sv %s /home/pi/allsky/images/'$1'/sequence/%04d.'$EXTENSION'\n", $0, a++ }' |
 bash
-ffmpeg -y -f image2 -r $FPS -i images/$1/sequence/%04d.$EXTENSION -vcodec libx264 -b:v 2000k -pix_fmt yuv420p images/$1/allsky-$1.mp4
+
+ffmpeg -y -f image2 \
+	-r $FPS \
+	-i images/$1/sequence/%04d.$EXTENSION \
+	-vcodec libx264 \
+	-b:v 2000k \
+	-pix_fmt yuv420p \
+	images/$1/allsky-$1.mp4
 
 if [ "$UPLOAD_VIDEO" = true ] ; then
 	lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$MP4DIR" -e "set net:max-retries 1; put images/$1/allsky-$1.mp4; bye"

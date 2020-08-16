8ba8dbf5f6aa85af8bd0ac9e0e578222f63b5010
Thomas Jacquin <jacquin.thomas@gmail.com>
2020-08-16 10:51:52 -0700

    Clean up in save image scripts

--- scripts/saveImageNight.sh
+++ scripts/saveImageNight.sh
@@ -50,10 +50,7 @@ if [ "$UPLOAD_IMG" = true ] ; then
 	echo -e "Resizing $FULL_FILENAME \n" >> log.txt
 
 	# Create a thumbnail for live view
-	# Here's what I use with my ASI224MC
 	convert "$IMAGE_TO_USE" -resize 962x720 -gravity East -chop 2x0 "$FILENAME-resize.$EXTENSION";
-	# Here's what I use with my ASI185MC (larger sensor so I crop the black around the image)
-	#convert "$IMAGE_TO_USE" -resize 962x720 -gravity Center -crop 680x720+40+0 +repage "$FILENAME-resize.$EXTENSION";
 
 	echo -e "Uploading \n"
 	echo -e "Uploading $FILENAME-resize.$EXTENSION \n" >> log.txt

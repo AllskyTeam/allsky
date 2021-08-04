ca5412b1e5fbfce2820ad3cf06f98e1cf86dcba5
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-08-03 21:30:31 -0600

    Update generateForDay.sh
    Added $DATE and $DIR for readability.

--- scripts/generateForDay.sh
+++ scripts/generateForDay.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# This script allows users to manually generate keograms,startrails, and timelapses,
+# outside of the ones generated automatically.
+
 source $ALLSKY_HOME/config.sh
 source $ALLSKY_HOME/scripts/filename.sh
 
 cd  $ALLSKY_HOME/scripts
+
+ME="$(basename "$BASH_ARGV0")"	# Include script name in output so it's easier to find in the log file
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -10,24 +15,27 @@ NC='\033[0m' # No Color
 
 if [ $# -lt 1 ]
   then
-    echo -en "${RED}You need to pass a day argument\n"
-        echo -en "    ex: generateForDay.sh 20180119${NC}\n"
+    echo -en "${ME}: ${RED}You need to pass a day argument\n"
+        echo -en "    ex: ${ME} 20180119${NC}\n"
         exit 3
 fi
 
+DATE=$1
+DIR="$ALLSKY_HOME/images/$DATE"
+
 # Generate timelapse from collected images
-echo -e "Generating Keogram\n"
-mkdir -p $ALLSKY_HOME/images/$1/keogram/
-../keogram $ALLSKY_HOME/images/$1/ $EXTENSION $ALLSKY_HOME/images/$1/keogram/keogram-$1.jpg
+echo -e "${ME}: Generating Keogram\n"
+mkdir -p ${DIR}/keogram/
+../keogram ${DIR}/ $EXTENSION ${DIR}/keogram/keogram-$DATE.jpg
 echo -e "\n"
 
 # Generate startrails from collected images. Treshold set to 0.1 by default in config.sh to avoid stacking over-exposed images
-echo -e "Generating Startrails\n"
-mkdir -p $ALLSKY_HOME/images/$1/startrails/
-../startrails $ALLSKY_HOME/images/$1/ $EXTENSION $BRIGHTNESS_THRESHOLD $ALLSKY_HOME/images/$1/startrails/startrails-$1.jpg
+echo -e "${ME}: Generating Startrails\n"
+mkdir -p ${DIR}/startrails
+../startrails ${DIR}/ $EXTENSION $BRIGHTNESS_THRESHOLD ${DIR}/startrails/startrails-$DATE.jpg
 echo -e "\n"
 
 # Generate timelapse from collected images
-echo -e "Generating Timelapse\n"
-./timelapse.sh $1
+echo -e "${ME}: Generating Timelapse\n"
+./timelapse.sh $DATE
 echo -e "\n"

12042dee506d3617b3016342a96b4b4134bb751d
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-08-03 21:28:02 -0600

    Update endOfNight_additionalSteps.sh
    Mention adding "$ME" to any messages.  "$ME" is the command name, and is useful to have in the log file to know which command produced the output.

--- scripts/endOfNight_additionalSteps.sh
+++ scripts/endOfNight_additionalSteps.sh
@@ -4,3 +4,5 @@
 # Place any additional code you require to be run at the end of the night in this script. This script is run prior
 # to the deletion of any old image files.
 #
+# Include "${ME}" in any output so it's easier to find in the log file, for debugging.
+ME="$(basename "$BASH_ARGV0")"

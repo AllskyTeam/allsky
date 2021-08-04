cecbf3117ebd50a2b353803e3914f07718eb25ef
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-08-03 21:49:42 -0600

    Update removeBadImages.sh
    Optionally also checks for too dark and too bright images, based on REMOVE_BAD_IMAGES_THRESHOLD_LOW and REMOVE_BAD_IMAGES_THRESHOLD_HIGH from config.sh.
    NOTE: I'm not sure how to do that check if "parallel" is used.
    Removes bad thumbnails in addition to bad images, and displays reason they are removed.
    Centralized redundant code for easier maintenance.
    Outputs a summary line.

--- scripts/removeBadImages.sh
+++ scripts/removeBadImages.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+REMOVE_BAD_IMAGES_THRESHOLD_LOW=${REMOVE_BAD_IMAGES_THRESHOLD_LOW:-0}	# in case not in config.sh file
+REMOVE_BAD_IMAGES_THRESHOLD_HIGH=${REMOVE_BAD_IMAGES_THRESHOLD_HIGH:-0}	# in case not in config.sh file
+
+ME="$(basename "$BASH_ARGV0")"	# Include script name in output so it's easier to find in the log file
+
 if [ $# -ne 1 -o 'x$1' == 'x-h' ] ; then
 	echo "Remove images with corrupt data which might mess up startrails and keograms"
-	echo "usage: $0 <directory>"
+	echo "usage: $ME <directory>"
 	exit 1
 fi
 
 if [ \! -d "$1" ] ; then
-	echo "$1 is not a directory"
+	echo "$ME: $1 is not a directory"
 	exit 1
 fi
+DIR=$1
 
 # Super simple: find the full size image-*jpg and image-*png files (not the
 # thumbnails) and ask imagemagick to compute a histogram (which is discarded)
@@ -28,11 +34,47 @@ fi
 # If GNU Parallel is installed (it's not by default), then blast through and
 # clean all the images as fast as possible without regard for CPU utilization.
 
+# Use IMAGE_FILES and ERROR_WORDS to avoid duplicating them.
+# Remove 0-length files ("insufficient image data") and files too dim or bright.
+# $DIR may end in a "/" so there will be "//" in the filenames, but there's no harm in that.
+cd $DIR
+IMAGE_FILES="$( find . -type f \( -iname image-\*.jpg -o -iname image-\*.png \) \! -ipath \*thumbnail\* )"
+ERROR_WORDS="Huffman|Bogus|Corrupt|Invalid|Trunc|Missing|insufficient image data|no decode delegate"
+
+TMP=badError.txt
+
 if which parallel > /dev/null ; then
-	find "$1" -type f \( -iname image-\*.jpg -o -iname image-\*.png \) \! -ipath \*thumbnail\*  | \
-		parallel -- "convert {} histogram:/dev/null 2>&1 | egrep -q 'Huffman|Bogus|Corrupt|Invalid|Trunc|Missing' && rm -vf {}"
+	echo $IMAGE_FILES | \
+		parallel -- "convert {} histogram:/dev/null 2>&1 | egrep -q "$ERROR_WORDS" && rm -vf {}"
+	# xxxxxxxxxx need to add THRESHOLD checking here and remove bad thumbnails...
+	# xxxxxxxxxx Can we replace "rm -vf" above with "echo" and redirect output to the tmp file,
+	# xxxxxxxxxx then do a "for f in $(cat $TMP); do" and remove the files that way?
 else
-	for f in $( find "$1" -type f \( -iname image-\*.jpg -o -iname image-\*.png \) \! -ipath \*thumbnail\* ) ; do
-		nice convert "$f"  histogram:/dev/null 2>&1 | egrep -q 'Huffman|Bogus|Corrupt|Invalid|Trunc|Missing' && rm -vf $f
+	typeset -i num_bad=0
+	for f in ${IMAGE_FILES} ; do
+		MEAN=$(nice convert "$f" -colorspace Gray -format "%[fx:image.mean]" info: 2> $TMP)
+		BAD=""
+		egrep -q "$ERROR_WORDS" $TMP
+		RET=$?
+		if [ $RET -eq 0 ] ; then
+			rm -f "$f" "thumbnails/$f"
+			BAD="'$f' (corrupt file: $(cat $TMP))"
+			let num_bad=num_bad+1
+		else
+			# Multiply MEAN by 100 to convert to integer (0-100 %) since bash doesn't work with floats.
+			MEAN=$(echo "$MEAN" | awk '{ printf("%d", $1 * 100); }')
+			if [ $MEAN -lt $REMOVE_BAD_IMAGES_THRESHOLD_LOW -o $MEAN -gt $REMOVE_BAD_IMAGES_THRESHOLD_HIGH ]; then
+				rm -f "$f" "thumbnails/$f"
+				BAD="'$f' (bad threshold: MEAN=$MEAN)"
+				let num_bad=num_bad+1
+			fi
+		fi
+		[ "$BAD" != "" ] && echo "$ME: Removed $BAD"
 	done
+	if [ $num_bad -eq 0 ]; then
+		echo "$ME: No bad files found."
+	else
+		echo "$ME: $num_bad bad file(s) found and removed."
+	fi
 fi
+rm -f $TMP

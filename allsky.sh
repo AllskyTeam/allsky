a2739115e8f9e33558c263c349cd6384feef688b
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-08-03 22:56:09 -0600

    Update allsky.sh
    Keep prior log file to aid in debugging.
    Ability to reset the USB bus if the ZWO camera isn't found.
    Put settings in a bash array, so they can have spaces in them.
    Optionally display "notification" images.
    Checks if output is a tty (user invoked it manually) or not (service invoked it) - this is used by capture program to display appropriate log messages.

--- allsky.sh
+++ allsky.sh
@@ -10,64 +10,72 @@ cd $ALLSKY_HOME
 echo "" > "$ALLSKY_HOME/autocam.sh"
 source $ALLSKY_HOME/config.sh
 
-echo "     ***** Starting AllSky *****"	# ECC added to make it easy to find beginning of this run in the log file
+# Make it easy to find the beginning of this run in the log file.
+echo "     ***** Starting AllSky *****"
 echo "Making sure allsky.sh is not already running..."
 ps -ef | grep allsky.sh | grep -v $$ | xargs "sudo kill -9" 2>/dev/null
 
-mv -f log.txt OLD_log.txt 2> /dev/null		# ECC added
+mv -f log.txt OLD_log.txt 2> /dev/null		# save the prior log file for debugging
 > log.txt
 
-# ECC added notification images
-USE_NOTIFICATION_IMAGES=$(jq -r '.notificationimages' "$CAMERA_SETTINGS")
-if [ "$USE_NOTIFICATION_IMAGES" = "1" ] ; then
-	$ALLSKY_HOME/scripts/copy_notification_image.sh "StartingUp" 2>&1
-fi
-
 # old/regular manual camera selection mode => exit if no requested camera was found
-RPiHQIsPresent=$(vcgencmd get_camera)
-if [[ $CAMERA == "RPiHQ" && $RPiHQIsPresent != "supported=1 detected=1" ]]; then
-echo "RPiHQ Camera not found. Exiting." >&2
+if [[ $(vcgencmd get_camera) == "supported=1 detected=1" ]]; then
+	RPiHQIsPresent=1
+else
+	RPiHQIsPresent=0
+fi
+if [[ $CAMERA == "RPiHQ" && $RPiHQIsPresent -eq 0 ]]; then
+	echo "RPiHQ Camera not found. Exiting." >&2
         sudo systemctl stop allsky
         exit 0
 fi
 
-# ECC: added: 2>/dev/null, and separated into 2 commands to better aid debugging when camera isn't found.
-ZWOdev=$(lsusb | awk '/ 03c3:/ { bus=$2; dev=$4; gsub(/[^0-9]/,"",dev); print "/dev/bus/usb/"bus"/"dev;}')
-ZWOIsPresent=$(lsusb -D ${ZWOdev} 2>/dev/null | grep -c 'iProduct .*ASI[0-9]')
-if [[ $CAMERA == "ZWO" &&  $ZWOIsPresent -eq 0 ]]; then
-        echo "ZWO Camera not found..." >&2
-	if [[ $ZWOdev == "" ]]; then
-		echo "  and no USB entry found for it." >&2
-	else
-		echo "  but entry '$ZWOdev' found for it." >&2
-	fi
-	if true; then
-        	echo "  Fixing USB and restarting." >&2
-		sudo ~/uhubctl/uhubctl -a cycle -l 2		# l-ocation 2 are USB 3 ports
-		# No need for 'sudo systemctl stop restart' since the service will automatically restart this script.
-        	exit 0
-	else
-        	echo "  Exiting." >&2
-        	sudo systemctl stop allsky
-        	exit 0
+if [[ $CAMERA != "RPiHQ" ]]; then
+	# Use two commands to better aid debugging when camera isn't found.
+	# xxxxx This doesn't catch cases where CAMERA is "auto" and we should use ZWO.
+	ZWOdev=$(lsusb | awk '/ 03c3:/ { bus=$2; dev=$4; gsub(/[^0-9]/,"",dev); print "/dev/bus/usb/"bus"/"dev;}')
+	ZWOIsPresent=$(lsusb -D ${ZWOdev} 2>/dev/null | grep -c 'iProduct .*ASI[0-9]')
+	if [[ $CAMERA == "ZWO" && $ZWOIsPresent -eq 0 ]]; then
+        	echo "ZWO Camera not found..." >&2
+		if [[ $ZWOdev == "" ]]; then
+			echo "  and no USB entry found for it." >&2
+		else
+			echo "  but USB entry '$ZWOdev' found for it." >&2
+		fi
+		if [ "$UHUBCTL_PATH" != "" ] ; then
+			if tty --silent ; then
+				echo "  Resetting USB ports; restart allsky.sh when done." >&2
+			else
+        			echo "  Resetting USB ports and restarting." >&2
+				# The service will automatically restart this script.
+			fi
+			sudo "$UHUBCTL_PATH" -a cycle -l "$UHUBCTL_PORT"
+        		exit 0
+		else
+        		echo "  Exiting." >&2
+        		echo "  If you have the 'uhubctl' command installed, add it to config.sh." >&2
+        		echo "  In the meantime, try running it to reset the USB bus." >&2
+        		sudo systemctl stop allsky
+        		exit 0
+		fi
 	fi
 fi
 
 # CAMERA AUTOSELECT
-# exit if no camare found at all
-if [[ $CAMERA -eq "auto" ]]; then
+# exit if no camera found at all
+if [[ $CAMERA == "auto" ]]; then
   echo "Trying to automatically choose between ZWO and RPI camera"
-  if [[ $ZWOIsPresent -eq 0 && $RPiHQIsPresent != "supported=1 detected=1" ]]; then
+  if [[ $ZWOIsPresent -eq 0 && $RPiHQIsPresent -eq 0 ]]; then
           echo "None of RPI or ZWO Cameras were found. Exiting." >&2
           sudo systemctl stop allsky
           exit 0
   fi
   # prioritize ZWO camera if exists, and use RPI camera otherwise
   if [[ $ZWOIsPresent -eq 0 ]]; then
-    echo "No ZWO camera found. Choosing RPI"
+    # echo "No ZWO camera found. Choosing RPI"	# CAMERA is displayed below; don't need it here too
     CAMERA="RPiHQ"
   else
-    echo "ZWO camera found. Choosing ZWO"
+    # echo "ZWO camera found. Choosing ZWO"	# CAMERA is displayed below; don't need it here too
     CAMERA="ZWO"
   fi
 
@@ -75,8 +83,13 @@ if [[ $CAMERA -eq "auto" ]]; then
   CAMERA_SETTINGS="$CAMERA_SETTINGS_DIR/settings_$CAMERA.json"
 fi
 
+# Optionally display a notification image
+USE_NOTIFICATION_IMAGES=$(jq -r '.notificationimages' "$CAMERA_SETTINGS")
+if [ "$USE_NOTIFICATION_IMAGES" = "1" ] ; then
+	$ALLSKY_HOME/scripts/copy_notification_image.sh "StartingUp" 2>&1
+fi
 
-echo "Settings check done"
+# echo "Settings check done"
 echo "CAMERA: ${CAMERA}"
 echo "CAMERA_SETTINGS: ${CAMERA_SETTINGS}"
 # save auto camera selection for the current session, will be read in "$ALLSKY_HOME/config.sh" file
@@ -87,31 +100,48 @@ source $ALLSKY_HOME/scripts/filename.sh
 
 echo "Starting allsky camera..."
 
-# Building the arguments to pass to the capture binary
-ARGUMENTS=""
+# Building the arguments to pass to the capture binary.
+# Want to allow spaces in arguments so need to put quotes around them,
+# but in order for it to work need to make ARGUMENTS an array.
+ARGUMENTS=()
 KEYS=( $(jq -r 'keys[]' $CAMERA_SETTINGS) )
 for KEY in ${KEYS[@]}
 do
-	ARGUMENTS="$ARGUMENTS -$KEY `jq -r '.'$KEY $CAMERA_SETTINGS` "
+	K="`jq -r '.'$KEY $CAMERA_SETTINGS`"
+	ARGUMENTS+=(-$KEY "$K")
 done
 
 # When using a desktop environment (Remote Desktop, VNC, HDMI output, etc), a preview of the capture can be displayed in a separate window
 # The preview mode does not work if allsky.sh is started as a service or if the debian distribution has no desktop environment.
 if [[ $1 == "preview" ]] ; then
-	ARGUMENTS="$ARGUMENTS -preview 1"
+	ARGUMENTS+=(-preview 1)
 fi
-ARGUMENTS="$ARGUMENTS -daytime $DAYTIME"
+ARGUMENTS+=(-daytime $DAYTIME)
 
-echo "$ARGUMENTS" >> log.txt			# ECC modified
+# Determine if we're called from the service (tty will fail).
+tty --silent
+if [ $? -eq 0 ] ; then
+	TTY=1
+else
+	TTY=0
+fi
+ARGUMENTS+=(-tty $TTY)
+
+Z=""
+for A in ${ARGUMENTS[@]}
+do
+	Z="$Z $A"
+done
+echo "$Z" >> log.txt
 
 RETCODE=0
 if [[ $CAMERA == "ZWO" ]]; then
-	$ALLSKY_HOME/capture $ARGUMENTS
+	$ALLSKY_HOME/capture "${ARGUMENTS[@]}"
 	RETCODE=$?
 	echo "capture exited with retcode=$RETCODE"
 
 elif [[ $CAMERA == "RPiHQ" ]]; then
-	$ALLSKY_HOME/capture_RPiHQ $ARGUMENTS
+	$ALLSKY_HOME/capture_RPiHQ "${ARGUMENTS[@]}"
 	RETCODE=$?
 else
 	exit 1

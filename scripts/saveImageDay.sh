4aa0104dd65acff71bfd6f8cb70efa8a06286e15
Thomas Jacquin <jacquin.thomas@gmail.com>
2021-08-15 23:03:15 -0700

    Fixed merge conflict in saveImageDay.sh

--- scripts/saveImageDay.sh
+++ scripts/saveImageDay.sh
@@ -1,126 +1,101 @@
 #!/bin/bash
-
-if [ $# -eq 1 ] ; then
-	if [ "x$1" = "x-h" ] ; then
-		echo "Usage: $BASH_ARGV0 [YYYYmmdd]"
-		exit
-	else
-		LAST_NIGHT=$1
-	fi
-else
-	LAST_NIGHT=$(date -d '12 hours ago' +'%Y%m%d')
-fi
-
 source $ALLSKY_HOME/config.sh
 source $ALLSKY_HOME/scripts/filename.sh
+source $ALLSKY_HOME/scripts/darkCapture.sh	# does not return if in darkframe mode
 source $ALLSKY_HOME/scripts/ftp-settings.sh
 
-cd  $ALLSKY_HOME/scripts
+cd $ALLSKY_HOME
 ME="$(basename "$BASH_ARGV0")"	# Include script name in output so it's easier to find in the log file
 
-# Post end of night data. This includes next twilight time
-if [[ $POST_END_OF_NIGHT_DATA == "true" ]]; then
-        echo -e "$ME: Posting next twilight time to let server know when to resume liveview\n"
-        ./postData.sh
-	echo -e "\n"
+IMAGE_TO_USE="$FULL_FILENAME"
+# quotes around $IMAGE_TO_USE below, in case it has a space or special characters.
+
+# Make sure the image isn't corrupted
+identify "$IMAGE_TO_USE" >/dev/null 2>&1
+RET=$?
+if [ $RET -ne 0 ] ; then
+	echo "*** $ME: ERROR: Image '${IMAGE_TO_USE} is corrupt; ignoring."
+	exit 1
 fi
 
-LAST_NIGHT_DIR="$ALLSKY_HOME/images/$LAST_NIGHT"
-
-# Scan for, and remove corrupt images before generating
-# keograms and startrails. This can take several (tens of) minutes to run
-# and isn't necessary unless your system produces corrupt images which then
-# generate funny colors in the summary images...
-if [[ "$REMOVE_BAD_IMAGES" == "true" ]]; then
-	echo -e "$ME: Removing bad images\n"
-	./removeBadImages.sh $LAST_NIGHT_DIR
+# Resize the image if required
+if [[ $IMG_RESIZE == "true" ]]; then
+        convert "$IMAGE_TO_USE" -resize "$IMG_WIDTH"x"$IMG_HEIGHT" "$IMAGE_TO_USE"
+	RET=$?
+	if [ $RET -ne 0 ] ; then
+		echo "*** $ME: ERROR: IMG_RESIZE failed with RET=$RET"
+		exit 1
+	fi
 fi
 
-TMP_DIR="$ALLSKY_HOME/tmp"
-mkdir -p "$TMP_DIR"
+# Crop the image around the center if required
+if [[ $CROP_IMAGE == "true" ]]; then
+        convert "$IMAGE_TO_USE" -gravity Center -crop "$CROP_WIDTH"x"$CROP_HEIGHT"+"$CROP_OFFSET_X"+"$CROP_OFFSET_Y" +repage "$IMAGE_TO_USE"
+	RET=$?
+	if [ $RET -ne 0 ] ; then
+		echo "*** $ME: ERROR: CROP_IMAGE failed with RET=$RET"
+		exit 1
+	fi
+fi
 
-# Generate keogram from collected images
-if [[ $KEOGRAM == "true" ]]; then
-        echo -e "$ME: Generating Keogram\n"
-        mkdir -p $LAST_NIGHT_DIR/keogram/
-	OUTPUT="$LAST_NIGHT_DIR/keogram/keogram-$LAST_NIGHT.$EXTENSION"
-	# The keogram command outputs one line for each of the many hundreds of files,
-	# and this adds needless clutter to the log file, so send output to a tmp file so we can output the
-	# number of images.
+# IMG_DIR and IMG_PREFIX are in config.sh
+# If the user specified an IMG_PREFIX, copy the file to that name so the websites can display it.
+if [ "${IMG_PREFIX}" != "" ]; then
+	cp "$IMAGE_TO_USE" "${IMG_PREFIX}${FILENAME}.${EXTENSION}"
+fi
 
-	TMP="$TMP_DIR/keogramTMP.txt"
-        ../keogram $LAST_NIGHT_DIR/ $EXTENSION $OUTPUT > ${TMP}
-	RETCODE=$?
-        if [[ $UPLOAD_KEOGRAM == "true" && $RETCODE = 0 ]] ; then
-                if [[ $PROTOCOL == "S3" ]] ; then
-                        $AWS_CLI_DIR/aws s3 cp $OUTPUT s3://$S3_BUCKET$KEOGRAM_DIR --acl $S3_ACL &
-		elif [[ $PROTOCOL == "local" ]] ; then
-                	cp $OUTPUT $KEOGRAM_DIR &
-                else
-                        lftp "$PROTOCOL://$USER:$PASSWORD@$HOST:$KEOGRAM_DIR" \
-                                -e "set net:max-retries 1; put "$OUTPUT"; bye" &
-                fi
+# If 24 hour saving is desired, save the current image in today's directory
+if [ "$CAPTURE_24HR" = "true" ] ; then
+	CURRENT=$(date +'%Y%m%d')
+
+	mkdir -p images/$CURRENT
+	THUMBNAILS_DIR=images/$CURRENT/thumbnails
+	mkdir -p $THUMBNAILS_DIR
+
+	# Save image in images/current directory
+	SAVED_FILE="$FILENAME-$(date +'%Y%m%d%H%M%S').$EXTENSION"
+	cp "$IMAGE_TO_USE" "images/$CURRENT/$SAVED_FILE"
+
+	# Create a thumbnail of the image for faster load in web GUI.
+	# If we resized above, this will be a resize of a resize,
+	# but for thumbnails it should be ok.
+	convert "$IMAGE_TO_USE" -resize "${THUMBNAIL_SIZE_X}x${THUMBNAIL_SIZE_Y}" "$THUMBNAILS_DIR/$SAVED_FILE"
+	RET=$?
+	if [ $RET -ne 0 ] ; then
+		echo "*** $ME: ERROR: THUMBNAIL resize failed with RET=$RET; continuing."
+    	fi
+fi
+
+# If upload is true, optionally create a smaller version of the image; either way, upload it
+if [ "$UPLOAD_IMG" = true ] ; then
+	if [[ "$RESIZE_UPLOADS" == "true" ]]; then
+		echo -e "$ME: Resizing '$IMAGE_TO_USE' for uploading"
+		echo -e "$ME: Resizing '$IMAGE_TO_USE' for uploading\n" >> log.txt
+
+		# Create a smaller version for upload
+		convert "$IMAGE_TO_USE" -resize "$RESIZE_UPLOADS_SIZE" -gravity East -chop 2x0 "$IMAGE_TO_USE"
+		RET=$?
+		if [ $RET -ne 0 ] ; then
+			echo "*** $ME: ERROR: RESIZE_UPLOADS failed with RET=$RET"
+			exit 1
+		fi
+	fi
+
+	TS=$(ls -l --time-style='+%H:%M:%S' "$IMAGE_TO_USE" | awk '{print $6}')
+	echo -e "$ME: Uploading '$IMAGE_TO_USE' with timestamp: $TS\n"
+	echo -e "$ME: Uploading '$IMAGE_TO_USE' with timestamp: $TS" >> log.txt
+	if [[ $PROTOCOL == "S3" ]] ; then
+                $AWS_CLI_DIR/aws s3 cp "$IMAGE_TO_USE" s3://$S3_BUCKET$IMGDIR --acl $S3_ACL &
+        elif [[ $PROTOCOL == "local" ]] ; then
+		cp "$IMAGE_TO_USE" "$IMGDIR" &
+	else
+		# Put to a temp name, then move the temp name to the final name.
+		# This is because slow uplinks can cause multiple lftp requests to be running at once,
+		# and only one lftp can upload the file at once, otherwise get this error:
+		# put: Access failed: 550 The process cannot access the file because it is being used by another process.  (image.jpg)
+		# Slow uploads also cause a problem with web pages that try to read the file as it's being uploaded.
+		# "si" = "save image" - use a unique temporary name
+		TEMP_NAME="si-$RANDOM"
+		lftp "$PROTOCOL://$USER:$PASSWORD@$HOST:$IMGDIR" -e "set net:max-retries 2; set net:timeout 20; put "$IMAGE_TO_USE" -o $TEMP_NAME; rm -f "$IMAGE_TO_USE"; mv $TEMP_NAME "$IMAGE_TO_USE"; bye" &
         fi
-	echo -e "$ME: Processed $(wc -l < ${TMP}) keogram files\n"
-	# Leave ${TMP} in case the user needs to debug something.
-
-	# Optionally copy to the local website in addition to the upload above.
-	if [ "$WEB_KEOGRAM_DIR" != "" ]; then
-		cp $OUTPUT "$WEB_KEOGRAM_DIR"
-	fi
-fi
-
-# Generate startrails from collected images.
-# Threshold set to 0.1 by default in config.sh to avoid stacking over-exposed images.
-if [[ $STARTRAILS == "true" ]]; then
-        echo -e "$ME: Generating Startrails\n"
-        mkdir -p $LAST_NIGHT_DIR/startrails/
-	OUTPUT="$LAST_NIGHT_DIR/startrails/startrails-$LAST_NIGHT.$EXTENSION"
-	# The startrails command outputs one line for each of the many hundreds of files,
-	# and this adds needless clutter to the log file, so send output to a tmp file so we can output the
-	# number of images.
-	TMP="$TMP_DIR/startrailsTMP.txt"
-        ../startrails $LAST_NIGHT_DIR/ $EXTENSION $BRIGHTNESS_THRESHOLD $OUTPUT > ${TMP}
-	RETCODE=$?
-        if [[ $UPLOAD_STARTRAILS == "true" && $RETCODE == 0 ]] ; then
-                if [[ $PROTOCOL == "S3" ]] ; then
-                        $AWS_CLI_DIR/aws s3 cp $OUTPUT s3://$S3_BUCKET$STARTRAILS_DIR --acl $S3_ACL &
-                elif [[ $PROTOCOL == "local" ]] ; then
-                        cp $OUTPUT $STARTRAILS_DIR &
-		else
-                        lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$STARTRAILS_DIR" \
-                                -e "set net:max-retries 1; put $OUTPUT; bye" &
-                fi
-        fi
-        echo -e "$ME: Processed $(wc -l < ${TMP}) startrails files.  Summary:\n"
-	grep "^Minimum" "${TMP}"
-	# Leave ${TMP} in case the user needs to debug something.
-
-	# Optionally copy to the local website in addition to the upload above.
-	if [ "$WEB_STARTRAILS_DIR" != "" ]; then
-		cp $OUTPUT "$WEB_STARTRAILS_DIR"
-	fi
-fi
-
-# Generate timelapse from collected images
-if [[ $TIMELAPSE == "true" ]]; then
-	echo -e "$ME: Generating Timelapse\n"
-	./timelapse.sh $LAST_NIGHT
-	echo -e "\n"
-
-	# Optionally copy to the local website in addition to the upload above.
-	if [ "$WEB_MP4DIR" != "" ]; then
-		cp $LAST_NIGHT_DIR/allsky-$LAST_NIGHT.mp4 "$WEB_MP4DIR"
-	fi
-fi
-
-# Run custom script at the end of a night. This is run BEFORE the automatic deletion just in case you need to do something with the files before they are removed
-./endOfNight_additionalSteps.sh
-
-# Automatically delete old images and videos
-if [[ $AUTO_DELETE == "true" ]]; then
-	del=$(date --date="$NIGHTS_TO_KEEP days ago" +%Y%m%d)
-	for i in `find $ALLSKY_HOME/images/ -type d -name "2*"`; do	# "2*" for years >= 2000
-	  (($del > $(basename $i))) && rm -rf $i
-	done
 fi

343d6b5e79619bfa25705d6322c6aafc727a1317
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-09-29 19:26:51 -0500

    Centralized upload

--- scripts/timelapse.sh
+++ scripts/timelapse.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-if [ -z "$ALLSKY_HOME" ] ; then
-	export ALLSKY_HOME=$(realpath $(dirname "$BASH_ARGV0")/..)
+# Allow this script to be executed manually, which requires ALLSKY_HOME to be set.
+if [ -z "${ALLSKY_HOME}" ] ; then
+	export ALLSKY_HOME="$(realpath $(dirname "${BASH_ARGV0}")/..)"
 fi
 
-source $ALLSKY_HOME/config.sh
-source $ALLSKY_HOME/scripts/filename.sh
-source $ALLSKY_HOME/scripts/ftp-settings.sh
+source "${ALLSKY_HOME}/variables.sh"
+source "${ALLSKY_CONFIG}/config.sh"
+source "${ALLSKY_SCRIPTS}/filename.sh"
+source "${ALLSKY_SCRIPTS}/ftp-settings.sh"
+
+ME="$(basename "${BASH_ARGV0}")"
 
 cd $ALLSKY_HOME
 
-ME="$(basename "$BASH_ARGV0")"	# Include script name in output so it's easier to find in the log file
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
-if [ $# -lt 1 -o $# -gt 2 -o "x$1" = "x-h" ] ; then
+if [ $# -lt 1 -o $# -gt 2 -o "${1}" = "-h" -o "${1}" = "--help" ] ; then
     TODAY=$(date +%Y%m%d)
-    echo -en "${RED}Usage: ${ME} <day> [directory]${NC}\n"
-    echo -en "    ex: timelapse.sh ${TODAY}\n"
-    echo -en "    or: timelapse.sh ${TODAY} /media/external/allsky/${TODAY}\n"
-    exit 3
+    echo -en "${RED}"
+	echo -n "Usage: ${ME} [--help]  DATE  [directory]"
+	echo -e "${NC}"
+	echo "    example: ${ME} ${TODAY}"
+	echo "    or:      ${ME} ${TODAY} /media/external/allsky"
+	echo -en "${YELLOW}"
+	echo "'DATE' must be in '${ALLSKY_IMAGES}' unless 'directory' is specified,"
+	echo "in which case 'DATE' must bin in 'directory', i.e., 'directory/DATE'."
+	echo -en "${NC}"
+	exit 1
 fi
 
-# Allow timelapses of pictures not in ALLSKY_HOME.
-# If $2 is passed, it's the top folder, otherwise use the one in ALLSKY_HOME.
-DAY="$1"
-if [ "$2" = "" ] ; then
-	DIR="$ALLSKY_HOME/images/$DAY"	# Need full pathname for links
+# Allow timelapses of pictures not in the standard ALLSKY_IMAGES directory.
+# If $2 is passed, it's the top folder, otherwise use the one in ALLSKY_IMAGES.
+DATE="${1}"
+if [ "${2}" = "" ] ; then
+	DATE_DIR="${ALLSKY_IMAGES}/${DATE}"	# Need full pathname for links
 else
-	DIR="$2"
+	DATE_DIR="${2}/${DATE}"
 fi
 
 # Guess what the likely image extension is (unless specified in the config) by
 # looking at the most common extension in the target day directory
-if [ -z "$EXTENSION" ] ; then
-	EXT_GUESS=$(ls $DIR | sed -e 's/.*[.]//' | sort | uniq -c | head -1 | sed -e 's/.* //')
-    echo -en "${RED}${ME}: file EXTENSION not found in configuration, guessing ${EXT_GUESS}${NC}\n"
-	EXTENSION=$EXT_GUESS
+if [ -z "${EXTENSION}" ] ; then
+	EXTENSION=$(ls "${DATE_DIR}" | sed -e 's/.*[.]//' | sort | uniq -c | head -1 | sed -e 's/.* //')
+	echo -en "${YELLOW}"
+	echo -n "${ME}: file EXTENSION not found in configuration, guessing ${EXTENSION}"
+	echo -e "${NC}"
 fi
 
 # If you are tuning encoder settings, run this script with KEEP_SEQUENCE, eg.
@@ -48,37 +52,36 @@ fi
 # of the sequence directory if it looks ok (contains at least 100 files). This
 # might save you some time when running your encoder repeatedly.
 
-NSEQ=$(ls "/${DIR}/sequence" 2>/dev/null | wc -l )
+SEQUENCE_DIR="${DATE_DIR}/sequencce"
+NSEQ=$(ls "${SEQUENCE_DIR}" 2>/dev/null | wc -l )
 if [ -z "$KEEP_SEQUENCE" -o $NSEQ -lt 100 ] ; then
-	echo -en "${ME}: ${GREEN}Creating symlinks to generate timelapse${NC}\n"
-	rm -fr $DIR/sequence
-	mkdir -p $DIR/sequence
+	rm -fr "${SEQUENCE_DIR}"
+	mkdir -p "${SEQUENCE_DIR}"
 
-	# find images, make symlinks sequentially and start avconv to build mp4; upload mp4 and move directory
-	find "$DIR" -name "*.$EXTENSION" -size 0 -delete
+	# Delete any 0=length files
+	find "${DATE_DIR}" -name "*.${EXTENSION}" -size 0 -delete
 
-	TMP_DIR="$ALLSKY_HOME/tmp"
-	mkdir -p "$TMP_DIR"
-	TMP="$TMP_DIR/timelapseTMP.txt"	# capture the "ln" commands in case the user needs to debug
-	> $TMP
-	ls -rt $DIR/*.$EXTENSION |
+	# capture the "ln" commands in case the user needs to debug
+	mkdir -p "${ALLSKY_TMP}"
+	TMP="${ALLSKY_TMP}/timelapseTMP.txt"
+	> "${TMP}"
+	ls -rt "${DATE_DIR}"/*.${EXTENSION} |
 	gawk 'BEGIN{ a=1 }
 		{
 			printf "ln -s %s '$DIR'/sequence/%04d.'$EXTENSION'\n", $0, a;
-			printf "ln -s %s '$DIR'/sequence/%04d.'$EXTENSION'\n", $0, a >> "'$TMP'";
-			# if (a % 100 == 0) printf "echo '$ME': %d links created so far\n", a;
+			printf "ln -s %s '$DIR'/sequence/%04d.'$EXTENSION'\n", $0, a >> "'${TMP}'";
 			a++;
 		}' |
 	bash
 
 	# Make sure there's at least one link.
-	NUM_FILES=$(wc -l < ${TMP})
+	NUM_FILES=$(wc -l < "${TMP}")
 	if [ $NUM_FILES -eq 0 ]; then
-		echo -en "*** ${ME}: ${RED}ERROR: No links found!${NC}\n"
-		rmdir "${DIR}/sequence"
+		echo -en "${RED}*** ${ME}: ERROR: No images found!${NC}\n"
+		rmdir "${SEQUENCE_DIR}"
 		exit 1
 	else
-		echo -e "$ME: Created $NUM_FILES links total\n"
+		echo "${ME}: Processing ${NUM_FILES} images..."
 	fi
 else
 	echo -en "${ME}: ${YELLOW}Not regenerating sequence because KEEP_SEQUENCE was given and $NSEQ links are present ${NC}\n"
@@ -86,69 +89,43 @@ fi
 
 SCALE=""
 TIMELAPSEWIDTH=${TIMELAPSEWIDTH:-0}
-
+TIMELAPSEHEIGHT=${TIMELAPSEHEIGHT:-0}
 if [ "${TIMELAPSEWIDTH}" != 0 ]; then
-    SCALE="-filter:v scale=${TIMELAPSEWIDTH:0}:${TIMELAPSEHEIGHT:0}"
-    echo "$ME: Using video scale ${TIMELAPSEWIDTH} * ${TIMELAPSEHEIGHT}"
+    SCALE="-filter:v scale=${TIMELAPSEWIDTH}:${TIMELAPSEHEIGHT}"
+    echo "$ME: Using video scale ${TIMELAPSEWIDTH}x${TIMELAPSEHEIGHT}"
 fi
 
 # "-loglevel warning" gets rid of the dozens of lines of garbage output
 # but doesn't get rid of "deprecated pixel format" message when -pix_ftm is "yuv420p".
-# set FFLOG=info in config.sh if you want to see what's going on for debugging
+# set FFLOG=info in config.sh if you want to see what's going on for debugging.
+OUTPUT_FILE="${DATE_DIR}/allsky-${DATE}.mp4"
 ffmpeg -y -f image2 \
 	-loglevel ${FFLOG:-warning} \
 	-r ${FPS:-25} \
-	-i $DIR/sequence/%04d.$EXTENSION \
+	-i ${SEQUENCE_DIR}/%04d.${EXTENSION} \
 	-vcodec ${VCODEC:-libx264} \
 	-b:v ${TIMELAPSE_BITRATE:-2000k} \
-	-pix_fmt yuv420p \
+	-pix_fmt ${PIX_FMT:-yuv420p} \
 	-movflags +faststart \
 	$SCALE \
-	$DIR/allsky-$DAY.mp4
+	${TIMELAPSE_PARAMETERS} \
+	${OUTPUT_FILE}
 RET=$?
 if [ $RET -ne 0 ]; then
 	echo -e "\n${RED}*** $ME: ERROR: ffmpeg failed with RET=$RET"
-	echo "Links in '$DIR/sequence' left for debugging."
+	echo "Links in '${SEQUENCE_DIR}' left for debugging."
 	echo -e "Remove them when the problem is fixed.${NC}\n"
 	exit 1
 fi
 
-if [ "$UPLOAD_VIDEO" = true ] ; then
-        if [[ "$PROTOCOL" == "S3" ]] ; then
-                $AWS_CLI_DIR/aws s3 cp $DIR/allsky-$DAY.mp4 s3://$S3_BUCKET$MP4DIR --acl $S3_ACL &
-	elif [[ $PROTOCOL == "local" ]] ; then
-                cp $DIR/allsky-$DAY.mp4 $MP4DIR &
-        else
-		# This sometimes fails with "mv: Access failed: 550 The process cannot access the file because it is being used by another process.  (cp)".
-		# xxxx Could it be because the web server is trying to access the file at the same time?
-		# If so, should we wait a few seconds and try lftp again?
-		# This probably isn't an issue with .jpg files since they are much smaller and the window for
-		# simultaneous access is much smaller.
-
-		# "put" to a temp name, then move the temp name to the final name.
-		# This is because slow uplinks can cause multiple lftp requests to be running at once,
-		# and only one lftp can upload the file at once, otherwise get this error:
-		# put: Access failed: 550 The process cannot access the file because it is being used by another process.  (image.jpg)
-		# Slow uploads also cause a problem with web pages that try to read the file as it's being uploaded.
-		# "tl" = "time lapse" - use a unique temporary name
-		TEMP_NAME="tl-$RANDOM"
-		(
-			FILE="allsky-$DAY.mp4"
-			FULL_FILE="/$DIR/$FILE"
-			lftp "$PROTOCOL://$USER:$PASSWORD@$HOST:$MP4DIR" -e "set net:max-retries 1; put "$FULL_FILE" -o $TEMP_NAME; rm -f "$FILE"; mv $TEMP_NAME "$FILE"; bye"
-			RET=$?
-			if [ $RET -ne 0 ]; then
-				echo "${RED}*** $ME: ERROR: lftp failed with RET=$RET on '$FULL_FILE'${NC}"
-		       fi
-		) &
-        fi
-fi
-
 if [ -z "$KEEP_SEQUENCE" ] ; then
-	echo -en "${ME}: ${GREEN}Deleting sequence${NC}\n"
 	rm -rf $DIR/sequence
 else
 	echo -en "${ME}: ${GREEN}Keeping sequence${NC}\n"
 fi
 
 echo -en "${ME}: ${GREEN}Timelapse was created${NC}\n"
+
+# timelapse is uploaded via endOfNight.sh, which called us.
+
+exit 0

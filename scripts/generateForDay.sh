192b1a5b77c6ef18862026259807a467f2cf69bc
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-09-29 19:52:33 -0500

    Centralize upload

--- scripts/generateForDay.sh
+++ scripts/generateForDay.sh
@@ -1,41 +1,168 @@
 #!/bin/bash
-# This script allows users to manually generate keograms,startrails, and timelapses,
-# outside of the ones generated automatically.
 
-source $ALLSKY_HOME/config.sh
-source $ALLSKY_HOME/scripts/filename.sh
+# This script allows users to manually generate or upload keograms,startrails, and timelapses.
 
-cd  $ALLSKY_HOME/scripts
+ME="$(basename "${BASH_ARGV0}")"
 
-ME="$(basename "$BASH_ARGV0")"	# Include script name in output so it's easier to find in the log file
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
-if [ $# -lt 1 ]
-  then
-    echo -en "${ME}: ${RED}You need to pass a day argument\n"
-        echo -en "    ex: ${ME} 20180119${NC}\n"
-        exit 3
+# Allow this script to be executed manually, which requires several variables to be set.
+if [ -z "${ALLSKY_HOME}" ] ; then
+	export ALLSKY_HOME=$(realpath $(dirname "${BASH_ARGV0}")/..)
 fi
 
-DATE=$1
-DIR="$ALLSKY_HOME/images/$DATE"
+# If the first argument is "--upload" then we upload files, otherwise we create them.
+if [ "$1" = "--upload" ]; then
+	shift
+	TYPE="UPLOAD"
+	MSG1="upload"
+	MSG2="uploaded"
+else
+	TYPE="GENERATE"
+	MSG1="create"
+	MSG2="created"
+fi
 
-# Generate timelapse from collected images
-echo -e "${ME}: Generating Keogram\n"
-mkdir -p ${DIR}/keogram/
-../keogram ${DIR}/ $EXTENSION ${DIR}/keogram/keogram-$DATE.jpg
-echo -e "\n"
+source "${ALLSKY_HOME}/variables.sh"
+source "${ALLSKY_CONFIG}/config.sh"
+source "${ALLSKY_SCRIPTS}/filename.sh"
+[ "${TYPE}" = "UPLOAD" ] && source "${ALLSKY_SCRIPTS}/ftp-settings.sh"
 
-# Generate startrails from collected images. Treshold set to 0.1 by default in config.sh to avoid stacking over-exposed images
-echo -e "${ME}: Generating Startrails\n"
-mkdir -p ${DIR}/startrails
-../startrails ${DIR}/ $EXTENSION $BRIGHTNESS_THRESHOLD ${DIR}/startrails/startrails-$DATE.jpg
-echo -e "\n"
 
-# Generate timelapse from collected images
-echo -e "${ME}: Generating Timelapse\n"
-./timelapse.sh $DATE
-echo -e "\n"
+if [ $# -eq 0 -o "${1}" = "-h" -o "${1}" = "--help" ]; then
+	echo -en "${RED}"
+	echo -e "Usage: ${ME} [-k] [-s] [-t] DATE"
+	echo -e "    where:"
+	echo -e "      'DATE' is the day in '${ALLSKY_IMAGES}' to process"
+	echo -e "      'k' is to ${MSG1} a keogram"
+	echo -e "      's' is to ${MSG1} a startrail"
+	echo -e "      't' is to ${MSG1} a timelapse"
+	echo -e "    If you don't specify k, s, or t, all three will be ${MSG2}."
+	echo -e "${NC}"
+	exit 0
+fi
+
+if [ $# -gt 1 ] ; then
+	DO_K="false"
+	DO_S="false"
+	DO_T="false"
+	while [ $# -gt 1 ]
+	do
+		if [ "${1}" = "-k" ] ; then
+			DO_K="true"
+		elif [ "${1}" = "-s" ] ; then
+			DO_S="true"
+		elif [ "${1}" = "-t" ] ; then
+			DO_T="true"
+		else
+			echo "Unknown image type: '${1}'; ignoring."
+		fi
+		shift
+	done
+else
+	DO_K="true"
+	DO_S="true"
+	DO_T="true"
+fi
+
+DATE="${1}"
+DATE_DIR="${ALLSKY_IMAGES}/${DATE}"
+if [ ! -d "${DATE_DIR}" ] ; then
+	echo -e "${ME}: ${RED}ERROR: '${DATE_DIR}' not found!${NC}"
+	exit 2
+fi
+
+if [ "${DO_K}" = "true" ] ; then
+	KEOGRAM_FILE="keogram-${DATE}.${EXTENSION}"
+	UPLOAD_FILE="${DATE_DIR}/keogram/${KEOGRAM_FILE}"
+	if [ "${TYPE}" = "GENERATE" ]; then
+		# Don't include ${ME} since this script is always executed manually
+		echo -e "===== Generating Keogram"
+		mkdir -p "${DATE_DIR}/keogram"
+
+		# The keogram command outputs one line for each of the many hundreds of files,
+		# and this adds needless clutter to the output, so send output to a tmp file so we
+		# can output the number of images.
+		TMP="${ALLSKY_TMP}/keogramTMP.txt"
+		"${ALLSKY_HOME}/keogram" -d "${DATE_DIR}" -e ${EXTENSION} -o "${UPLOAD_FILE}" ${KEOGRAM_PARAMETERS} > "${TMP}"
+		[ $? -eq 0 ] && echo -e "Completed"
+	else
+		if [ -s "${UPLOAD_FILE}" ]; then
+			# If the user specified a different name for the destination file, use it.
+			if [ "${KEOGRAM_DESTINATION_NAME}" != "" ]; then
+				KEOGRAM_FILE="${KEOGRAM_DESTINATION_NAME}"
+			fi
+			echo "Uploading ${KEOGRAM_FILE}"
+			# KG == KeoGram.   "--silent" is for silent mode
+			"${ALLSKY_SCRIPTS}/upload.sh" --silent "${UPLOAD_FILE}" "${KEOGRAM_DIR}" "${KEOGRAM_FILE}" "KG"
+			[ $? -eq 0 ] && echo "${KEOGRAM_FILE} uploaded"
+		else
+			echo -en "${YELLOW}"
+			echo -n "ERROR: Keogram file '${UPLOAD_FILE}' not found; skipping"
+			echo -e "${NC}"
+		fi
+	fi
+	echo -e "\n"
+fi
+
+if [ "${DO_S}" = "true" ] ; then
+	STARTRAILS_FILE="startrails-${DATE}.${EXTENSION}"
+	UPLOAD_FILE="${DATE_DIR}/startrails/${STARTRAILS_FILE}"
+	if [ "${TYPE}" = "GENERATE" ]; then
+		echo -e "===== Generating Startrails, threshold=${BRIGHTNESS_THRESHOLD}"
+		mkdir -p "${DATE_DIR}/startrails"
+
+		# The staretrails command outputs one line for each of the many hundreds of files,
+		# and this adds needless clutter to the output, so send output to a tmp file so we
+		# can output the number of images.
+		TMP="${ALLSKY_TMP}/startrailsTMP.txt"
+		"${ALLSKY_HOME}/startrails" -d "${DATE_DIR}/" -e ${EXTENSION} -b "${BRIGHTNESS_THRESHOLD}" -o "${UPLOAD_FILE}" > "${TMP}"
+		[ $? -eq 0 ] && echo -e "Completed"
+	else
+		if [ -s "${UPLOAD_FILE}" ]; then
+			if [ "${STARTRAILS_DESTINATION_NAME}" != "" ]; then
+				STARTRAILS_FILE="${STARTRAILS_DESTINATION_NAME}"
+			fi
+			echo "Uploading ${STARTRAILS_FILE}"
+			# ST = Star Trails
+			"${ALLSKY_SCRIPTS}/upload.sh" --silent "${UPLOAD_FILE}" "${STARTRAILS_DIR}" "${STARTRAILS_FILE}" "ST"
+			[ $? -eq 0 ] && echo "${STARTRAILS_FILE} uploaded"
+		else
+			echo -en "${YELLOW}"
+			echo -n "WARNING: Startrails file '${UPLOAD_FILE}' not found; skipping"
+			echo -e "${NC}"
+		fi
+	fi
+	echo -e "\n"
+fi
+
+if [ "${DO_T}" = "true" ] ; then
+	VIDEOS_FILE="allsky-${DATE}.mp4"
+	UPLOAD_FILE="${DATE_DIR}/${VIDEOS_FILE}"
+	if [ "${TYPE}" = "GENERATE" ]; then
+		echo -e "===== Generating Timelapse"
+		"${ALLSKY_SCRIPTS}/timelapse.sh" ${DATE}	# it creates the necessary directory
+		[ $? -eq 0 ] && echo -e "Completed"
+	else
+		if [ -s "${UPLOAD_FILE}" ]; then
+			if [ "${VIDEOS_DESTINATION_NAME}" != "" ]; then
+				VIDEOS_FILE="${VIDEOS_DESTINATION_NAME}"
+			fi
+			echo "Uploading ${VIDEOS_FILE}"
+			# TL = Time Lapse
+			"${ALLSKY_SCRIPTS}/upload.sh" --silent "${UPLOAD_FILE}" "${VIDEOS_DIR}" "${VIDEOS_FILE}" "TL"
+			[ $? -eq 0 ] && echo "${VIDEOS_FILE} uploaded"
+		else
+			echo -en "${YELLOW}"
+			echo -n "ERROR: Timelapse file '${UPLOAD_FILE}' not found; skipping"
+			echo -e "${NC}"
+		fi
+	fi
+	echo -e "\n"
+fi
+
+if [ "${TYPE}" = "GENERATE" ]; then
+	echo -e "\n================"
+	echo "If you want to upload the file(s) you just created, execute 'uploadForDay.sh ${DATE}"
+	echo "================"
+fi
+
+exit 0

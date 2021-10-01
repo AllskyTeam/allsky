998c07215190391ec633c694d208ae878c9d0d32
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-10-01 18:58:26 -0500

    darkCapture.sh: fix bug
    Fixes #463

--- scripts/darkCapture.sh
+++ scripts/darkCapture.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
 # This file is "source"d into another.
-cd $ALLSKY_HOME
+# "${CURRENT_IMAGE}" is the name of the current image we're working on.
+CURRENT_IMAGE="dark.${EXTENSION}"		# XXXXX in future release this will be set by saveImage.sh
 
-# If we are in darkframe mode, we only save to the dark file
+# ${TEMPERATURE} is passed to us by saveImage.sh, but may be null.
+# If ${TEMPERATURE} is set, use it as the temperature, otherwise read the ${TEMPERATURE_FILE}.
+# If the ${TEMPERATURE_FILE} file doesn't exist, set the temperature to "n/a".
+if [ "${TEMPERATURE}" = "" ]; then
+	TEMPERATURE_FILE="${ALLSKY_HOME}/temperature.txt"
+	if [ -s "${TEMPERATURE_FILE}" ]; then	# -s so we don't use an empty file
+		TEMPERATURE=$(printf "%2.0f" "$(< ${TEMPERATURE_FILE})")
+	else
+		TEMPERATURE="n/a"
+	fi
+fi
+
 DARK_MODE=$(jq -r '.darkframe' "$CAMERA_SETTINGS")
-USE_NOTIFICATION_IMAGES=$(jq -r '.notificationimages' "$CAMERA_SETTINGS")
-
-# If the temperature file doesn't exist, set a default value
-TEMP=20
 if [ "$DARK_MODE" = "1" ] ; then
-
-	TEMP_FILE="temperature.txt"
-	if [ -s "$TEMP_FILE" ]; then		# -s so we don't use an empty file
-		TEMP=$(printf "%.0f" "`cat ${TEMP_FILE}`")
+	DARKS_DIR="${ALLSKY_DARKS}"
+	mkdir -p "${DARKS_DIR}"
+	# If the camera doesn't support temperature, we will keep overwriting the file until
+	#	user creates temperature.txt file.
+	if [ "${TEMPERATURE}" = "n/a" ]; then
+		cp "${CURRENT_IMAGE}" "${DARKS_DIR}/dark.${EXTENSION}"
+	else
+		cp "${CURRENT_IMAGE}" "${DARKS_DIR}/${TEMPERATURE}.${EXTENSION}"
 	fi
 
-        mkdir -p darks
-	# To avoid having the websites display a dark frame, when in dark mode
-	# the image file is different.
-	DARK_FRAME="dark.$EXTENSION"
-        cp "$DARK_FRAME" "darks/$TEMP.$EXTENSION"
-
-	# If the user has notification images on, the current images says we're taking dark frames, so don't overwrite.
-	if [ "$USE_NOTIFICATION_IMAGES" = "0" ] ; then
-		# Go ahead and let the web sites see the dark frame.  Not very interesting though...
-        	cp "$DARK_FRAME" "${IMG_PREFIX}${FILENAME}.${EXTENSION}"
+	# If the user has notification images on, the current image says we're taking dark frames,
+	# so don't overwrite it.
+	# xxxx It's possible some people will want to see the dark frame even if notification images
+	# are being used - may need to make it optional to see the dark frame.
+	USE_NOTIFICATION_IMAGES=$(jq -r '.notificationimages' "${CAMERA_SETTINGS}")
+	if [ "${USE_NOTIFICATION_IMAGES}" = "0" ] ; then
+		# Go ahead and let the web sites see the dark frame to see if it's working.
+		cp "${CURRENT_IMAGE}" "${FULL_FILENAME}"
 	fi
 
-        exit 0	# exit so the calling script exits.
+	exit 0	# exit so the calling script exits.
 fi

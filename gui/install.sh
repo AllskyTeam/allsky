391a678c79b708d3896e680ba38c8106f692cb2f
Michael J. Kidd <linuxkidd@gmail.com>
2021-10-03 22:27:40 -0700

    Fixed up gui/install.sh to use variables.sh

--- gui/install.sh
+++ gui/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+
+source ../config/variables.sh
 
 if [ $# -eq 1 ] ; then
 	HOST_NAME=$1
@@ -11,9 +10,9 @@ fi
 
 
 echo -en '\n'
-echo -e "${RED}****************************************************************"
+echo -e "****************************************************************"
 echo    "*** Welcome to the Allsky Administration Portal installation ***"
-echo -e "****************************************************************${NC}"
+echo -e "****************************************************************"
 echo -en '\n'
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
@@ -25,17 +24,18 @@ apt-get update && apt-get install -y lighttpd php-cgi php-gd hostapd dnsmasq ava
 lighty-enable-mod fastcgi-php
 service lighttpd restart
 echo -en '\n'
+
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo -e "${GREEN}* Configuring lighttpd${NC}"
 sed -i "s|/home/pi/allsky|$(dirname "$SCRIPTPATH")|g" $SCRIPTPATH/lighttpd.conf
-cp $SCRIPTPATH/lighttpd.conf /etc/lighttpd/lighttpd.conf
+install -m 0644 $SCRIPTPATH/lighttpd.conf /etc/lighttpd/lighttpd.conf
 echo -en '\n'
 echo -e "${GREEN}* Changing hostname to allsky${NC}"
 echo "$HOST_NAME" > /etc/hostname
 sed -i "s/raspberrypi/$HOST_NAME/g" /etc/hosts
 echo -en '\n'
 echo -e "${GREEN}* Setting avahi-daemon configuration${NC}"
-cp $SCRIPTPATH/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
+install -m 0644 $SCRIPTPATH/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
 sed -i "s/allsky/$HOST_NAME/g" /etc/avahi/avahi-daemon.conf
 echo -en '\n'
 echo -e "${GREEN}* Adding the right permissions to the web server${NC}"
@@ -47,19 +47,19 @@ echo -en '\n'
 echo -e "${GREEN}* Retrieving github files to build admin portal${NC}"
 rm -rf /var/www/html
 git clone https://github.com/thomasjacquin/allsky-portal.git /var/www/html
-chown -R `logname`:www-data /var/www/html
+chown -R ${SUDO_USER}:www-data /var/www/html
 chmod -R 775 /var/www/html
 mkdir -p /etc/raspap
 mv /var/www/html/raspap.php /etc/raspap/
 mv /var/www/html/camera_options_ZWO.json /etc/raspap/
 mv /var/www/html/camera_options_RPiHQ.json /etc/raspap/
-cp $(dirname "$SCRIPTPATH")/config/settings_ZWO.json /etc/raspap/settings_ZWO.json
-cp $(dirname "$SCRIPTPATH")/config/settings_RPiHQ.json /etc/raspap/settings_RPiHQ.json
+install -m 0644 -o www-data -g www-data ${ALLSKY_CONFIG}/settings_ZWO.json /etc/raspap/
+install -m 0644 -o www-data -g www-data ${ALLSKY_CONFIG}/settings_RPiHQ.json /etc/raspap/
 chown -R www-data:www-data /etc/raspap
-usermod -a -G www-data `logname`
+usermod -a -G www-data $SUDO_USER
 echo -en '\n'
 echo -e "${GREEN}* Modify config.sh${NC}"
-sed -i '/CAMERA_SETTINGS_DIR=/c\CAMERA_SETTINGS_DIR="/etc/raspap"' $(dirname "$SCRIPTPATH")/config/config.sh
+sed -i '/CAMERA_SETTINGS_DIR=/c\CAMERA_SETTINGS_DIR="/etc/raspap"' $ALLSKY_CONFIG/config.sh
 echo -en '\n'
 echo -en '\n'
 echo "The Allsky Portal is now installed"

e49bc82061f3eb8086633ee9d94d95f801f368da
Chris Kuethe <chris.kuethe+github@gmail.com>
2021-10-05 12:11:03 -0700

    A tool to generate notification images
    Based on #565. I changed the camera-off-during-day background color to
    keep it consistent with the others.
    
    Also made the error-see-logfile all use 72pt rather than 128+72pt because
    it was easier to code.

---
+++ notification_images/generate_images.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function make_image() {
+    BASENAME="$1"
+    TEXTCOLOR="$2"
+    STROKE="$3"
+    BGCOLOR="$4"
+    MSG="$5"
+    FS=$6
+    EXTS="JPG PNG"
+
+    echo "$BASENAME" | grep -qEi '[.](${EXTS/ /|})'
+    if [ $? -ne 1 ] ; then
+        echo "run this function with only the image basename"
+        echo "make_image StartingUp '#00ff00' 'Allsky Starting Up'"
+        exit 1
+    fi
+
+	# Hack to make these images more detectable. Typically camera images are
+	# at least an even number of pixels, and usually a multiple of 8 pixels.
+	# So just in case someone has a camera configured for 960x720 images, or
+	# is rescaling to 960x720... this will allow them to be detected.
+    IM_SIZE="959x719"
+    FONT="Arial"
+    FONT_SIZE=${FS:-128}
+
+    for EXT in jpg png ; do
+        convert \
+            -background "$BGCOLOR" \
+            -fill "$TEXTCOLOR" \
+            -strokewidth 2 \
+            -stroke "$STROKE}" \
+            -size $IM_SIZE \
+            -font $FONT \
+            -pointsize $FONT_SIZE \
+            -gravity center \
+            label:"${MSG}" \
+            "${BASENAME}.${EXT}"
+    done
+
+}
+
+#          BaseName           TxtColor  Stroke    BgColor   Message                                                       FontSize
+make_image NotRunning         "#ff0000" "#000000" "#404040" "Allsky software\nis not running"
+make_image DarkFrames         "#00ff00" "#ffffff" "#000000" "Camera is taking\ndark frames"
+make_image StartingUp         "#00ff00" "#000000" "#404040" "Allsky Software\nis starting up"
+make_image CameraOffDuringDay "#ffff4a" "#000000" "#404040" "Camera is off\nduring the day"
+make_image Error              "#ff0000" "#000000" "#404040" "ERROR: See log\nfile for information\n\n/var/log/allsky.log" 72
+

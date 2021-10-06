b30c5b4f5b50f45c27086e638d71920228df002d
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-10-06 15:59:23 -0500

    Implement WEBSITE_DIR

--- website/install.sh
+++ website/install.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+
+if [ -z "${ALLSKY_HOME}" ] ; then
+	export ALLSKY_HOME=$(realpath $(dirname "${BASH_ARGV0}")/..)
+fi
+source ${ALLSKY_HOME}/variables.sh
+
 echo -en '\n'
-echo -e "${RED}*******************************"
+echo -e "*************************************"
 echo    "*** Welcome to the Allsky Website ***"
-echo -e "*************************************${NC}"
+echo -e "*************************************"
 echo -en '\n'
 
 echo -en "${GREEN}* Fetching website files\n${NC}"
-git clone https://github.com/thomasjacquin/allsky-website.git /var/www/html/allsky
+git clone https://github.com/thomasjacquin/allsky-website.git "${WEBSITE_DIR}"
 echo -en '\n'
 
-echo -en "${GREEN}* Create thumbnails directories\n${NC}"
-cd /var/www/html
-mkdir ./allsky/startrails/thumbnails
-mkdir ./allsky/keograms/thumbnails
-mkdir ./allsky/videos/thumbnails
+cd "${WEBSITE_DIR}"
+
+# xxxx Should set some variables in config.js based on settings_*.json and config.sh
+
+echo -en "${GREEN}* Creatig thumbnails directories\n${NC}"
+mkdir startrails/thumbnails
+mkdir keograms/thumbnails
+mkdir videos/thumbnails
 echo -en '\n'
 
 echo -en "${GREEN}* Fixing ownership and permissions\n${NC}"
-cd /var/www/html
-chown -R pi:www-data allsky
-chmod -R 755 allsky
-chmod 0775 ./allsky/startrails/thumbnails
-chmod 0775 ./allsky/keograms/thumbnails
-chmod 0775 ./allsky/videos/thumbnails
+chown -R pi:www-data .
+chmod -R 755 .
+chmod 0775 startrails/thumbnails
+chmod 0775 keograms/thumbnails
+chmod 0775 videos/thumbnails
 echo -en '\n'

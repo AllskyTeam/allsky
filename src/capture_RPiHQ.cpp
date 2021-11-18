6ca1ad5c6979296a6759f025115c16cf23c34e5b
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-11-17 23:28:12 -0600

    capture_RPiHQ.cpp: move US_IN_MS
    Moved it up so it could be used by mode_RPiHQ_mean.cpp

--- src/capture_RPiHQ.cpp
+++ src/capture_RPiHQ.cpp
@@ -20,6 +20,10 @@
 #include <fstream>
 #include <stdarg.h>
 
+#define US_IN_MS 1000                     // microseconds in a millisecond
+#define MS_IN_SEC 1000                    // milliseconds in a second
+#define US_IN_SEC (US_IN_MS * MS_IN_SEC)  // microseconds in a second
+
 // new includes (MEAN)
 #include "include/RPiHQ_raspistill.h"
 #include "include/mode_RPiHQ_mean.h"
@@ -34,10 +38,6 @@ using namespace std;
 #define KMAG "\x1B[35m"
 #define KCYN "\x1B[36m"
 #define KWHT "\x1B[37m"
-
-#define US_IN_MS 1000                     // microseconds in a millisecond
-#define MS_IN_SEC 1000                    // milliseconds in a second
-#define US_IN_SEC (US_IN_MS * MS_IN_SEC)  // microseconds in a second
 
 //-------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------

0434d8bf58a996e43cedb2bee2030f111e87867c
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-11-24 01:14:27 -0600

    mode_RPiHQ_mean.hpp: tidy up
    RPiHQcalcMean() now returns the mean.
    Removed debugLevel since it was redundant with the global debugLevel.
    Clarified some comments.
    Aligned values and comments.

--- src/include/mode_RPiHQ_mean.h
+++ src/include/mode_RPiHQ_mean.h
@@ -9,27 +9,27 @@
 #define MEAN_H
 
 struct modeMeanSetting {
-    bool mode_mean = false;   // activate mode mean
-    bool init = true;   // set some settin before first calculation
-	double ExposureLevelMax = 1; 
-	double ExposureLevelMin = - 1;
-    double mean_value    = 0.3;     // mean value for well exposed images
-    double mean_threshold = 0.01;   // threshold value
-    double shuttersteps = 6.0;      // shuttersteps
-    int historySize = 3;            // use the last images for mean calculation
-    int debugLevel = 0;             // show some debug infos
-    int quickstart = 10;            // Sets the quickstart. Deactivate delay between captures for quickstart times.
-    int ExposureLevel = 1;          // current ExposureLevel 
-    double mean_p0    = 5.0;        // ExposureChange (Steps) = p0 + p1 * diff + (p2*diff)^2
-    double mean_p1    = 20.0;
-    double mean_p2    = 45.0;
-    //int maskHorizon = 0;            //
-    //int longplay = 0;               // make delay between captures 
-    //int brightnessControl = 0;      //
+	bool mode_mean			= false;	// Activate mode mean.  User can change this.
+	bool init				= true;		// Set some settings before first calculation.
+										// This is set to "false" after calculation.
+	double ExposureLevelMin	= - 1;		// Set during first calculation.
+	double ExposureLevelMax	= 1; 		// Set during first calculation.
+	double mean_value		= 0.3;		// mean value for well exposed images
+	double mean_threshold	= 0.01;		// threshold value
+	double shuttersteps		= 6.0;		// shuttersteps
+	int historySize			= 3;		// Use this many last images for mean target calculation
+	int quickstart			= 10;		// Shorten delay between captures for this many images
+										// to help get us to a good exposure quicker.
+	int ExposureLevel		= 1;		// current ExposureLevel 
+	double mean_p0			= 5.0;		// ExposureChange (Steps) = p0 + p1 * diff + (p2*diff)^2
+	double mean_p1			= 20.0;
+	double mean_p2			= 45.0;
+	//int maskHorizon		= 0;
+	//int longplay			= 0;		// make delay between captures 
+	//int brightnessControl	= 0;
 };
 
 
-void RPiHQcalcMean(const char* fileName, int asiExposure, double asiGain, raspistillSetting &currentRaspistillSetting, modeMeanSetting &currentModeMeanSetting);
-//void RPiHQmask(const char* fileName);
+int RPiHQcalcMean(const char* fileName, int asiExposure, double asiGain, raspistillSetting &currentRaspistillSetting, modeMeanSetting &currentModeMeanSetting);
 
 #endif

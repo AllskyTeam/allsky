4b202577542e746da4922d51ddc3e8e80263d5b4
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-12-22 11:11:16 -0600

    mode_RPiHQ_mean.cpp: remove unneeded argument

--- src/mode_RPiHQ_mean.cpp
+++ src/mode_RPiHQ_mean.cpp
@@ -39,7 +39,7 @@ bool fastforward = false;
 
 // https://stackoverflow.com/questions/7765810/is-there-a-way-to-detect-if-an-image-is-blurry
 // https://drive.google.com/file/d/0B6UHr3GQEkQwYnlDY2dKNTdudjg/view?resourcekey=0-a73PvBnc3a2B5wztAV0QaA
-double get_focus_metric(cv::Mat img, modeMeanSetting &currentModeMeanSetting)
+double get_focus_metric(cv::Mat img)
 {
  	cv::Mat lap;
 	cv::Laplacian(img, lap, CV_64F);

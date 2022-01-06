c5acd71bd7a5c74ebe03fd72189a94df9422939a
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-01-06 04:26:07 -0600

    postData.sh: include sunrise time
    adding sunrise time allows website to know if it's daytime or nighttime.

--- scripts/postData.sh
+++ scripts/postData.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# This script uploads a file to a website to tell the website when the user has defined "sunset",
-# which is the end of daytime and beginning of nighttime.
-# Use the angle set by the user.
+# This script uploads a file to a website to tell the website when the user has defined
+# "sunrise" and "sunset".  Use the angle set by the user.
 
 source "${ALLSKY_HOME}/variables.sh"
 source "${ALLSKY_CONFIG}/config.sh"
@@ -11,24 +10,27 @@ source "${ALLSKY_CONFIG}/ftp-settings.sh"
 ME="$(basename "${BASH_ARGV0}")"
 
 angle=$(jq -r '.angle' "${CAMERA_SETTINGS}")
-latitude=$(jq -r '.latitude' "${CAMERA_SETTINGS}")
-longitude=$(jq -r '.longitude' "${CAMERA_SETTINGS}")
+latlong=$(jq -r '.latitude,.longitude' "${CAMERA_SETTINGS}")
 timezone=$(date "+%z")
 
 # If nighttime happens after midnight, sunwait returns "--:-- (Midnight sun)"
 # If nighttime happens before noon, sunwait returns "--:-- (Polar night)"
-time="$(sunwait list set angle ${angle} ${latitude} ${longitude})"
-hhmm=${time:0:5}
+sunrise="$(sunwait list rise angle ${angle} ${latlong})"
+sunrise_hhmm=${sunrise:0:5}
+sunset="$(sunwait list set angle ${angle} ${latlong})"
+sunset_hhmm=${sunset:0:5}
 
-if [ "${hhmm}" = "--:--" ]; then
+if [ "${sunrise_hhmm}" = "--:--" -o "${sunset_hhmm}" = "--:--" ]; then
 	# nighttime starts after midnight or before noon.
 	today=$(date --date='tomorrow' +%Y-%m-%d)		# is actually tomorrow
-	# TODO What SHOULD hhmm be?
-	hhmm="00:00"
+	# TODO What SHOULD *_hhmm be?
+	sunrise_hhmm="00:00"
+	sunset_hhmm="00:00"
 
 	echo "***"
-	echo -e "${RED}${ME}: WARNING: angle ${angle} caused sunwait to return '${time}'.${NC}"
-	echo "Using tomorrow at '${hhmm}' instead."
+	echo -e "${RED}${ME}: WARNING: angle ${angle} caused sunwait to return"
+	echo -e "sunrise='${sunrise}' and sunset='${sunset}'.${NC}"
+	echo "Using tomorrow at '${sunriset_hhmm}' instead."
 	echo "***"
 else
 	today=$(date +%Y-%m-%d)
@@ -38,7 +40,8 @@ FILE="data.json"
 OUTPUT_FILE="${ALLSKY_TMP}/${FILE}"
 (
 	echo {
-	echo \"sunset\": \"${today}T${hhmm}:00.000${timezone}\",
+	echo \"sunrise\": \"${today}T${sunrise_hhmm}:00.000${timezone}\",
+	echo \"sunset\": \"${today}T${sunset_hhmm}:00.000${timezone}\",
 	echo \"streamDaytime\": \"${DAYTIME_CAPTURE}\"
 	echo }
 ) > "${OUTPUT_FILE}"

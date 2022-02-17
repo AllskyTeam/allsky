87b085088d18c28ec583296d36b5d324ec2e557b
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-02-17 13:21:33 -0600

    add include/allsky_common.h
    This goes along with src/allsky_common.cpp

---
+++ src/include/allsky_common.h
@@ -0,0 +1,95 @@
+// Items used by the "capture" and other programs.
+
+// Image formats.  Match the ASI_* settings to make it consistent with ZWO's library.
+#ifndef ASI_IMG_RAW8
+#define ASI_IMG_RAW8	0	// xxxx OLD name
+#define ASI_IMG_RGB24	1	// xxxx OLD name
+#define ASI_IMG_RAW16	2	// xxxx OLD name
+#define ASI_IMG_Y8		3	// xxxx OLD name
+#endif
+#define IMG_RAW8	0
+#define IMG_RGB24	1
+#define IMG_RAW16	2
+#define IMG_Y8		3
+
+#ifndef ASI_TRUE
+#define ASI_TRUE true
+#define ASI_FALSE false
+#endif
+
+// Colors for messages
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
+
+// Times
+#define US_IN_MS 1000						// microseconds in a millisecond
+#define MS_IN_SEC 1000						// milliseconds in a second
+#define US_IN_SEC (US_IN_MS * MS_IN_SEC)	// microseconds in a second
+#define S_IN_MIN 60
+#define S_IN_HOUR (60 * 60)
+#define S_IN_DAY (24 * S_IN_HOUR)
+
+// Defaults
+#define NOT_SET						-1					// signifies something isn't set yet
+#define DEFAULT_NOTIFICATIONIMAGES	1
+#define DEFAULT_SAVEDIR				"tmp"
+#define DEFAULT_FILENAME			"image.jpg"
+#define DEFAULT_TIMEFORMAT			"%Y%m%d %H:%M:%S"	// format to display time in
+#define DEFAULT_LOCALE				"en_US.UTF-8"
+#define AUTO_IMAGE_TYPE				99	// must match what's in the camera_settings.json file
+#define DEFAULT_IMAGE_TYPE			AUTO_IMAGE_TYPE
+#define DEFAULT_DAYTIMECAPTURE		0	// Capture images during the day?
+#define DEFAULT_WIDTH				0
+#define DEFAULT_HEIGHT				0
+#define	DEFAULT_LONGITUDE			NOT_SET
+#define DEFAULT_LATITUDE			NOT_SET
+#define DEFAULT_ANGLE				"-6"
+
+// Default overlay values - will go away once external overlay program is implemented
+#define DEFAULT_FONTNUMBER			0
+#define DEFAULT_ITEXTX				15
+#define DEFAULT_ITEXTY				25
+#define DEFAULT_ITEXTLINEHEIGHT		30
+#define DEFAULT_FONTSIZE			32
+#define SMALLFONTSIZE_MULTIPLIER	0.08
+#define DEFAULT_LINEWIDTH			1
+#define DEFAULT_OUTLINEFONT			0
+#define DEFAULT_LINENUMBER			0
+#define DEFAULT_SHOWTIME			1	// Show the date/time in the overlay?
+
+
+
+// Global variables and functions.
+extern int debugLevel;
+extern char debug_text[];
+extern bool tty;
+extern int notificationImages;
+extern std::string dayOrNight;
+
+void cvText(cv::Mat, const char *, int, int, double,
+	int, int,
+	int, int [], int, bool, int);
+timeval getTimeval();
+char *formatTime(timeval, char const *);
+char *getTime(char const *);
+double time_diff_us(int64, int64);
+long timeval_diff_us(timeval, timeval);
+std::string exec(const char *);
+void add_variables_to_command(char *, long, int, float,
+	bool, bool, bool, float, float,
+	int, int, float,
+	int, int, int);
+void waitToFix(char const *);
+void calculateDayOrNight(const char *, const char *, const char *);
+int calculateTimeToNightTime(const char *, const char *, const char *);
+void Log(int, const char *, ...);
+char const *c(char const *);
+void closeUp(int);
+char const *yesNo(int);
+char *length_in_units(long, bool);

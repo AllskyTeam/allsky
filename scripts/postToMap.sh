748c06c4c23b44f60190d5e330ac1f876c7a0340
Thomas Jacquin <jacquin.thomas@gmail.com>
2022-03-18 18:03:23 -0700

    Removed shellcheck warning on source file

--- scripts/postToMap.sh
+++ scripts/postToMap.sh
@@ -4,6 +4,8 @@
 # https://www.thomasjacquin.com/allsky-map/
 # Information is gathered automatically from the settings file
 
+# Disabling shellcheck to force CI to compile - May need to find a better way to deal with this
+# shellcheck disable=SC2059
 source "${ALLSKY_CONFIG}/config.sh"
 
 LOCATION="Whitehorse YT"

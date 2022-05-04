56c4d21b417fadbabf51f3c229304520f103cfd0
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-05-03 23:28:31 -0500

    install.sh: update list of newest sudoers entry
    "ifconfig" is added in this PR

--- gui/install.sh
+++ gui/install.sh
@@ -87,8 +87,8 @@ if [ "${1}" = "--update" ] || [ "${1}" = "-update" ] ; then
 	# Add entries to sudoers file if not already there.
 	# This is only needed for people who updated allsky-portal but didn't update allsky.
 	# Don't simply copy the "allsky" file to /etc/sudoers.d in case "allsky" isn't up to date.
-	grep --silent "/usr/bin/vcgencmd" /etc/sudoers.d/allsky &&
-	grep --silent "postData.sh" /etc/sudoers.d/allsky
+	grep --silent "postData.sh" /etc/sudoers.d/allsky &&
+	grep --silent "ifconfig" /etc/sudoers.d/allsky
 	# shellcheck disable=SC2181
 	if [ $? -ne 0 ]; then
 		echo -e "${GREEN}* Updating sudoers list${NC}"

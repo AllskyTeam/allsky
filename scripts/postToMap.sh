f12c733b66a13a380ff36d763c7e2d3023f5c0a7
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-05-30 08:18:07 -0500

    postToMap.sh: Add checks
    * Check that URLs can be reached, both URLs are in the same domain, if one URL is specified they both are, and others.
    * Support error messages going to the WebUI when invoked from the WebUI, and to stdout if run manually.

--- scripts/postToMap.sh
+++ scripts/postToMap.sh
@@ -18,18 +18,86 @@ source "${ALLSKY_HOME}/variables.sh"
 # shellcheck disable=SC1090
 source "${ALLSKY_CONFIG}/config.sh"
 
+if [ ${ON_TTY} -eq 1 ]; then
+	RED_START="${RED}"
+	GREEN_START="${GREEN}"
+	YELLOW_START="${YELLOW}"
+	COLOR_END="${NC}"
+	BR=" \n"
+else
+	RED_START="<span style='color: red'>"
+	GREEN_START="<span style='color: green'>"
+	YELLOW_START="<span style='color: yellow'>"
+	COLOR_END="</span>"
+	BR="<br>"
+fi
+
 function usage_and_exit()
 {
 	RET_CODE=${1}
-	[ ${RET_CODE} -ne 0 ] && echo -en "${RED}"
-	echo -e "\nUsage: ${ME} [--help] [--whisper] [--delete] [--force] [--debug]\n"
+	[ ${RET_CODE} -ne 0 ] && echo -en "${RED_START}"
+	echo -e "${BR}Usage: ${ME} [--help] [--whisper] [--delete] [--force] [--debug]${BR}"
 	echo "--help: Print this usage message and exit immediately."
 	echo "--whisper: Be quiet with non-error related output - only display results."
 	echo "--delete: Delete map data; all fields except machine_id are ignored."
 	echo "--force: Force updates, even if not scheduled automatically for today."
 	echo "--debug: Output debugging statements."
-	[ ${RET_CODE} -ne 0 ] && echo -e "${NC}"
+	[ ${RET_CODE} -ne 0 ] && echo -e "${COLOR_END}"
 	exit ${RET_CODE}
+}
+
+function get_domain()
+{
+	# Get the domain name or IP address from a URL
+	# Examples:
+	#	http://myallsky.com						# Return "myallsky.com"
+	#	http://myallsky.com/allsky				# Return "myallsky.com"
+	#	http://123.456.789.000:8080/allsky		# Return "123.456.789.000"
+	URL="${1}"
+	D="${URL/*\/\//}"	# Remove everything up to and including "//"
+	D="${D/[:\/]*/}"	# Remove from ":" or "/" to end
+	echo "${D}"
+}
+
+TIMEOUT=30		# seconds to wait when trying to reach a URL
+
+function check_URL()
+{
+	URL="${1}"
+	URL_TYPE="${2}"
+	FIELD_NAME="${3}"
+
+	D="$(get_domain "${URL}")"
+	if [ "${D:0:7}" = "192.168" -o "${D:0:4}" = "10.0" -o "${D:0:6}" = "172.16" -o "${D:0:9}" = "169.254.0" -o "${D:0:6}" = "198.18" -o "${D:0:10}" = "198.51.100"  -o "${D:0:9}" = "203.0.113" -o "${D:0:3}" = "240" ]; then
+		M="${ERROR_MSG_START}ERROR: '${URL}' is not reachable from the Internet.${ERROR_MSG_END}${BR}${M}"
+	elif [ "${URL:0:5}" != "http:" ] && [ "${URL:0:6}" != "https:" ]; then
+		M="${ERROR_MSG_START}ERROR: 'Website URL' must begin with 'http:' or 'https:'.${ERROR_MSG_END}${BR}${M}"
+	else
+		# Make sure it's a valid URL
+		CONTENT="$(curl --head --silent --show-error --connect-timeout ${TIMEOUT} "${URL}" 2>&1)"
+		RET=$?
+		if [ ${RET} -eq 6 ]; then
+			M="${ERROR_MSG_START}ERROR: '${URL}' not found - check spelling.${ERROR_MSG_END}${BR}${M}"
+		elif [ ${RET} -eq 28 ]; then
+			M="${ERROR_MSG_START}ERROR: Could not connect to '${URL}' after ${TIMEOUT} seconds.${ERROR_MSG_END}${BR}${M}"
+		elif [ ${RET} -ne 0 ]; then
+				M="${ERROR_MSG_START}ERROR: '${URL}' cannot be reached (${CONTENT}).${ERROR_MSG_END}${BR}${M}"
+		else
+			if [ "${URL_TYPE}" = "websiteurl" ]; then
+				TYPE="$(echo "${CONTENT}" | grep -i "Content-Type: text")"
+				T="web site"
+			else
+				TYPE="$(echo "${CONTENT}" | grep -i "Content-Type: image")"
+				T="image"
+			fi
+			if [ -z "${TYPE}" ]; then
+				M="${ERROR_MSG_START}ERROR: ${FIELD_NAME} '${URL}' does not appear to be a valid ${T}.${ERROR_MSG_END}${BR}${M}"
+			else
+				return 0
+			fi
+		fi
+	fi
+	return 1
 }
 
 DEBUG=false
@@ -58,18 +126,16 @@ while [ $# -ne 0 ]; do
 done
 
 if [ "${WHISPER}" = "true" ];then
-		MSG_START=""
-		ERROR_MSG_START="<span style='color: red;'>"
-		ERROR_MSG_END="</span>"
-		WARNING_MSG_START="<span style='color: yellow;'>"
-		WARNING_MSG_END="</span>"
+	MSG_START=""
+	ERROR_MSG_START="${RED_START}"
+	WARNING_MSG_START="${YELLOW_START}"
 else
-		MSG_START="${ME}: "
-		ERROR_MSG_START="${RED}*** ${ME}: "
-		ERROR_MSG_END="${NC}"
-		WARNING_MSG_START="${YELLOW}*** ${ME}: "
-		WARNING_MSG_END="${NC}"
+	MSG_START="${ME}: "
+	ERROR_MSG_START="${RED_START}*** ${ME}: "
+	WARNING_MSG_START="${YELLOW_START}*** ${ME}: "
 fi
+ERROR_MSG_END="${COLOR_END}"
+WARNING_MSG_END="${COLOR_END}"
 
 MACHINE_ID="$(< /etc/machine-id)"
 if [ -z "${MACHINE_ID}" ]; then
@@ -100,46 +166,62 @@ else
 	COMPUTER="$(jq -r '.computer' "${CAMERA_SETTINGS}")"
 
 	OK=true
+	M=""
 	# Check for required fields
 	if [ "${LATITUDE}" = "" ]; then
-		echo -e "${ERROR_MSG_START}ERROR: 'Latitude' is required.${ERROR_MSG_END}"
+		M="${ERROR_MSG_START}ERROR: 'Latitude' is required.${ERROR_MSG_END}${BR}${M}"
 		OK=false
 	fi
 	if [ "${LONGITUDE}" = "" ]; then
-		echo -e "${ERROR_MSG_START}ERROR: 'Longitude' is required.${ERROR_MSG_END}"
+		M="${ERROR_MSG_START}ERROR: 'Longitude' is required.${ERROR_MSG_END}${BR}${M}"
 		OK=false
 	fi
 	if [ "${CAMERA}" = "" ]; then
-		echo -e "${ERROR_MSG_START}ERROR: 'Camera' is required.${ERROR_MSG_END}"
+		M="${ERROR_MSG_START}ERROR: 'Camera' is required.${ERROR_MSG_END}${BR}${M}"
 		OK=false
 	fi
 	if [ "${COMPUTER}" = "" ]; then
-		echo -e "${ERROR_MSG_START}ERROR: 'Computer' is required.${ERROR_MSG_END}"
+		M="${ERROR_MSG_START}ERROR: 'Computer' is required.${ERROR_MSG_END}${BR}${M}"
 		OK=false
 	fi
 
 	# Check for optional, but suggested fields
 	if [ "${LOCATION}" = "" ]; then
-		echo -e "${WARNING_MSG_START}WARNING: 'Location' not set; continuing.${WARNING_MSG_END}"
+		M="${WARNING_MSG_START}WARNING: 'Location' not set; continuing.${WARNING_MSG_END}${BR}${M}"
 	fi
 	if [ "${OWNER}" = "" ]; then
-		echo -e "${WARNING_MSG_START}WARNING: 'Owner' not set; continuing.${WARNING_MSG_END}"
+		M="${WARNING_MSG_START}WARNING: 'Owner' not set; continuing.${WARNING_MSG_END}${BR}${M}"
 	fi
 	if [ "${LENS}" = "" ]; then
-		echo -e "${WARNING_MSG_START}WARNING: 'Lens' not set; continuing.${WARNING_MSG_END}"
+		M="${WARNING_MSG_START}WARNING: 'Lens' not set; continuing.${WARNING_MSG_END}${BR}${M}"
 	fi
 
-	# website_url and image_url are optional, but need to start with "http:" or "https:" if present
-	if [ "${WEBSITE_URL}" != "" ] && [ "${WEBSITE_URL:0:5}" != "http:" ] && [ "${WEBSITE_URL:0:6}" != "https:" ]; then
-		echo -e "${ERROR_MSG_START}ERROR: 'Website URL' must begin with 'http:' or 'https:'.${ERROR_MSG_END}"
+	# website_url and image_url are optional
+
+	if [ -n "${WEBSITE_URL}" -a -z "${IMAGE_URL}" ] || [ -z "${WEBSITE_URL}" -a -n "${IMAGE_URL}" ]; then
+		M="${ERROR_MSG_START}ERROR: If you specify the Website URL or Image URL, you must specify both URLs.${ERROR_MSG_END}${BR}${M}"
 		OK=false
-	fi
-	if [ "${IMAGE_URL}" != "" ] && [ "${IMAGE_URL:0:5}" != "http:" ] && [ "${IMAGE_URL:0:6}" != "https:" ]; then
-		echo -e "${ERROR_MSG_START}ERROR: 'Image URL' must begin with 'http:' or 'https:'.${ERROR_MSG_END}"
-		OK=false
+	elif [ -n "${WEBSITE_URL}" ]; then		# they specified both
+		# The domain names (or IP addresses) must be the same.
+		W="$(get_domain "${WEBSITE_URL}")"
+		I="$(get_domain "${IMAGE_URL}")"
+		if [ "${W}" != "${I}" ]; then
+			M="${ERROR_MSG_START}ERROR: The Website and Image URLs must have the same domain name or IP address.${ERROR_MSG_END}${BR}${M}"
+			OK=false
+		fi
+		if [ -n "${WEBSITE_URL}" ]; then
+			check_URL "${WEBSITE_URL}" websiteurl "Website URL" || OK=false
+		fi
+		if [ -n "${IMAGE_URL}" ]; then
+			check_URL "${IMAGE_URL}" imageurl "Image URL" || OK=false
+		fi
 	fi
 
-	[ "${OK}" = "false" ] && exit 2
+	if [ "${OK}" = "false" ]; then
+		echo -e "${M%%${BR}}"
+		exit 2
+	fi
+
 
 	generate_post_data()
 	{
@@ -180,11 +262,11 @@ if [ "${UPLOAD}" = "true" ]; then
 	CMD="curl --silent -i -H 'Accept: application/json' -H 'Content-Type:application/json'"
 	# shellcheck disable=SC2089
 	CMD="${CMD} --data '$(generate_post_data)' 'https://www.thomasjacquin.com/allsky-map/postToMap.php'"
-	[ "${DEBUG}" = "true" ] && echo -e "\nExecuting:\n${GREEN}${CMD}${NC}\n"
+	[ "${DEBUG}" = "true" ] && echo -e "\nExecuting:\n${GREEN_START}${CMD}${COLOR_END}\n"
 	# shellcheck disable=SC2090
 	RETURN="$(echo ${CMD} | bash)"
 	RETURN_CODE=$?
-	[ "${DEBUG}" = "true" ] && echo -e "\nReturned:\n${YELLOW}${RETURN}${NC}.\n"
+	[ "${DEBUG}" = "true" ] && echo -e "\nReturned:\n${YELLOW_START}${RETURN}${COLOR_END}.\n"
 	if [ ${RETURN_CODE} -ne 0 ]; then
 		echo -e "${ERROR_MSG_START}ERROR while uploading map data with curl: ${RETURN}.${ERROR_MSG_END}"
 		exit ${RETURN_CODE}

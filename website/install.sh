b7ec1835a3b4be414495689458c4de56168f02f4
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-07-26 19:14:30 -0500

    install.sh: many changes
    * Check for running as sudo
    * Restore any prior "myImages" directory.
    * Update message about editing config file.
    * Give more details in "usage" message.
    * Don't bother sending anything to stderr.
    * Minor improvements in comments and messages.
    * Renamed OLD_WEBSET to OLD_WEBSITE_TYPE to better reflect what it is.
    * Fixed missing copy of configuration.json file.
    * Use new ALLSKY_REPO variable.
    * Check for "git" failure.
    * Added spaces in some commands to separate the destination so it's easier to see.

--- website/install.sh
+++ website/install.sh
@@ -4,6 +4,15 @@ if [ -z "${ALLSKY_HOME}" ] ; then
 	export ALLSKY_HOME=$(realpath $(dirname "${BASH_ARGV0}")/..)
 fi
 source "${ALLSKY_HOME}/variables.sh"
+
+if [[ $EUID -eq 0 ]]; then
+	echo -e "\n${RED}**********"
+	echo -e "This script must NOT be run as root, do NOT use 'sudo'."
+	echo -e "**********${NC}\n"
+
+	exit 1
+fi
+
 source "${ALLSKY_CONFIG}/config.sh"
 source "${ALLSKY_CONFIG}/ftp-settings.sh"
 ME="$(basename "${BASH_ARGV0}")"
@@ -22,10 +31,15 @@ usage_and_exit()
 	echo -e "${C}Usage: ${ME} [--help] [--remote] [--branch branch_name] [--update]${NC}"
 	echo
 	echo "'--help' displays this message and exits."
-	echo "The '--remote' option will HELP install the Website on a remote server."
+	echo
+	echo "'--remote' keeps a copy of a remote server's configuration file on the"
+	echo "   Pi where it can be updated in the WebUI and uploaded to the server."
+	echo "   This will have no impact on a local Allsky Website, if installed."
+	echo
 	echo "The '--branch' option should only be used when instructed to by an Allsky developer."
 	echo "  'branch_name' is a valid branch at ${GITHUB_ROOT}/allsky-website."
-	echo "'--update' should only be used when instructed to by the Allsky Website."
+	echo
+	echo "'--update' should only be used when instructed to by an Allsky Website page."
 	echo
 	exit ${RET}
 }
@@ -57,7 +71,7 @@ while [ $# -gt 0 ]; do
 			UPDATE="true"
 			;;
 		*)
-			echo -e "${RED}*** ERROR: Unknown argument: '${ARG}'${NC}" 1>&2
+			echo -e "${RED}*** ERROR: Unknown argument: '${ARG}'${NC}"
 			OK="false"
 			;;
 	esac
@@ -86,12 +100,14 @@ modify_locations() {
 }
 
 
-##### Create a new data.json file and check that it's newest version
+##### Create and upload a new data.json file.
 create_data_json_file() {
 	if [ "${POST_END_OF_NIGHT_DATA}" != "true" ]; then
 		echo -e "${GREEN}* Enabling POST_END_OF_NIGHT_DATA.${NC}"
-		sed -i 's/POST_END_OF_NIGHT_DATA.*/POST_END_OF_NIGHT_DATA="true"/' "${ALLSKY_CONFIG}/config.sh"
+		sed -i 's/^POST_END_OF_NIGHT_DATA.*/POST_END_OF_NIGHT_DATA="true"/' "${ALLSKY_CONFIG}/config.sh"
 	fi
+
+	# Create the file.  postData.sh copies it to ${ALLSKY_WEBSITE}.
 	OUTPUT="$(${ALLSKY_SCRIPTS}/postData.sh 2>&1)"
 	if [[ $? -ne 0 || ! -f ${ALLSKY_WEBSITE}/data.json ]]; then
 		echo -e "${RED}* Unable to create new 'data.json' file:"
@@ -105,11 +121,10 @@ create_data_json_file() {
 ##### Update some of the configuration files and variables
 modify_configuration_variables() {
 	# If the user is updating the website, use the prior config file(s).
-	# NOTE: if we add/delete a setting in the files we'll need to change the code below.
 
 	if [ "${SAVED_OLD}" = "true" ]; then
-		if [ "${OLD_WEBSITE}" = "new" ]; then
-			echo -e "${GREEN}* Retoring prior '${CONFIGURATION_FILE_NAME}' file.${NC}"
+		if [ "${OLD_WEBSITE_TYPE}" = "new" ]; then
+			echo -e "${GREEN}* Restoring prior '${CONFIGURATION_FILE_NAME}'.${NC}"
 			cp "${PRIOR_WEBSITE}/${CONFIGURATION_FILE_NAME}" "${ALLSKY_WEBSITE}"
 		else
 			# User had the older Website - merge old config files into new one.
@@ -119,10 +134,12 @@ if false; then
 			echo -e "${GREEN}* Merging contents of prior 'config.js' and 'virtualsky.json' files into '${CONFIGURATION_FILE_NAME}'.${NC}"
 else
 			echo -e "\b${YELLOW}"
-			echo "* When installation is done you must move the contents of the prior"
+			echo "* When installation is done you must copy the contents of the prior"
 			echo "'${ALLSKY_WEBSITE_OLD}/config.js' and"
 			echo "'${ALLSKY_WEBSITE_OLD}/virtualsky.json' files"
-			echo "into '${CONFIGURATION_FILE_NAME}'."
+			echo "into '${ALLSKY_WEBSITE}/${CONFIGURATION_FILE_NAME}'."
+			echo
+			echo "Check the Wiki for meaning of the options - there are many new ones."
 			echo -e "${NC}\n"
 fi
 		fi
@@ -151,6 +168,7 @@ fi
 ## TODO: hide the "videos", "keograms", and/or "startrails" if not being used.
 		
  
+		cp "${ALLSKY_REPO}/${CONFIGURATION_FILE_NAME}.repo" "${DIR}/${CONFIGURATION_FILE_NAME}"
 		echo -e "${GREEN}* Updating settings in ${DIR}/${CONFIGURATION_FILE_NAME}${NC}"
 
 ### TODO: Not sure capture programs accept either way.
@@ -230,7 +248,7 @@ if [[ ${REMOTE_WEBSITE} == "true" ]]; then
 		echo -e "\n${RED}"
 		echo "* ERROR: The 'REMOTE_HOST' must be set in 'config/ftp-settings.sh'"
 		echo "in order to do a remote website installation."
-		echo "Please set it, the password, and other information, then re-run this installation".
+		echo "Please set it, the password, and other information, then re-run this installation."
 		echo -e "${NC}\n"
 		exit 1
 	fi
@@ -245,88 +263,93 @@ if [[ ${REMOTE_WEBSITE} == "true" ]]; then
 		# Don't know if the user is upgrading and old-style remote website,
 		# or they don't even have a remote website.
 		HAS_PRIOR_REMOTE_SERVER="false"
-		MSG="You can keep a copy of your remote website configuration file on your Pi"
+		MSG="You can keep a copy of your remote website's configuration file on your Pi"
 		MSG="${MSG}\nso you can easily edit it in the WebUI and have it automatically uploaded."
+		MSG="${MSG}\n** This is the recommended way of making changes to the configuration **."
 		MSG="${MSG}\n\nWould you like to do that?"
 		if (whiptail --title "Allsky Website Installer" --yesno "${MSG}" 15 60 3>&1 1>&2 2>&3); then 
-			cp "${ALLSKY_HOME}/config_repo/${CONFIGURATION_FILE_NAME}.repo" "${ALLSKY_CONFIG}/${CONFIGURATION_FILE_NAME}"
+			cp "${ALLSKY_REPO}/${CONFIGURATION_FILE_NAME}.repo" "${ALLSKY_CONFIG}/${CONFIGURATION_FILE_NAME}"
 			echo -e "${GREEN}\n"
 			echo "To edit the remote configuration file, go to the 'Editor' page in the WebUI"
 			echo "and select '${CONFIGURATION_FILE_NAME} (remote Allsky Website)'."
 			echo -e "\n${NC}"
-# XXXXXXXXXXXXXX anything else to do?
 		else
 			echo -e "\n${YELLOW}"
-			echo "You need to manually copy '${ALLSKY_HOME}/config_repo/${CONFIGURATION_FILE_NAME}.repo'"
+			echo "You need to manually copy '${ALLSKY_REPO}/${CONFIGURATION_FILE_NAME}.repo'"
 			echo "to your remote server and rename it to '${CONFIGURATION_FILE_NAME}'."
 			echo -e "${NC}\n"
 		fi
 	fi
 
 	echo -e "\n${GREEN}"
-	echo "*** The Pi portion of the Allsky Website Installation is complete."
+	echo "*** The Pi portion of the Remote Allsky Website Installation is complete."
 	if [ "${HAS_PRIOR_REMOTE_SERVER}" = "true" ]; then
-		echo "Please manually update all the code"
-		echo "on the remote server in the same way you installed it orginally."
+		echo "Please manually update all the files"
+		echo "on the remote server the same way you installed them orginally."
 	else
-		echo "Please manually install all the code"
-		echo "on the remote server in the same way you installed it orginally."
+		echo "Please manually install all the files"
+		echo "on the remote server from ${GITHUB_ROOT}/allsky-website.git."
+### TODO: Can we tell the user how?
+### Is it possible to ask them if they want this script to download the files to the Pi
+### and then ftp the whole directory structure?   Wouldn't that be cool?
 	fi
 	echo -e "${NC}\n"
 	exit 0
 fi
 
 
+#########    Local install
+
+
 # See if they are upgrading the website, and if so, if the prior website was an "old" one.
 # "old" means in the old location and with the old configuration files.
 if [ -d "${ALLSKY_WEBSITE}" ]; then
-	OLD_WEBSITE="new"
+	OLD_WEBSITE_TYPE="new"
 	ALLSKY_WEBSITE_OLD="${ALLSKY_WEBSITE}"
 elif [ -d "/var/www/html/allsky" ]; then
-	OLD_WEBSITE="old"
+	OLD_WEBSITE_TYPE="old"
 	ALLSKY_WEBSITE_OLD="/var/www/html/allsky"
 else
-	OLD_WEBSITE=""
+	OLD_WEBSITE_TYPE=""
 	ALLSKY_WEBSITE_OLD=""
 fi
 
 
 # Check if the user is updating an existing installation.
-# This really only applies if they manually update some files rather than the whole release.
+# This really only applies if they manually update some files rather than the whole release,
+# and ideally would never happen.
 if [ "${UPDATE}" = "true" ] ; then
 	if [ ! -d "${ALLSKY_WEBSITE}" ]; then
-		echo -e "${RED}*** ERROR: --update specified but no existing website found in '${ALLSKY_WEBSITE}'${NC}" 1>&2
-		echo
+		echo -e "\n${RED}"
+		echo "*** ERROR: --update specified but no existing website found at '${ALLSKY_WEBSITE}'."
+		echo -e "${NC}\n"
 		exit 2
 	fi
 
-	[[ ${REMOTE_WEBSITE} == "false" ]] && modify_locations
+	modify_locations
 	create_data_json_file
 
-	echo
-	echo -e "${GREEN}* Update complete${NC}"
-	echo
-	exit 0		# currently nothing else to do for updates
+	echo -e "\n${GREEN}* Update complete${NC}\n"
+	exit 0
 fi
 
 
 if [ -d "${ALLSKY_WEBSITE_OLD}" ]; then
-	# git will fail if the directory already exists
+	# git will fail if the new directory already exists
 	PRIOR_WEBSITE="${ALLSKY_WEBSITE}-OLD"
 	if [ -d "${PRIOR_WEBSITE}" ]; then
-		echo -e "${RED}*** ERROR: A saved copy of a prior Allsky Website already exists.  Exiting.${NC}" 1>&2
-		echo -e "See '${PRIOR_WEBSITE}'." 1>&2
-		echo -e "Can only have one saved prior directory at a time." 1>&2
-		echo -e "Can only have one saved prior directory at a time." 1>&2
-		echo -e "\n${NC}"
+		echo -e "\n${RED}"
+		echo "*** ERROR: A saved copy of a prior Allsky Website already exists."
+		echo "See '${PRIOR_WEBSITE}'."
+		echo "Can only have one saved prior directory at a time.  Exiting."
+		echo -e "${NC}\n"
 		exit 3
 	fi
 
 	echo -e "${GREEN}* Moving prior website to '${PRIOR_WEBSITE}'${NC}"
 	mv "${ALLSKY_WEBSITE}" "${PRIOR_WEBSITE}"
 	if [ $? -ne 0 ]; then
-		echo -e "${RED}*** ERROR: Unable to move prior website.  Exiting.${NC}" 1>&2
-		echo
+		echo -e "\n${RED}*** ERROR: Unable to move prior website.  Exiting.${NC}\n"
 		exit 3
 	fi
 	SAVED_OLD=true
@@ -340,73 +363,84 @@ else
 	B=" from branch ${BRANCH}"
 	BRANCH="-b ${BRANCH}"
 fi
-echo -e "${GREEN}* Fetching website files$B into '${ALLSKY_WEBSITE}'${NC}"
+echo -e "${GREEN}* Fetching new website files$B into '${ALLSKY_WEBSITE}'${NC}"
 git clone ${BRANCH} ${GITHUB_ROOT}/allsky-website.git "${ALLSKY_WEBSITE}"
-[ $? -ne 0 ] && echo -e "\n${RED}*** ERROR: Exiting installation${NC}\n" && exit 4
+if [ $? -ne 0 ]; then
+	echo -e "\n${RED}"
+	echo "*** ERROR: Unable to get Allsky Website files from git."
+	echo -e "${NC}\n"
+	exit 4
+fi
 echo
 
 cd "${ALLSKY_WEBSITE}" || exit 1
 
 echo -e "${GREEN}* Creating thumbnails directories${NC}"
 mkdir -p startrails/thumbnails keograms/thumbnails videos/thumbnails
-echo
 
 echo -e "${GREEN}* Fixing ownership and permissions${NC}"
-U=$(id --name --user)
+U=$(id --name --user)		# User running this script
 chown -R "${U}:www-data" .
 find ./ -type f -exec chmod 644 {} \;
 find ./ -type d -exec chmod 775 {} \;
-echo
 
-[[ ${REMOTE_WEBSITE} == "false" ]] && modify_locations
+modify_locations
 modify_configuration_variables
 create_data_json_file
 
 if [ "${SAVED_OLD}" = "true" ]; then
 	# Each directory will have zero or more images.
+	# Make sure we do NOT mv any .php files.
 
 	if [ -d "${PRIOR_WEBSITE}/videos/thumbnails" ]; then
-		mv "${PRIOR_WEBSITE}/videos/thumbnails" videos
+		mv "${PRIOR_WEBSITE}/videos/thumbnails"   videos
 	fi
 	count=$(ls -1 "${PRIOR_WEBSITE}"/videos/allsky-* 2>/dev/null  | wc -l)
 	if [ "${count}" -ge 1 ]; then
-		echo -e "${GREEN}* Restoring prior videos${NC}"
-		mv "${PRIOR_WEBSITE}"/videos/allsky-* videos
+		echo -e "${GREEN}* Restoring prior videos.${NC}"
+		mv "${PRIOR_WEBSITE}"/videos/allsky-*   videos
 	fi
 
 	if [ -d "${PRIOR_WEBSITE}/keograms/thumbnails" ]; then
-		mv "${PRIOR_WEBSITE}/keograms/thumbnails" keograms
+		mv "${PRIOR_WEBSITE}/keograms/thumbnails"   keograms
 	fi
 	count=$(ls -1 "${PRIOR_WEBSITE}"/keograms/keogram-* 2>/dev/null | wc -l)
 	if [ "${count}" -ge 1 ]; then
-		echo -e "${GREEN}* Restoring prior keograms${NC}"
-		mv "${PRIOR_WEBSITE}"/keograms/keogram-* keograms
+		echo -e "${GREEN}* Restoring prior keograms.${NC}"
+		mv "${PRIOR_WEBSITE}"/keograms/keogram-*   keograms
 	fi
 
 	if [ -d "${PRIOR_WEBSITE}/startrails/thumbnails" ]; then
-		mv "${PRIOR_WEBSITE}/startrails/thumbnails" startrails
+		mv "${PRIOR_WEBSITE}/startrails/thumbnails"   startrails
 	fi
 	count=$(ls -1 "${PRIOR_WEBSITE}"/startrails/startrails-* 2>/dev/null | wc -l)
 	if [ "${count}" -ge 1 ]; then
-		echo -e "${GREEN}* Restoring prior startrails${NC}"
-		mv "${PRIOR_WEBSITE}"/startrails/startrails-* startrails
+		echo -e "${GREEN}* Restoring prior startrails.${NC}"
+		mv "${PRIOR_WEBSITE}"/startrails/startrails-*   startrails
+	fi
+
+	if [ -d "${PRIOR_WEBSITE}/myImages" ]; then
+		echo -e "${GREEN}* Restoring prior 'myImages' directory.${NC}"
+		mv "${PRIOR_WEBSITE/myImages}"   .
 	fi
 fi
 
 echo
-echo -e "${GREEN}* Installation complete${NC}"
+echo -e "${GREEN}***** Installation complete *****${NC}"
 echo
 
 echo    "+++++++++++++++++++++++++++++++++++++"
 if [ "${SAVED_OLD}" = "true" ]; then
 	echo -e "Your prior website is in '${PRIOR_WEBSITE}'."
-	echo    "All your prior videos, keograms, and startrails, as well as prior configuration files"
-	echo    "were MOVED to the updated website".
-	echo -e "\nAfter you are convinced everything is working remove your prior version.\n"
+	echo    "All your prior videos, keograms, and startrails were MOVED to the updated website."
+	echo -e "\nAfter you are convinced everything is working, remove your prior version.\n"
 else
-	echo    "Before using the website you should:"
-	echo -e "   * Edit ${YELLOW}${ALLSKY_WEBSITE}/config.js${NC}"
-	echo -e "   * Look at, and possibly edit ${YELLOW}${ALLSKY_WEBSITE}/virtualsky.json${NC}"
-	echo    "     See ${GITHUB_ROOT}/allsky/wiki/allsky-website-Settings for more information".
+	echo -e "\n${YELLOW}"
+	echo    "Before using the website you must edit its configuration by clicking on"
+	echo	"the "Editor" link in the WebUI, then select the ${CONFIGURATION_FILE_NAME} entry."
+	echo
+	echo	"There are many options, so see ${GITHUB_ROOT}/allsky/wiki/allsky-website-Settings"
+	echo	"for more information".
+	echo -e "${NC}\n"
 fi
 echo

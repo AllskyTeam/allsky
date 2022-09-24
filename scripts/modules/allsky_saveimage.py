5984f4193ea91493b5e7e65a52681c65ad5a2089
alex-developer <alex.greenland@gmail.com>
2022-09-24 17:39:58 +0100

    Module dev

--- scripts/modules/allsky_saveimage.py
+++ scripts/modules/allsky_saveimage.py
@@ -1,58 +1,55 @@
 '''
-allsky_export.py
+allsky_saveimafe.py
 
 Part of allsky postprocess.py modules.
 https://github.com/thomasjacquin/allsky
 
 This module will save the final image after all modules have run
 
-Expected parameters:
-None
 '''
 import allsky_shared as s
 import os 
 import json
 import cv2
+import pathlib
 
 metaData = {
     "name": "Saves the image",
     "description": "Saves the image",
+    "module": "allsky_saveimage",    
     "events": [
         "day",
         "night"
     ] 
 }
 
-def createThumbnail():
-    thumbnailSize = (s.thumbnailWidth,s.thumbnailHeight)
-    s.thumbnail = cv2.resize(s.image,thumbnailSize,interpolation=cv2.INTER_AREA)
-    writeImage(s.thumbnail, s.thumbnailFile)
+def writeImage(image, path, quality):
+    fileExtension = pathlib.Path(path).suffix
 
-def writeImage(image, path):
     result = True
     try:
-        cv2.imwrite(path, image, [int(cv2.IMWRITE_PNG_COMPRESSION),9])
+        if fileExtension == ".jpg":
+            cv2.imwrite(path, image, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
+        else:
+            cv2.imwrite(path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), quality])        
     except:
-        s.log(0, "ERROR: Failed to save image {0}".format(path), exitCode=1)
         result = False
     
     return result
 
 def saveimage(params):
-    result = "Image {0} Saved".format(s.CURRENTIMAGEPATH)    
-    #if s.settings["takeDaytimeImages"] or s.tod == "night":
-    #    if not os.path.exists(s.imageFolder):
-    #        os.makedirs(s.imageFolder)
+    quality = s.getSetting("quality")
+    if quality is not None:
+        quality = int(quality)
+        result = "Image {0} Saved, quality {1}".format(s.CURRENTIMAGEPATH, quality)
 
-    #    if s.createThumbnails:
-    #        if not os.path.exists(s.thumbnailFolder):
-    #            os.makedirs(s.thumbnailFolder)
-    #        createThumbnail()
-
-    #writeImage(s.image, s.websiteImageFile)
-    #writeImage(s.image, s.imageFile)
-    if not writeImage(s.image, s.CURRENTIMAGEPATH):
-        result = "Failed to save {0}".format(s.CURRENTIMAGEPATH) 
-    #os.remove(s.CURRENTIMAGEPATH)
+        if not writeImage(s.image, s.CURRENTIMAGEPATH, quality):
+            result = "Failed to save {0}".format(s.CURRENTIMAGEPATH) 
+            s.log(0, "ERROR: Failed to save image {0}".format(s.CURRENTIMAGEPATH), exitCode=1)
+        else:
+            s.log(1, "INFO: {}".format(result))
+    else:
+        result = "Cannot determine the image quality. Image NOT saved"
+        s.log(0, "ERROR: {}".format(result))
 
     return result

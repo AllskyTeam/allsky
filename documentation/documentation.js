3bda45892a51d8ba8cbdf23527e09234d4eb7d78
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-10-07 21:31:38 -0500

    Update documentation.js

--- documentation/documentation.js
+++ documentation/documentation.js
@@ -1,7 +1,7 @@
 // This file resides on the Allsky Github page with a copy on a person's Pi.
 
 // "onPi" and branch are updated during installation.
-var onPi = false;		// TODO: determine automatically
+var onPi = true;		// TODO: determine automatically
 var branch = "dev";
 
 var preURL;			// What gets prepended to the desired URL.
@@ -27,7 +27,6 @@ function u(id) {
 	var url = me.getAttribute(attributeName);
 	if (! url) return false;
 
-	console.log("going to " + url);
 	url = preURL + url;
 	window.location.href = url;
 

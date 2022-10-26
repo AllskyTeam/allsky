0729ad0d64b53eb6a848d57269e07a5f5c7b454e
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-10-26 03:15:40 -0500

    status_messages.php: remove "."
    $status doesn't exist, so can't use    $status .= xxx
    Also convert spaces to tabs.

--- html/includes/status_messages.php
+++ html/includes/status_messages.php
@@ -1,20 +1,20 @@
 <?php
 class StatusMessages {
-  public $messages = array();
+	public $messages = array();
 
-  public function addMessage($message, $level='success', $dismissable=true) {
-	$status .= "<table class='alert alert-$level' width='100%'><tr><td>$message</td>";
-    if ($dismissable) $status .= '<td class="alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button></td>';
-    $status .= "</tr></table>";
+	public function addMessage($message, $level='success', $dismissable=true) {
+		$status = "<table class='alert alert-$level' width='100%'><tr><td>$message</td>";
+		if ($dismissable) $status .= '<td class="alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button></td>';
+		$status .= "</tr></table>";
 
-    array_push($this->messages, $status);
-  }
+		array_push($this->messages, $status);
+	}
 
-  public function showMessages($clear = true) {
-    foreach($this->messages as $message) {
-      echo $message;
-    }
-    if ( $clear ) $this->messages = array();
-  }
+	public function showMessages($clear = true) {
+		foreach($this->messages as $message) {
+			echo $message;
+		}
+		if ( $clear ) $this->messages = array();
+	}
 }
 ?>

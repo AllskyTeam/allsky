c47fd51375b79ee1982c66156fb6d3d52b4d0a1d
Alex <3010165+Alex-developer@users.noreply.github.com>
2022-11-10 22:05:43 +0000

    Update index.php

--- html/index.php
+++ html/index.php
@@ -50,24 +50,21 @@ include_once('includes/allskySettings.php');
 include_once('includes/days.php');
 include_once('includes/images.php');
 include_once('includes/editor.php');
-include_once('includes/overlay.php');
-include_once('includes/module.php');
-include_once('includes/data.php');
 
 $output = $return = 0;
 if (isset($_GET['page']))
-    $page = $_GET['page'];
+	$page = $_GET['page'];
 else
-    $page = "";
+	$page = "";
 
 if ($useLogin) {
 	session_start();
 	if (empty($_SESSION['csrf_token'])) {
-    	if (function_exists('mcrypt_create_iv')) {
-        	$_SESSION['csrf_token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
-    	} else {
-        	$_SESSION['csrf_token'] = bin2hex(openssl_random_pseudo_bytes(32));
-    	}
+		if (function_exists('mcrypt_create_iv')) {
+			$_SESSION['csrf_token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+		} else {
+			$_SESSION['csrf_token'] = bin2hex(openssl_random_pseudo_bytes(32));
+		}
 	}
 	$csrf_token = $_SESSION['csrf_token'];
 }
@@ -77,178 +74,169 @@ if ($useLogin) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Thomas Jacquin">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="Thomas Jacquin">
 
-    <title>AllSky WebUI</title>
+	<title>AllSky WebUI</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap Core CSS -->
+	<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 	<!-- Make messages look nicer, and align the "x" with the message. -->
 	<style>
 		.x {line-height: 150%;}
 	</style>
-    <!-- MetisMenu CSS -->
-    <link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+	<!-- MetisMenu CSS -->
+	<link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
 
-    <!-- Timeline CSS -->
-    <link href="dist/css/timeline.css" rel="stylesheet">
+	<!-- Timeline CSS -->
+	<link href="dist/css/timeline.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+	<!-- Custom CSS -->
+	<link href="dist/css/sb-admin-2.css" rel="stylesheet">
 
-    <!-- Morris Charts CSS -->
-    <link href="bower_components/morrisjs/morris.css" rel="stylesheet">
+	<!-- Morris Charts CSS -->
+	<link href="bower_components/morrisjs/morris.css" rel="stylesheet">
 
-    <!-- Font Awesome -->
-    <script defer src="js/all.min.js"></script>
+	<!-- Font Awesome -->
+	<script defer src="js/all.min.js"></script>
 
-    <!-- Custom CSS -->
-    <link href="dist/css/custom.css" rel="stylesheet">
+	<!-- Custom CSS -->
+	<link href="dist/css/custom.css" rel="stylesheet">
 
-    <link rel="shortcut icon" type="image/png" href="img/allsky-favicon.png">
+	<link rel="shortcut icon" type="image/png" href="img/allsky-favicon.png">
 
-    <!-- RaspAP JavaScript -->
-    <script src="dist/js/functions.js"></script>
+	<!-- RaspAP JavaScript -->
+	<script src="dist/js/functions.js"></script>
 
-    <!-- jQuery -->
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<!-- jQuery -->
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
+	<!-- Metis Menu Plugin JavaScript -->
+	<script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
-    <script src="js/bigscreen.min.js"></script>
+	<script src="js/bigscreen.min.js"></script>
 
-    <script type="text/javascript">
-        function getImage() {
-            var img = $("<img />").attr('src', '<?php echo $image_name ?>?_ts=' + new Date().getTime())
-                .attr("id", "current")
-                .attr("class", "current")
-                .css("width", "100%")
-                .on('load', function () {
-                    if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-                        console.log('broken image!');
-                        setTimeout(function () {
-                            getImage();
-                        }, 500);
-                    } else {
-                        $("#live_container").empty().append(img);
-                    }
-                });
-        }
+	<script type="text/javascript">
+		function getImage() {
+			var img = $("<img />").attr('src', '<?php echo $image_name ?>?_ts=' + new Date().getTime())
+				.attr("id", "current")
+				.attr("class", "current")
+				.css("width", "100%")
+				.on('load', function () {
+					if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+						console.log('broken image!');
+						setTimeout(function () {
+							getImage();
+						}, 500);
+					} else {
+						$("#live_container").empty().append(img);
+					}
+				});
+		}
 
-        // Inititalize theme to light
-        if (!localStorage.getItem("theme")) {
-            localStorage.setItem("theme", "light")
-        }
+		// Inititalize theme to light
+		if (!localStorage.getItem("theme")) {
+			localStorage.setItem("theme", "light")
+		}
 
-    </script>
+	</script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src="dist/js/sb-admin-2.js"></script>
+	<!-- Custom Theme JavaScript -->
+	<script src="dist/js/sb-admin-2.js"></script>
 
-    <!-- Code Mirror editor -->
-    <link rel="stylesheet" href="lib/codeMirror/codemirror.css">
-    <link rel="stylesheet" href="lib/codeMirror/monokai.min.css">
-    <script type="text/javascript" src="lib/codeMirror/codemirror.js"> </script>
-    <script type="text/javascript" src="lib/codeMirror/shell.js"> </script>
+	<!-- Code Mirror editor -->
+	<link rel="stylesheet" href="lib/codeMirror/codemirror.css">
+	<link rel="stylesheet" href="lib/codeMirror/monokai.min.css">
+	<script type="text/javascript" src="lib/codeMirror/codemirror.js"> </script>
+	<script type="text/javascript" src="lib/codeMirror/shell.js"> </script>
 <?php if (file_exists(ALLSKY_WEBSITE_LOCAL_CONFIG) || file_exists(ALLSKY_WEBSITE_REMOTE_CONFIG)) { ?>
-    <script type="text/javascript" src="lib/codeMirror/javascript.js"> </script>
-    <script type="text/javascript" src="lib/codeMirror/json.js"> </script>
+	<script type="text/javascript" src="lib/codeMirror/javascript.js"> </script>
+	<script type="text/javascript" src="lib/codeMirror/json.js"> </script>
 <?php } ?>
 </head>
 <body>
 
 <div id="wrapper">
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand valign-center" href="index.php">
-                <img src="img/allsky-logo.png" title="Allsky logo">
-                <div class="navbar-title">Web User Interface (WebUI)</div>
+	<!-- Navigation -->
+	<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand valign-center" href="index.php">
+				<img src="img/allsky-logo.png" title="Allsky logo">
+				<div class="navbar-title">Web User Interface (WebUI)</div>
 				<div class="version-title"><?php displayVersions(); ?></div>
-            </a>
-        </div> <!-- /.navbar-header -->
+			</a>
+		</div> <!-- /.navbar-header -->
 
-        <!-- Navigation -->
-        <div class="navbar-default sidebar" role="navigation">
-            <div class="sidebar-nav navbar-collapse">
-                <ul class="nav" id="side-menu">
-                    <li>
-                        <a href="index.php?page=live_view"><i class="fa fa-eye fa-fw"></i> Live View</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=list_days"><i class="fa fa-image fa-fw"></i> Images</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=image_data"><i class="fa fa-tachometer-alt fa-fw"></i> Image Data</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=camera_conf"><i class="fa fa-camera fa-fw"></i> Allsky Settings</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=editor"><i class="fa fa-code fa-fw"></i> Editor</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=overlay"><i class="fa fa-edit fa-fw"></i> Overlay Editor</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=module"><i class="fa fa-bars fa-fw"></i> Module Editor</a>
-                    </li>					
+		<!-- Navigation -->
+		<div class="navbar-default sidebar" role="navigation">
+			<div class="sidebar-nav navbar-collapse">
+				<ul class="nav" id="side-menu">
 					<li>
-                        <a href="index.php?page=LAN_info"><i class="fa fa-network-wired fa-fw"></i> <b>LAN</b> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=WLAN_info"><i class="fa fa-tachometer-alt fa-fw"></i> <b>WLAN</b> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=wpa_conf"><i class="fa fa-wifi fa-fw"></i> Configure Wifi</a>
-                    </li>
-                    <?php if (RASPI_OPENVPN_ENABLED) : ?>
-                        <li>
-                            <a href="index.php?page=openvpn_conf"><i class="fa fa-lock fa-fw"></i> Configure OpenVPN</a>
-                        </li>
-                    <?php endif; ?>
-                    <?php if (RASPI_TORPROXY_ENABLED) : ?>
-                        <li>
-                            <a href="index.php?page=torproxy_conf"><i class="fa fa-eye-slash fa-fw"></i> Configure TOR
-                                proxy</a>
-                        </li>
-                    <?php endif; ?>
-                    <li>
-                        <a href="index.php?page=auth_conf"><i class="fa fa-lock fa-fw"></i> Change Password</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=system_info"><i class="fa fa-cube fa-fw"></i> System</a>
-                    </li>
-                    <li>
-                        <a href="/documentation" target="_blank" title="Opens in new window"><i class="fa fa-book fa-fw"></i> Allsky Documentation <i class="fa fa-external-link-alt fa-fw"></i></a>
-                    </li>
-                    <li>
-                        <span onclick="switchTheme()"><i class="fa fa-moon fa-fw"></i> Light/Dark mode</span>
-                    </li>
+						<a href="index.php?page=live_view"><i class="fa fa-eye fa-fw"></i> Live View</a>
+					</li>
+					<li>
+						<a href="index.php?page=list_days"><i class="fa fa-image fa-fw"></i> Images</a>
+					</li>
+					<li>
+						<a href="index.php?page=camera_conf"><i class="fa fa-camera fa-fw"></i> Allsky Settings</a>
+					</li>
+					<li>
+						<a href="index.php?page=editor"><i class="fa fa-code fa-fw"></i> Editor</a>
+					</li>
+					<li>
+						<a href="index.php?page=LAN_info"><i class="fa fa-network-wired fa-fw"></i> <b>LAN</b> Dashboard</a>
+					</li>
+					<li>
+						<a href="index.php?page=WLAN_info"><i class="fa fa-tachometer-alt fa-fw"></i> <b>WLAN</b> Dashboard</a>
+					</li>
+					<li>
+						<a href="index.php?page=wpa_conf"><i class="fa fa-wifi fa-fw"></i> Configure Wifi</a>
+					</li>
+					<?php if (RASPI_OPENVPN_ENABLED) : ?>
+						<li>
+							<a href="index.php?page=openvpn_conf"><i class="fa fa-lock fa-fw"></i> Configure OpenVPN</a>
+						</li>
+					<?php endif; ?>
+					<?php if (RASPI_TORPROXY_ENABLED) : ?>
+						<li>
+							<a href="index.php?page=torproxy_conf"><i class="fa fa-eye-slash fa-fw"></i> Configure TOR
+								proxy</a>
+						</li>
+					<?php endif; ?>
+					<li>
+						<a href="index.php?page=auth_conf"><i class="fa fa-lock fa-fw"></i> Change Password</a>
+					</li>
+					<li>
+						<a href="index.php?page=system_info"><i class="fa fa-cube fa-fw"></i> System</a>
+					</li>
+					<li>
+						<a href="/documentation" target="_blank" title="Opens in new window"><i class="fa fa-book fa-fw"></i> Allsky Documentation <i class="fa fa-external-link-alt fa-fw"></i></a>
+					</li>
+					<li>
+						<span onclick="switchTheme()"><i class="fa fa-moon fa-fw"></i> Light/Dark mode</span>
+					</li>
 
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.navbar-default -->
-    </nav>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.navbar-default -->
+	</nav>
 
-    <div id="page-wrapper">
-        <div class="row right-panel">
-            <div class="col-lg-12">
-                <?php
+	<div id="page-wrapper">
+		<div class="row right-panel">
+			<div class="col-lg-12">
+				<?php
 				$status = new StatusMessages();
 				if (isset($_GET['clear'])) {
 					exec("sudo rm -f " . ALLSKY_MESSAGES, $result, $retcode);
@@ -280,86 +268,77 @@ if ($useLogin) {
 					echo "</div>";
 				}
 
-                switch ($page) {
-                    case "live_view":
-                        DisplayLiveView("$image_name", $delay, $daydelay, $nightdelay, $darkframe);
-                        break;
-                    case "WLAN_info":
-                        DisplayDashboard_WLAN("wlan0");
-                        break;
-                    case "LAN_info":
-                        DisplayDashboard_LAN("eth0");
-                        break;
-                    case "camera_conf":
-                        DisplayCameraConfig();
-                        break;
-                    case "wpa_conf":
-                        DisplayWPAConfig();
-                        break;
-                    case "auth_conf":
-                        DisplayAuthConfig($config['admin_user'], $config['admin_pass']);
-                        break;
-                    case "system_info":
-                        DisplaySystem();
-                        break;
-                    case "list_days":
-                        ListDays();
-                        break;
-                    case "list_images":
-                        ListImages();
-                        break;
-                    case "list_videos":
-                        // directory, file name prefix, formal name, type of file
+				switch ($page) {
+					case "live_view":
+						DisplayLiveView("$image_name", $delay, $daydelay, $nightdelay, $darkframe);
+						break;
+					case "WLAN_info":
+						DisplayDashboard_WLAN("wlan0");
+						break;
+					case "LAN_info":
+						DisplayDashboard_LAN("eth0");
+						break;
+					case "camera_conf":
+						DisplayCameraConfig();
+						break;
+					case "wpa_conf":
+						DisplayWPAConfig();
+						break;
+					case "auth_conf":
+						DisplayAuthConfig($config['admin_user'], $config['admin_pass']);
+						break;
+					case "system_info":
+						DisplaySystem();
+						break;
+					case "list_days":
+						ListDays();
+						break;
+					case "list_images":
+						ListImages();
+						break;
+					case "list_videos":
+						// directory, file name prefix, formal name, type of file
 						ListFileType("", "allsky", "Timelapse", "video");
-                        break;
-                    case "list_keograms":
-                        // directory, file name prefix, formal name, type of file
+						break;
+					case "list_keograms":
+						// directory, file name prefix, formal name, type of file
 						ListFileType("keogram/", "keogram", "Keogram", "picture");
-                        break;
-                    case "list_startrails":
-                        // directory, file name prefix, formal name, type of file
+						break;
+					case "list_startrails":
+						// directory, file name prefix, formal name, type of file
 						ListFileType("startrails/", "startrails", "Startrails", "picture");
-                        break;
-                    case "editor":
-                        DisplayEditor();
-                        break;
-                    case "overlay":
-                        DisplayOverlay("$image_name");
-                        break;
-                    case "module":
-                        DisplayModule();
-                        break;						
-                    case "image_data":
-                        DisplayImageData();
-                        break;
-                    default:
-                        DisplayLiveView("$image_name", $delay, $daydelay, $nightdelay, $darkframe);
-                }
-                ?>
-            </div>
-        </div>
-    </div><!-- /#page-wrapper -->
+						break;
+					case "editor":
+						DisplayEditor();
+						break;
+					default:
+						DisplayLiveView("$image_name", $delay, $daydelay, $nightdelay, $darkframe);
+				}
+				?>
+			</div>
+		</div>
+	</div><!-- /#page-wrapper -->
 </div><!-- /#wrapper -->
 
 <script type="text/javascript">
-    $("body").attr("class", localStorage.getItem("theme"));
+	$("body").attr("class", localStorage.getItem("theme"));
 
-    function switchTheme() {
-        if (localStorage.getItem("theme") === "light") {
-            localStorage.setItem("theme", "dark");
-        } else {
-            localStorage.setItem("theme", "light");
-        }
-        $("body").attr("class", localStorage.getItem("theme"));
-    }
+	function switchTheme() {
+		if (localStorage.getItem("theme") === "light") {
+			localStorage.setItem("theme", "dark");
+		} else {
+			localStorage.setItem("theme", "light");
+		}
+		$("body").attr("class", localStorage.getItem("theme"));
+	}
 
 	$("#live_container").click(function () {
-            if (BigScreen.enabled) {
-                BigScreen.toggle(this, null, null, null);
-            } else {
-                console.log("Not Supported");
-            }
-        });
+			if (BigScreen.enabled) {
+				BigScreen.toggle(this, null, null, null);
+			} else {
+				console.log("Not Supported");
+			}
+		});
 </script>
 
 </body>

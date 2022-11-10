f6949e7f3ca5f34740d68aa80f5a218e8423b06f
Alex <3010165+Alex-developer@users.noreply.github.com>
2022-11-10 22:07:04 +0000

    Update index.php

--- html/index.php
+++ html/index.php
@@ -50,6 +50,9 @@ include_once('includes/allskySettings.php');
 include_once('includes/days.php');
 include_once('includes/images.php');
 include_once('includes/editor.php');
+include_once('includes/overlay.php');
+include_once('includes/module.php');
+include_once('includes/data.php');
 
 $output = $return = 0;
 if (isset($_GET['page']))
@@ -189,12 +192,21 @@ if ($useLogin) {
 					<li>
 						<a href="index.php?page=list_days"><i class="fa fa-image fa-fw"></i> Images</a>
 					</li>
+                    <li>
+                        <a href="index.php?page=image_data"><i class="fa fa-tachometer-alt fa-fw"></i> Image Data</a>
+                    </li>					
 					<li>
 						<a href="index.php?page=camera_conf"><i class="fa fa-camera fa-fw"></i> Allsky Settings</a>
 					</li>
 					<li>
 						<a href="index.php?page=editor"><i class="fa fa-code fa-fw"></i> Editor</a>
 					</li>
+                    <li>
+                        <a href="index.php?page=overlay"><i class="fa fa-edit fa-fw"></i> Overlay Editor</a>
+                    </li>
+                    <li>
+                        <a href="index.php?page=module"><i class="fa fa-bars fa-fw"></i> Module Editor</a>
+                    </li>					
 					<li>
 						<a href="index.php?page=LAN_info"><i class="fa fa-network-wired fa-fw"></i> <b>LAN</b> Dashboard</a>
 					</li>
@@ -311,6 +323,15 @@ if ($useLogin) {
 					case "editor":
 						DisplayEditor();
 						break;
+                    case "overlay":
+                        DisplayOverlay("$image_name");
+                        break;
+                    case "module":
+                        DisplayModule();
+                        break;						
+                    case "image_data":
+                        DisplayImageData();
+                        break;						
 					default:
 						DisplayLiveView("$image_name", $delay, $daydelay, $nightdelay, $darkframe);
 				}

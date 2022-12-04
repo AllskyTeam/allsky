3aeebd0f85d827f7f524551081d7f22e625d6c4b
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-12-03 22:22:20 -0700

    periodic.sh: shellcheck changes

--- scripts/periodic.sh
+++ scripts/periodic.sh
@@ -4,12 +4,14 @@ if [ -z "${ALLSKY_HOME}" ]; then
 	export ALLSKY_HOME="$(realpath $(dirname $(dirname "${BASH_ARGV0}")))"
 fi
 
+# shellcheck disable=SC1090,SC1091
 source "${ALLSKY_HOME}/variables.sh" || exit 1
+# shellcheck disable=SC1090,SC1091
 source "${ALLSKY_HOME}/config/config.sh"
 
 trap "exit" SIGTERM SIGINT
 
-cd "${ALLSKY_SCRIPTS}"
+cd "${ALLSKY_SCRIPTS}" || exit 1
 
 while :
 do

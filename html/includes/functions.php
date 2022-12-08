c1eae03ccb8fcfe6b752be3020c40ca9dc8d7d07
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-12-08 00:46:01 -0700

    functions.php: use $lastChange

--- html/includes/functions.php
+++ html/includes/functions.php
@@ -24,10 +24,10 @@ if ((include $defs) == false) {
 $status = null;		// Global pointer to status messages
 $image_name=null; $delay=null; $daydelay=null; $nightdelay=null; $darkframe=null; $useLogin=null;
 $temptype = null;
-$configured = false;
+$lastChange = null;
 function initialize_variables() {
 	global $image_name, $delay, $daydelay, $nightdelay;
-	global $darkframe, $useLogin, $temptype, $configured;
+	global $darkframe, $useLogin, $temptype, $lastChange;
 
 	// The Camera Type should be set during the installation, so this "should" never fail...
 	$cam_type = getCameraType();
@@ -49,7 +49,7 @@ function initialize_variables() {
 
 	$settings_str = file_get_contents($settings_file, true);
 	$settings_array = json_decode($settings_str, true);
-	$configured = getVariableOrDefault($settings_array, 'configured', false);
+
 	// $img_dir is an alias in the web server's config that points to where the current image is.
 	// It's the same as ${ALLSKY_TMP} which is the physical path name on the server.
 	$img_dir = get_variable(ALLSKY_CONFIG . '/config.sh', 'IMG_DIR=', 'current/tmp');
@@ -57,6 +57,7 @@ function initialize_variables() {
 	$darkframe = $settings_array['takeDarkFrames'];
 	$useLogin = getVariableOrDefault($settings_array, 'useLogin', true);
 	$temptype = getVariableOrDefault($settings_array, 'temptype', "C");
+	$lastChange = getVariableOrDefault($settings_array, 'lastChange', null);
 
 
 	////////////////// Determine delay between refreshes of the image.

80abe624b52f4cdf3523e19b91698d5e2ce4852a
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2023-01-03 23:24:47 -0600

    postToMap.sh: use variable instead of hard-code

--- scripts/postToMap.sh
+++ scripts/postToMap.sh
@@ -275,7 +275,7 @@ else
 	if [ -f "${ALLSKY_HOME}/version" ]; then
 		ALLSKY_VERSION="$(< "${ALLSKY_HOME}/version")"
 	else
-		ALLSKY_VERSION="$(grep "Allsky Camera Software" /var/log/allsky.log | tail -1 | sed -e 's/.*Software //' -e 's/ .*//')"
+		ALLSKY_VERSION="$(grep "Allsky Camera Software" "${ALLSKY_LOG}" | tail -1 | sed -e 's/.*Software //' -e 's/ .*//')"
 		[ -z "${ALLSKY_VERSION}" ] && ALLSKY_VERSION="unknown"
 	fi
 

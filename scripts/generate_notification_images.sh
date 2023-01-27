d196b2ffc6d7518bfb1b685c89e6fe06fee92d69
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2023-01-27 03:20:54 -0600

    Update generate_notification_images.sh

--- scripts/generate_notification_images.sh
+++ scripts/generate_notification_images.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 # Create notification image(s).
 # The default ones are listed in this file, but
@@ -7,7 +6,7 @@
 # This is quick - on a Pi 4 it takes about one second per image.
 
 # Allow this script to be executed manually, which requires several variables to be set.
-if [ -z "${ALLSKY_HOME}" ] ; then
+if [[ -z ${ALLSKY_HOME} ]]; then
 	ALLSKY_HOME="$(realpath "$(dirname "${BASH_ARGV0}")/..")"
 	export ALLSKY_HOME
 fi
@@ -20,12 +19,13 @@ function usage_and_exit
 {
 	RET=${1}
 	(
-		[ ${RET} -ne 0 ] && echo -en "${RED}"
+		[[ ${RET} -ne 0 ]] && echo -en "${RED}"
 		echo -e "\nUsage: ${ME} [--help] [--directory dir] [type TextColor Font FontSize StrokeColor StrokeWidth BgColor BorderWidth BorderColor Extensions ImageSize 'Message']\n"
-		[ ${RET} -ne 0 ] && echo -en "${NC}"
+		[[ ${RET} -ne 0 ]] && echo -en "${NC}"
 		echo "When run with no arguments, all notification types are created with extensions: ${ALL_EXTS}."
 		echo "'--directory dir' creates the file(s) in that directory, otherwise in \${PWD}."
 	) >&2
+	# shellcheck disable=SC2086
 	exit $RET
 }
 
@@ -49,18 +49,18 @@ function make_image() {
 	MSG="${12}"
 
 	echo "${BASENAME}" | grep -qEi "[.](${ALL_EXTS/ /|})"
-	if [ $? -ne 1 ] ; then
+	if [[ $? -ne 1 ]]; then
 		echo -e "${RED}${ME}: ERROR: Do not add an extension to the basename${NC}."
 		usage_and_exit 1
 	fi
 
-	if [ ${BORDER_WIDTH} -ne 0 ]; then
+	if [[ ${BORDER_WIDTH} -ne 0 ]]; then
 		BORDER="-border ${BORDER_WIDTH} -bordercolor ${BORDER_COLOR}"
 	else
 		BORDER=""
 	fi
 
-	[ "${ON_TTY}" = "1" ] && echo "Creating '${BASENAME}' in ${PWD}."
+	[[ ${ON_TTY} -eq 1 ]] && echo "Creating '${BASENAME}' in ${PWD}."
 	for EXT in ${EXTS} ; do
 		# Make highest quality for jpg and highest loss-less compression for png.
 		# jpg files at 95% produce somewhat bad artifacts.  Even 100% produces some artifacts.
@@ -84,7 +84,7 @@ function make_image() {
 }
 
 which mogrify > /dev/null
-if [ $? -ne 0 ] ; then
+if [[ $? -ne 0 ]]; then
 	# Testing for mogrify which seems like a much more distinctive executable
 	# name than "convert". I assume that if "mogrify" is in the path, then
 	# ImageMagick is installed and "convert" will run ImageMagick and not some
@@ -94,14 +94,14 @@ if [ $? -ne 0 ] ; then
 fi
 
 # TODO: use getopt
-[ "${1}" = "--help" ] && usage_and_exit 0
+[[ ${1} == "--help" ]] && usage_and_exit 0
 
 # Optional argument specifying where to create the image(s).
 # If not specified, create in current directory.
-if [ "${1}" = "--directory" ]; then
+if [[ ${1} == "--directory" ]]; then
 	DIRECTORY="${2}"
-	[ -z "${DIRECTORY}" ] && usage_and_exit 2
-	if [ ! -d "${DIRECTORY}" ]; then
+	[[ -z ${DIRECTORY} ]] && usage_and_exit 2
+	if [[ ! -d ${DIRECTORY} ]]; then
 		echo -e "\n${RED}*** ${ME} ERROR: Directory '${DIRECTORY}' not found!\n${NC}" >&2
 		exit 2
 	fi
@@ -110,14 +110,14 @@ if [ "${1}" = "--directory" ]; then
 fi
 
 # If the arguments were specified on the command line, use them instead of the list below.
-if [ $# -eq 12 ]; then
-	if [ "${1}" = "" -o "${12}" = "" ]; then
+if [[ $# -eq 12 ]]; then
+	if [[ -z ${1} || -z ${12} ]]; then
 		echo -e '${RED}${ME}: ERROR: Basename ($1) and message ($12) must be specified.${NC}' >&2
 		usage_and_exit 1
 	fi
 	make_image "${@}"
 
-elif [ $# -eq 0 ]; then
+elif [[ $# -eq 0 ]]; then
 #            #1                   #2         #3                 #4     #5        #6      #7         #8      #9        #10         #11       #12
 #            Basename             Text       Font               Font   Stroke    Stroke  Background Border  Border    Extensions  Image     Message
 #                                 Color      Name               Size   Color     Width   Color      Width   Color                 Size

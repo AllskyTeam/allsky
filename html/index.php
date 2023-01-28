3d0f0d411be0b393114a4138f70aa4725c3882dd
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2023-01-28 04:27:10 -0600

    Update index.php

--- html/index.php
+++ html/index.php
@@ -211,14 +211,14 @@ if (file_exists(ALLSKY_WEBSITE_REMOTE_CONFIG)) {
 					echo "<span class='nowrap'>";
 					echo "<a class='version-title-color' href='allsky/index.php' target='_blank' title='Click to go to local Website'>";
 					echo "Local Website: $localWebsiteVersion";
-					echo "</a><i class='fa fa-external-link-alt fa-fw'></i></span>";
+					echo " <i class='fa fa-external-link-alt fa-fw'></i></span></a>";
 } ?>
 					&nbsp; &nbsp;
 <?php if ($remoteWebsiteVersion !== "") {
 					echo "<span class='nowrap'>";
 					echo "<a class='version-title-color' href='$websiteURL' target='_blank' title='Click to go to remote Website'>";
 					echo "Remote Website: $remoteWebsiteVersion";
-					echo "</a><i class='fa fa-external-link-alt fa-fw'></i></span>";
+					echo " <i class='fa fa-external-link-alt fa-fw'></i></span></a>";
 } ?>
 				</div>
 		</div> <!-- /.navbar-header -->
@@ -425,3 +425,4 @@ if (file_exists(ALLSKY_WEBSITE_REMOTE_CONFIG)) {
 
 </body>
 </html>
+

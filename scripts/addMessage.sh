2e2505e72224818658f2818101037d5498ae959d
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2023-02-11 20:21:41 -0600

    Update addMessage.sh

--- scripts/addMessage.sh
+++ scripts/addMessage.sh
@@ -5,15 +5,13 @@
 ME="$(basename "${BASH_ARGV0}")"
 
 # Allow this script to be executed manually, which requires several variables to be set.
-if [[ -z ${ALLSKY_HOME} ]]; then
-	ALLSKY_HOME="$(realpath "$(dirname "${BASH_ARGV0}")/..")"
-	export ALLSKY_HOME
-fi
-source "${ALLSKY_HOME}/variables.sh"
+[[ -z ${ALLSKY_HOME} ]] && export ALLSKY_HOME="$(realpath "$(dirname "${BASH_ARGV0}")/..")"
+source "${ALLSKY_HOME}/variables.sh" || exit 99
 
 if [ $# -ne 2 ]; then
 	# shellcheck disable=SC2154
 	echo -e "${wERROR}Usage: ${ME} message_type message${wNC}" >&2
+	echo -e "\nWhere 'message_type' is one of 'error', 'warning', or 'debug'." >&2
 	exit 1
 fi
 

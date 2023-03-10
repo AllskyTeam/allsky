cb4bf676dca7dd0ad8a99c254914040ba4bac39a
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2023-03-10 00:28:36 -0600

    admin.php: more error messages
    * Add error messages when data isn't supplied.
    * Use $status->isMessage().
    * Set form field values to variables sine they are used in multiple places.

--- html/includes/admin.php
+++ html/includes/admin.php
@@ -2,41 +2,52 @@
 
 include_once( 'includes/status_messages.php' );
 
-function DisplayAuthConfig($username, $password){
+function DisplayAuthConfig($username, $password) {
   global $page;
   $status = new StatusMessages();
   if (isset($_POST['UpdateAdminPassword'])) {
-    if (CSRFValidate()) {
-      if (password_verify($_POST['oldpass'], $password)) {
-        $new_username=trim($_POST['username']);
-        if ($_POST['newpass'] != $_POST['newpassagain']) {
-          $status->addMessage('New passwords do not match', 'danger');
+	// Update the password
+	if (CSRFValidate()) {
+      $new_username=trim($_POST['username']);
+	  $old = $_POST['oldpass'];
+	  $new1 = $_POST['newpass'];
+	  $new2 = $_POST['newpassagain'];
+      if ($new_username == "") {
+          $status->addMessage('You must enter the username.', 'danger');
+	  }
+      if ($old == "" || $new1 == "" || $new2 == "") {
+          $status->addMessage('You must enter the old (current) password, and the new password twice.', 'danger');
+	  } else if (password_verify($old, $password)) {
+        if ($new1 != $new2) {
+          $status->addMessage('New passwords do not match.', 'danger');
         } else if ($new_username == '') {
-          $status->addMessage('Username must not be empty', 'danger');
+          $status->addMessage('Username must not be empty.', 'danger');
         } else {
-          $contents = $new_username.PHP_EOL . password_hash($_POST['newpass'], PASSWORD_BCRYPT).PHP_EOL;
+          $contents = $new_username.PHP_EOL;
+          $contents .= password_hash($new1, PASSWORD_BCRYPT).PHP_EOL;
           $ret = updateFile(RASPI_ADMIN_DETAILS, $contents, "admin password file", true);
           if ($ret === "") {
             $username = $new_username;
-            $status->addMessage('Admin password updated');
+            $status->addMessage("$new_username password updated.", 'success');
           } else {
             $status->addMessage($ret, 'danger');
           }
         }
       } else {
-        $status->addMessage('Old password does not match', 'danger');
+        $status->addMessage('Old password does not match.', 'danger');
       }
-    } else {
-      error_log('CSRF violation');
-    }
+	} else {
+      error_log('CSRF violation');		// TODO: need better message
+	}
   }
 ?>
   <div class="row">
     <div class="col-lg-12">
       <div class="panel panel-primary">
-        <div class="panel-heading"><i class="fa fa-lock fa-fw"></i> Change Password</div>
+        <div class="panel-heading"><i class="fa fa-lock fa-fw"></i> Change Admin Username and/or Password</div>
         <div class="panel-body">
-          <p><?php $status->showMessages(); ?></p>
+		  <?php if ($status->isMessage()) echo "<p>" . $status->showMessages() . "</p>"; ?>
+
           <form role="form" action="?page=<?php echo $page ?>" method="POST">
             <?php CSRFToken() ?>
             <div class="row">
@@ -71,5 +82,4 @@ function DisplayAuthConfig($username, $password){
   </div><!-- /.row -->
 <?php 
 }
-
 ?>

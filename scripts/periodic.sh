ee9f016848b2dadc935878a6933797b1cd5c6ad4
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2023-03-26 03:59:44 -0500

    periodic.sh: add source functions.sh

--- scripts/periodic.sh
+++ scripts/periodic.sh
@@ -3,9 +3,11 @@
 [[ -z "${ALLSKY_HOME}" ]] && export ALLSKY_HOME="$(realpath "$(dirname "${BASH_ARGV0}")/..")"
 
 # shellcheck disable=SC1090,SC1091
-source "${ALLSKY_HOME}/variables.sh"		|| exit 99
+source "${ALLSKY_HOME}/variables.sh"		|| exit ${ALLSKY_ERROR_STOP}
+#shellcheck disable=SC2086 source-path=scripts
+source "${ALLSKY_SCRIPTS}/functions.sh"		|| exit ${ALLSKY_ERROR_STOP}
 # shellcheck disable=SC1090,SC1091
-source "${ALLSKY_HOME}/config/config.sh"	|| exit 99
+source "${ALLSKY_HOME}/config/config.sh"	|| exit ${ALLSKY_ERROR_STOP}
 
 trap "exit 0" SIGTERM SIGINT
 

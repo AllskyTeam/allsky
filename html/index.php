5e93d5dc4800571c5be5ffe346b5c65d4ca820a0
Lucas Isaias Campos <lucasic@gmail.com>
2023-05-19 20:13:53 +0200

    gui improvements
    * Add back to Top button on AllSky Settings page
    * Add sticky class to buttons on AllSky Settings page

--- html/index.php
+++ html/index.php
@@ -171,7 +171,7 @@ if (file_exists($f)) {
 
 	<!-- Custom CSS -->
 	<link href="documentation/css/custom.css" rel="stylesheet">
-
+        <link href="css/general.css" rel="stylesheet">
 	<link rel="shortcut icon" type="image/png" href="documentation/img/allsky-favicon.png">
 
 	<!-- RaspAP JavaScript -->
@@ -492,6 +492,26 @@ if (file_exists($f)) {
 	addTimestamp("auth_conf");
 	addTimestamp("system");
 </script>
+<script type="text/javascript">
+// Get the button:
+let mybutton = document.getElementById("backToTopBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+</script>
 </body>
 </html>

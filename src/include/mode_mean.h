e5dfb9fb6507ea8c3298472b1b34d3e66f05b11f
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-05-22 10:07:01 -0500

    Update mode_mean.h: fix indentation to look nicer

--- src/include/mode_mean.h
+++ src/include/mode_mean.h
@@ -32,32 +32,35 @@ typedef enum {
 } MEAN_AUTO_MODE;
 
 struct modeMeanSetting {
-	bool modeMean = false;					// Activate mode mean.  User can change this.
-	MEAN_AUTO_MODE meanAuto = MEAN_AUTO_OFF; // Different modes are available - see MEAN_AUTO_MODE.
-	bool init				= true;			// Set some settings before first calculation.
+	bool modeMean				= false;	// Use mode mean algorithm?  User can change this.
+	MEAN_AUTO_MODE meanAuto		= MEAN_AUTO_OFF;	// Different modes are available.
+	bool init					= true;		// Set some settings before first calculation.
 											// This is set to "false" after calculation.
-	int exposureLevelMin	= NOT_SET;		// Set during initialization.
-	int exposureLevelMax	= NOT_SET;		// Set during initialization.
-	int exposureLevel		= NOT_SET;		// current ExposureLevel.
-	double minGain			= NOT_SET;		// Set during initialization.
-	double maxGain			= NOT_SET;		// Set during initialization.
-	long maxExposure_us		= NOT_SET;		// Set during initialization.
-	long minExposure_us		= NOT_SET;		// Set during initialization.
-	double meanValue		= NOT_SET;		// Default mean value for well exposed images.
+	int exposureLevelMin		= NOT_SET;	// Set during initialization.
+	int exposureLevelMax		= NOT_SET;	// Set during initialization.
+	int exposureLevel			= NOT_SET;	// current ExposureLevel.
+	double minGain				= NOT_SET;	// Set during initialization.
+	double maxGain				= NOT_SET;	// Set during initialization.
+	long maxExposure_us			= NOT_SET;	// Set during initialization.
+	long minExposure_us			= NOT_SET;	// Set during initialization.
+	double meanValue			= NOT_SET;	// Default mean value for well exposed images.
 
 	// Default mean value for daytime and nighttime images.  User can change.
-	double dayMean			= DEFAULT_DAYMEAN;
-	double nightMean		= DEFAULT_NIGHTMEAN;
-	double dayMean_threshold= DEFAULT_DAYMEAN_THRESHOLD;	// threshold value.  User can change.
-	double nightMean_threshold	= DEFAULT_NIGHTMEAN_THRESHOLD;
-// TODO: only use day and night versions
-	double mean_threshold	= DEFAULT_DAYMEAN_THRESHOLD;
+	double dayMean				= DEFAULT_DAYMEAN_RPi;
+	double nightMean			= DEFAULT_NIGHTMEAN_RPi;
 
-	double const shuttersteps = 6.0;		// shuttersteps
-	int const historySize	= 3;			// Number of last images for mean target calculation.
-	double mean_p0 = DEFAULT_MEAN_P0;		// ExposureChange (Steps) = p0 + p1 * diff + (p2*diff)^2
-	double mean_p1 = DEFAULT_MEAN_P1;
-	double mean_p2 = DEFAULT_MEAN_P2;
+	// Default threshold values for daytime and nighttime.  User can change.
+	double dayMean_threshold	= DEFAULT_DAYMEAN_THRESHOLD_RPi;
+	double nightMean_threshold	= DEFAULT_NIGHTMEAN_THRESHOLD_RPi;
+	double mean_threshold		= DEFAULT_DAYMEAN_THRESHOLD_RPi; // TODO: only use day and night
+
+	double const shuttersteps	= 6.0;		// shuttersteps
+	int const historySize		= 3;		// Number of last images for mean target calculation.
+
+	// ExposureChange (Steps) = p0 + p1 * diff + (p2*diff)^2
+	double mean_p0				= DEFAULT_MEAN_P0_RPi;
+	double mean_p1				= DEFAULT_MEAN_P1_RPi;
+	double mean_p2				= DEFAULT_MEAN_P2_RPi;
 };
 
 bool aegInit(config, raspistillSetting &, modeMeanSetting &);

e5eb9086e0198438bfa3b363f7fcb58d27213c29
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-06-01 22:12:27 -0500

    Update ASI_functions.cpp: add experimentalExposure

--- src/ASI_functions.cpp
+++ src/ASI_functions.cpp
@@ -963,6 +963,12 @@ void saveCameraInfo(ASI_CAMERA_INFO cameraInfo, char const *file, int width, int
 	fprintf(f, "\t\t},\n");
 
 	fprintf(f, "\t\t{\n");
+	fprintf(f, "\t\t\t\"Name\" : \"%s\",\n", "experimentalExposure");
+	fprintf(f, "\t\t\t\"argumentName\" : \"%s\",\n", "experimentalExposure");
+	fprintf(f, "\t\t\t\"DefaultValue\" : \"%s\"\n", CG.HB.useExperimentalExposure ? 1 : 0);
+	fprintf(f, "\t\t},\n");
+
+	fprintf(f, "\t\t{\n");
 	fprintf(f, "\t\t\t\"Name\" : \"%s\",\n", "histogrambox");
 	fprintf(f, "\t\t\t\"argumentName\" : \"%s\",\n", "histogrambox");
 	fprintf(f, "\t\t\t\"DefaultValue\" : \"%s\"\n", CG.HB.sArgs);

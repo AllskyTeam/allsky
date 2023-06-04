73b93e496268a47d922c1645f936572b10a2161b
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-06-04 00:27:00 -0500

    status_messages.php: add $highlight argument
    If a caller wants to highlight the table of messages it can set the new $highlight argument which puts a border around the table and add a line at the top and bottom.

--- html/includes/status_messages.php
+++ html/includes/status_messages.php
@@ -4,17 +4,23 @@ class StatusMessages {
 
 	public function addMessage($message, $level='success', $dismissable=true) {
 		$status = "<tr class='alert alert-$level'><td>$message</td>";
-		if ($dismissable)
-			$status .= "<td class='alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>x</button></td>";
-		else
+		if ($dismissable) {
+			$status .= "<td class='alert-dismissable'>";
+			$status .= "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>x</button>";
+			$status .= "</td>";
+		} else {
 			$status .= "<td></td>";
+		}
 		$status .= "</tr>";
 
 		array_push($this->messages, $status);
 	}
 
-	// if $escape is true, escape single quotes.
-	public function showMessages($clear=true, $escape=false) {
+
+	// If $escape is true, escape single quotes.
+	// If $highlight is true, hightlight the groups of messages (often only error message(s)).
+
+	public function showMessages($clear=true, $escape=false, $highlight=false) {
 		if ($escape === true)
 			// We can't have any single quotes in the output.
 			$apos = "&apos;";
@@ -23,21 +29,42 @@ class StatusMessages {
 
 		$count = 0;
 		foreach($this->messages as $message) {
-			if ($count === 0)
-				echo "<table width=$apos" . "100%$apos>";
+			$count++;
+			if ($count === 1) {
+				if ($highlight) {
+					$x .= " style=$apos" . "border: 3px dashed black; margin-top: 20px;$apos";
+				} else {
+					$x = "";
+				}
+				echo "<table width=$apos" . "100%$apos $x>";
+				if ($highlight) {
+					echo "<tr class=$apos alert-danger$apos style=$apos" . "height: 1em;$apos>";
+					echo "<td colspan=$apos" . "2$apos></td>";
+					echo "</tr>";
+				}
+			}
 
-			if ($count++ >= 1) {
+			if ($count >= 2) {
 				// space between messages
 				echo "<tr style=$apos" . "height: 5px$apos><td></td></tr>";
 			}
+
 			if ($escape === true)
 				$message = str_replace("'", "&apos;", $message);
+
 
 			// Replace newlines with HTML breaks.
 			echo str_replace("\n", "<br>", $message);
 		}
 
-		if ($count > 0) echo "</table>";
+		if ($count > 0) {
+			if ($highlight) {
+				echo "<tr class=$apos alert-danger$apos style=$apos" . "height: 1em;$apos>";
+				echo "<td colspan=$apos" . "2$apos></td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+		}
 
 		if ( $clear ) $this->messages = array();
 	}

d4e0b871cb726257a221364dcb84e01044065414
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-06-09 23:34:15 -0500

    Update darkCapture.sh: changes due to changes in saveImage.sh
    * We're only called if taking dark frames, so no need to check.  This changed indentation.
    * saveImage.sh updates AS_TEMPERATURE_C as needed so we don't need to.

--- scripts/darkCapture.sh
+++ scripts/darkCapture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This file is "source"d into another.
-# "${CURRENT_IMAGE}" is the name of the current image we're working on and is passed to us.
+# "${CURRENT_IMAGE}" is the full pathname of the current image we're working on and is passed to us.
 ME2="$(basename "${BASH_SOURCE[0]}")"
 
 # Make sure the input file exists; if not, something major is wrong so exit.
@@ -14,49 +14,29 @@ if [[ ! -f ${CURRENT_IMAGE} ]]; then
 	exit 2
 fi
 
-# ${AS_TEMPERATURE_C} is passed to us by saveImage.sh, but may be null.
-# If ${AS_TEMPERATURE_C} is set, use it as the temperature, otherwise read the ${TEMPERATURE_FILE}.
-# If the ${TEMPERATURE_FILE} file doesn't exist, set the temperature to "n/a".
+# The extension on $CURRENT_IMAGE may not be $EXTENSION.
+DARK_EXTENSION="${CURRENT_IMAGE##*.}"
+
+DARKS_DIR="${ALLSKY_DARKS}"
+mkdir -p "${DARKS_DIR}"
 if [[ -z ${AS_TEMPERATURE_C} ]]; then
-	TEMPERATURE_FILE="${ALLSKY_TMP}/temperature.txt"
-	if [[ -s ${TEMPERATURE_FILE} ]]; then	# -s so we don't use an empty file
-		AS_TEMPERATURE_C=$( < "${TEMPERATURE_FILE}")
-	else
-		AS_TEMPERATURE_C="n/a"
-	fi
+	# The camera doesn't support temperature so we'll keep overwriting the file until
+	# AS_TEMPERATURE_C is set.
+	# This allows users to continually look for a new dark file and rename it manually.
+	MOVE_TO_FILE="${DARKS_DIR}/$(basename "${CURRENT_IMAGE}")"
+else
+	MOVE_TO_FILE="${DARKS_DIR}/${AS_TEMPERATURE_C}.${DARK_EXTENSION}"
 fi
+mv "${CURRENT_IMAGE}" "${MOVE_TO_FILE}" || exit 3
 
-if [[ $(settings ".takeDarkFrames") -eq 1 ]]; then
-	# The extension on $CURRENT_IMAGE may not be $EXTENSION.
-	DARK_EXTENSION="${CURRENT_IMAGE##*.}"
+# If the user has notification images on, the current image says "Taking dark frames",
+# so don't overwrite it.
+# If notification images are off, let the user see the dark from to know it's working.
+# Some people may want to see the dark frame even if notification images
+# are being used, but no one's askef for that feature so don't worry about it.
 
-	DARKS_DIR="${ALLSKY_DARKS}"
-	mkdir -p "${DARKS_DIR}"
-	# If the camera doesn't support temperature, we will keep overwriting the file until
-	# the user creates a temperature.txt file.
-	if [[ ${AS_TEMPERATURE_C} == "n/a" ]]; then
-		MOVE_TO_FILE="${DARKS_DIR}/$(basename "${CURRENT_IMAGE}")"
-	else
-		MOVE_TO_FILE="${DARKS_DIR}/${AS_TEMPERATURE_C}.${DARK_EXTENSION}"
-	fi
-	mv "${CURRENT_IMAGE}" "${MOVE_TO_FILE}"
-
-	# If the user has notification images on, the current image says we're taking dark frames,
-	# so don't overwrite it.
-	# xxxx It's possible some people will want to see the dark frame even if notification images
-	# are being used - may need to make it optional to see the dark frame.
-	USE_NOTIFICATION_IMAGES=$(settings ".notificationimages")
-	if [[ ${USE_NOTIFICATION_IMAGES} -eq 0 ]]; then
-		# Go ahead and let the web sites see the dark frame to see if it's working.
-		# We're copying back the file we just moved, but the assumption is few people
-		# will want to see the dark frames on the web.
-
-		# xxxx  TODO: don't use $FULL_FILENAME since that assumes $EXTENSION is the same as
-		# $DARK_EXTENSION.  If we start saving darks as .png files the extensions will be
-		# different and we'll need to run "convert" to make the dark a .jpg file to
-		# be displayed on the web.
-		cp "${MOVE_TO_FILE}" "$(dirname "${CURRENT_IMAGE}")/${FULL_FILENAME}"
-	fi
-
-	exit 0	# exit so the calling script exits and doesn't try to process the file.
+if [[ $(settings ".notificationimages") -eq 0 ]]; then
+	# We're copying back the file we just moved, but the assumption is few people
+	# will want to see the dark frames so the performance hit is 
+	cp "${MOVE_TO_FILE}" "${ALLSKY_TMP}/${FILENAME}.${EXTENSION}"
 fi

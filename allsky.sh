e2d2b34d617411961d5f8c0609044cb781ebdf13
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-07-28 19:28:50 -0500

    Update allsky.sh: remove message about ALLSKY_INSTALLATION_LOGS

--- allsky.sh
+++ allsky.sh
@@ -82,12 +82,6 @@ if [[ -f ${POST_INSTALLATION_ACTIONS} ]]; then
 		"${ALLSKY_SCRIPTS}/addMessage.sh" "info" "${MSG}"
 	fi
 fi
-if [[ -d ${ALLSKY_INSTALLATION_LOGS} ]]; then
-	MSG="Logs from the last installation are in '${ALLSKY_INSTALLATION_LOGS}'."
-	MSG="${MSG}\nIf Allsky is working fine, you can remove the logs:"
-	MSG="${MSG}\n  &nbsp; &nbsp; <code>rm -fr '${ALLSKY_INSTALLATION_LOGS}'</code>"
-	"${ALLSKY_SCRIPTS}/addMessage.sh" "info" "${MSG}"
-fi
 
 USE_NOTIFICATION_IMAGES=$(settings ".notificationimages")
 

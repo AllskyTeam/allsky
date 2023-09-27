f527145e9ab3c6bfcdb36dc6b7aeca6249fdd7da
Alex Greenland <alex.greenland@gmail.com>
2023-09-27 20:12:33 +0100

    Add Website

---
+++ html/website/videos/index.php
@@ -0,0 +1,6 @@
+<?php
+$dir = ".";
+$prefix = "allsky";
+$title = "Timelapse Videos";
+include_once("../show_thumbnails.php");
+?>
---
+++ html/website/viewSettings/README.txt
@@ -0,0 +1,1 @@
+This directory holds settings-related files from the Pi so they can optionally be displayed in the Allsky Website.

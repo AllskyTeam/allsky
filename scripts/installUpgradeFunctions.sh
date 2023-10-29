e9f281da2a2691e33eeba5640ad460eb1841a67b
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-10-28 19:23:07 -0500

    Update installUpgradeFunctions.sh: don't set ALLSKY_VERSION
    It's now set in variables.sh

--- scripts/installUpgradeFunctions.sh
+++ scripts/installUpgradeFunctions.sh
@@ -317,7 +317,6 @@ function check_remote_server()
 export ALLSKY_OWNER=$(id --group --name)		# The login installing Allsky
 export ALLSKY_GROUP=${ALLSKY_OWNER}
 export WEBSERVER_GROUP="www-data"
-export ALLSKY_VERSION="$(get_version "${ALLSKY_HOME}" )"
 export REPO_WEBCONFIG_FILE="${ALLSKY_REPO}/${ALLSKY_WEBSITE_CONFIGURATION_NAME}.repo"
 export OLD_WEBUI_LOCATION="/var/www/html"		# location of old-style WebUI
 export OLD_WEBSITE_LOCATION="${OLD_WEBUI_LOCATION}/allsky"

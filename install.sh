7da2e498f984ca9aded4677b720c1191d5aedae2
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-10-28 19:44:13 -0500

    Update install.sh: add ALLSKY_VERSION to message

--- install.sh
+++ install.sh
@@ -110,9 +110,9 @@ do_initial_heading()
 	fi
 
 	if [[ ${do_initial_heading} == "true" ]]; then
-		display_header "Welcome back to the ${TITLE}!"
+		display_header "Welcome back to the ${TITLE} for ${ALLSKY_VERSION}!"
 	else
-		MSG="Welcome to the ${TITLE}!\n"
+		MSG="Welcome to the ${TITLE} for ${ALLSKY_VERSION}!\n"
 
 		if [[ -n ${PRIOR_ALLSKY} ]]; then
 			MSG="${MSG}\nYou will be asked if you want to use the images and darks (if any) from"
@@ -137,7 +137,7 @@ do_initial_heading()
 			exit_installation 1 "${STATUS_CLEAR}" ""
 		fi
 
-		display_header "Welcome to the ${TITLE}!"
+		display_header "Welcome to the ${TITLE} for ${ALLSKY_VERSION}"
 	fi
 
 	[[ ${do_initial_heading} != "true" ]] && STATUS_VARIABLES+=("do_initial_heading='true'\n")

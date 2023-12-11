b53286ddd3044a7474770134dd609184d3a7c253
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-12-10 22:42:46 -0600

    Update variables.sh: Add ALLSKY_PYTHON_VENV

--- variables.sh
+++ variables.sh
@@ -150,6 +150,9 @@ if [[ -z "${ALLSKY_VARIABLE_SET}" ]]; then
 	SETTINGS_FILE="${ALLSKY_CONFIG}/settings.json"
 	OPTIONS_FILE="${ALLSKY_CONFIG}/options.json"
 
+	# Python virtual environment
+	ALLSKY_PYTHON_VENV="${ALLSKY_HOME}/venv"
+
 	# These EXIT codes from the capture programs must match what's in src/include/allsky_common.h
 	# Anything at or above EXIT_ERROR_STOP is unrecoverable and the service must be stopped
 	EXIT_OK=0

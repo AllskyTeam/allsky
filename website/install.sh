7a7b4018ef38530d87da3202ebb1c21e1d33c8c7
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-12-31 02:39:06 -0600

    Update install.sh

--- website/install.sh
+++ website/install.sh
@@ -512,7 +512,7 @@ create_website_configuration_file() {
 
 	LOCATION="$(settings ".location")"
 	OWNER="$(settings ".owner")"
-	CAMERA_MODEL="$(settings ".cameraModel")"
+	CAMERA_MODEL="$(settings ".cameramodel")"
 	CAMERA="${CAMERA_TYPE}${CAMERA_MODEL}"
 	LENS="$(settings ".lens")"
 	COMPUTER="$(sed --quiet -e 's/Raspberry Pi/RPi/' -e '/^Model/ s/.*: // p' /proc/cpuinfo)"

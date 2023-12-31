7dd173a84b9a0f6e1ab4fd270a4ce7bf62f43fb5
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-12-31 02:28:45 -0600

    Update copy_notification_image.sh

--- scripts/copy_notification_image.sh
+++ scripts/copy_notification_image.sh
@@ -133,8 +133,8 @@ fi
 # Don't save in main image directory because we don't want the notification image in timelapses.
 # If at nighttime, save them in (possibly) yesterday's directory.
 # If during day, save in today's directory.
-if [[ $(settings ".takeDaytimeImages") == "1" && \
-	  $(settings ".saveDaytimeImages") == "1" && \
+if [[ $(settings ".takedaytimeimages") == "1" && \
+	  $(settings ".savedaytimeimages") == "1" && \
 	  ${IMG_CREATE_THUMBNAILS} == "true" ]]; then
 	DATE_DIR="${ALLSKY_IMAGES}/$(date +'%Y%m%d')"
 	# Use today's folder if it exists, otherwise yesterday's

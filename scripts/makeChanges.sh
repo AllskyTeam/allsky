0b940e6e3b36b9691c70aedbd115b6198c17b5fb
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2023-12-31 05:38:17 -0600

    Update makeChanges.sh: fix cameraModel
    It has uppercase "M" in the cc_file

--- scripts/makeChanges.sh
+++ scripts/makeChanges.sh
@@ -260,7 +260,7 @@ do
 
 				# Create a link to a file that contains the camera type and model in the name.
 				CAMERA_TYPE="${NEW_VALUE}"		# already know it
-				CAMERA_MODEL="$( settings .cameramodel "${CC_FILE}" )"
+				CAMERA_MODEL="$( settings .cameraModel "${CC_FILE}" )"
 				if [[ -z ${CAMERA_MODEL} ]]; then
 					echo -e "${wERROR}ERROR: 'cameraModel' not found in ${CC_FILE}.${wNC}"
 					[[ -f ${CC_FILE_OLD} ]] && mv "${CC_FILE_OLD}" "${CC_FILE}"

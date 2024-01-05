0bed832347272f15784052a5cc102345cbe94be0
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-01-05 14:45:29 -0600

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -177,7 +177,7 @@ if [[ ${ACTION} == "upgrade" ]]; then
 	#	cd
 	#	Git new code into ${ALLSKY_HOME}
 	#	cd ${ALLSKY_HOME}
-	#	Run: ./install.sh $DEBUG_ARG .... --doUpgrade
+	#	Run: ./install.sh ${DEBUG_ARG} .... --doUpgrade
 	#		--doUpgrade tells it to use prior version without asking and to
 	#		not display header, change messages to say "upgrade", not "install", etc.
 	#	?? anything else?
@@ -196,21 +196,21 @@ if [[ ${ACTION} == "upgrade" ]]; then
 elif [[ ${ACTION} == "restore" ]]; then
 	:
 
-	# If running in $ALLSKY_HOME		# us 1st time through
+	# If running in ${ALLSKY_HOME}		# us 1st time through
 	#	Make sure ${ALLSKY_HOME}-OLD exists
 	#		If not, warn user and exit:
 	#			"No prior version to restore from: ${ALLSKY_HOME}-OLD does not exist".
 	#	cp ${ME} /tmp
 	#	chmod 775 /tmp/${ME}
-	#	exec /tmp/${ME} --restore ${ALL_ARGS} $ALLSKY_HOME
+	#	exec /tmp/${ME} --restore ${ALL_ARGS} ${ALLSKY_HOME}
 
 	# Else		# running from /tmp - do the actual work
 	#	Stop allsky
-	#	mv $ALLSKY_HOME} ${ALLSKY_HOME}-new_tmp
-	#	mv ${ALLSKY_HOME}-OLD $ALLSKY_HOME
-	#	move images from ${ALLSKY_HOME}-new_tmp to $ALLSKY_HOME
-	#	move darks from ${ALLSKY_HOME}-new_tmp to $ALLSKY_HOME
-	#	copy scripts/endOfNight_additionalSteps.sh from ${ALLSKY_HOME}-new_tmp to $ALLSKY_HOME
+	#	mv ${ALLSKY_HOME} ${ALLSKY_HOME}-new_tmp
+	#	mv ${ALLSKY_HOME}-OLD ${ALLSKY_HOME}
+	#	move images from ${ALLSKY_HOME}-new_tmp to ${ALLSKY_HOME}
+	#	move darks from ${ALLSKY_HOME}-new_tmp to ${ALLSKY_HOME}
+	#	copy scripts/endOfNight_additionalSteps.sh from ${ALLSKY_HOME}-new_tmp to ${ALLSKY_HOME}
 
 	# Prompt the user if they want to:
 	#	restore their old "images" folder (if there's anything in it)

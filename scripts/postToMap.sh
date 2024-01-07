3c097a12be2ab305ee06e3e2a7993aaac26970b0
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-01-06 20:44:04 -0600

    Update postToMap.sh: fix: add missing &&

--- scripts/postToMap.sh
+++ scripts/postToMap.sh
@@ -333,7 +333,7 @@ RETURN_CODE=0
 if [[ ${UPLOAD} == "true" ]]; then
 	if [[ ${DELETE} == "true" ]]; then
 		[[ ${WHISPER} == "false" ]] && echo "${ME}: Deleting map data."
-	elif [[ ${ON_TTY} == "true" ${ALLSKY_DEBUG_LEVEL} -ge 3 ]]; then
+	elif [[ ${ON_TTY} == "true" && ${ALLSKY_DEBUG_LEVEL} -ge 3 ]]; then
 		[[ ${WHISPER} == "false" ]] && echo "${ME}: Uploading map data."
 	fi
 	# shellcheck disable=SC2089

d9446f744e9a490215f180d1545ba9acf266ca94
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-01-08 04:14:01 -0600

    Update overlayutil.php: delete unneeded spaces

--- html/includes/overlayutil.php
+++ html/includes/overlayutil.php
@@ -23,7 +23,7 @@ class OVERLAYUTIL
         "\${TEMPERATURE_F}" => array(
             "ccfield" => "hasSensorTemperature",
             "value" => false,
-        )            
+        )
     );
 
     public function __construct()
@@ -205,7 +205,7 @@ class OVERLAYUTIL
         $fileName = $this->overlayPath . '/config/fields.json';
         $fields = file_get_contents($fileName);
         $systemData = json_decode($fields);
-        
+
         $fileName = $this->overlayPath . '/config/userfields.json';
         $fields = file_get_contents($fileName);
         $userData = json_decode($fields);
@@ -214,7 +214,7 @@ class OVERLAYUTIL
         $mergedFields = array();
 
         foreach($systemData->data as $systemField) {
-            if ($this->includeField($systemField->name)) { 
+            if ($this->includeField($systemField->name)) {
                 $field = array(
                     "id" => $counter,
                     "name" => $systemField->name,
@@ -231,7 +231,7 @@ class OVERLAYUTIL
 
         foreach($userData->data as $userField) {
 
-            if ($this->includeField($systemField->name)) {          
+            if ($this->includeField($systemField->name)) {
                 $field = array(
                     "id" => $counter,
                     "name" => $userField->name,
@@ -250,7 +250,7 @@ class OVERLAYUTIL
             "data" => $mergedFields
         );
         $jsonFields = json_encode($fields);
-        
+
         $this->sendResponse($jsonFields);
     }
 
@@ -451,7 +451,7 @@ class OVERLAYUTIL
         }
 
         if ($proceed) {
-            $saveFolder = $this->overlayPath . "/fonts/";          
+            $saveFolder = $this->overlayPath . "/fonts/";
             $result = array();
             $zipArchive = new ZipArchive();
             $zipArchive->open($downloadPath);

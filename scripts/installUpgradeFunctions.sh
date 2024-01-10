32e2898baae22318ee0a126ed901a4ee6d41a564
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-01-10 01:01:31 -0600

    Update installUpgradeFunctions.sh: add WEBSERVER_OWNER

--- scripts/installUpgradeFunctions.sh
+++ scripts/installUpgradeFunctions.sh
@@ -203,7 +203,8 @@ function display_msg()
 # export to keep shellcheck quiet
 export ALLSKY_OWNER=$( id --group --name )		# The login installing Allsky
 export ALLSKY_GROUP=${ALLSKY_OWNER}
-export WEBSERVER_GROUP="www-data"
+export WEBSERVER_OWNER="www-data"
+export WEBSERVER_GROUP="${WEBSERVER_OWNER}"
 export REPO_WEBCONFIG_FILE="${ALLSKY_REPO}/${ALLSKY_WEBSITE_CONFIGURATION_NAME}.repo"
 export OLD_WEBUI_LOCATION="/var/www/html"		# location of old-style WebUI
 export OLD_WEBSITE_LOCATION="${OLD_WEBUI_LOCATION}/allsky"

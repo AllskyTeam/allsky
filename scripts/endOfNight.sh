7389b31ca4f3ac3e248ccafa91d8e26a21b0e19b
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-02-02 18:19:37 -0600

    Update endOfNight.sh

--- scripts/endOfNight.sh
+++ scripts/endOfNight.sh
@@ -12,10 +12,6 @@ ME="$( basename "${BASH_ARGV0}" )"
 source "${ALLSKY_HOME}/variables.sh"		|| exit "${EXIT_ERROR_STOP}"
 #shellcheck source-path=scripts
 source "${ALLSKY_SCRIPTS}/functions.sh"		|| exit "${EXIT_ERROR_STOP}"
-#shellcheck disable=SC1091		# file doesn't exist in GitHub
-source "${ALLSKY_CONFIG}/config.sh"			|| exit "${EXIT_ERROR_STOP}"
-#shellcheck disable=SC1091		# file doesn't exist in GitHub
-source "${ALLSKY_CONFIG}/ftp-settings.sh"	|| exit "${EXIT_ERROR_STOP}"
 
 if [[ $# -eq 1 ]]; then
 	if [[ ${1} = "--help" ]]; then
@@ -53,26 +49,26 @@ if [[ ${WEBSITES} != "none" ]]; then
 fi
 
 # Generate keogram from collected images
-if [[ ${KEOGRAM} == "true" ]]; then
+if [[ $( settings ".keogramgenerate" ) == "true" ]]; then
 	echo -e "${ME}: ===== Generating Keogram for ${DATE}"
 	#shellcheck disable=SC2086
 	"${ALLSKY_SCRIPTS}/generateForDay.sh" ${NICE_ARG} --silent --keogram "${DATE}"
 	RET=$?
 	echo -e "${ME}: ===== Keogram complete"
-	if [[ ${UPLOAD_KEOGRAM} == "true" && ${RET} = 0 ]] ; then
+	if [[ $( settings ".keogramupload" ) == "true" && ${RET} = 0 ]] ; then
 		"${ALLSKY_SCRIPTS}/generateForDay.sh" --upload --keogram "${DATE}"
 	fi
 fi
 
 # Generate startrails from collected images.
-# Threshold set to 0.1 by default in config.sh to avoid stacking over-exposed images.
-if [[ ${STARTRAILS} == "true" ]]; then
+# Threshold set to 0.1 by default to avoid stacking over-exposed images.
+if [[ $( settings ".startrailsgenerate" ) == "true" ]]; then
 	echo -e "${ME}: ===== Generating Startrails for ${DATE}"
 	#shellcheck disable=SC2086
 	"${ALLSKY_SCRIPTS}/generateForDay.sh" ${NICE_ARG} --silent --startrails "${DATE}"
 	RET=$?
 	echo -e "${ME}: ===== Startrails complete"
-	if [[ ${UPLOAD_STARTRAILS} == "true" && ${RET} = 0 ]] ; then
+	if [[ $( settings ".startrailsupload" ) == "true" && ${RET} = 0 ]] ; then
 		"${ALLSKY_SCRIPTS}/generateForDay.sh" --upload --startrails "${DATE}"
 	fi
 fi
@@ -80,20 +76,18 @@ fi
 # Generate timelapse from collected images.
 # Use generateForDay.sh instead of putting all the commands here so users can easily
 # test the timelapse creation, which sometimes has issues.
-if [[ ${TIMELAPSE} == "true" ]]; then
+if [[ $( settings ".timelapsegenerate" ) == "true" ]]; then
 	echo -e "${ME}: ===== Generating Timelapse for ${DATE}"
 	#shellcheck disable=SC2086
 	"${ALLSKY_SCRIPTS}/generateForDay.sh" ${NICE_ARG} --silent --timelapse "${DATE}"
 	RET=$?
 	echo -e "${ME}: ===== Timelapse complete"
-	if [[ ${UPLOAD_VIDEO} == "true" && ${RET} = 0 ]] ; then
+	if [[ $( settings ".timelapseupload" ) == "true" && ${RET} = 0 ]] ; then
 		"${ALLSKY_SCRIPTS}/generateForDay.sh" --upload --timelapse "${DATE}"
 	fi
 fi
 
-DAYS_TO_KEEP=${DAYS_TO_KEEP:-0}					# old versions allowed "" to disable
-WEB_DAYS_TO_KEEP=${WEB_DAYS_TO_KEEP:-0}			# old versions allowed "" to disable
-
+DAYS_TO_KEEP="$( settings ".daystokeep" )"
 # Automatically delete old images and videos.
 if [[ ${DAYS_TO_KEEP} -gt 0 ]]; then
 	del=$( date --date="${DAYS_TO_KEEP} days ago" +%Y%m%d )
@@ -110,16 +104,16 @@ if [[ ${DAYS_TO_KEEP} -gt 0 ]]; then
 	done
 fi
 
-# Automatically delete old LOCAL Website images and videos.
+# Automatically delete old Website images and videos.
 
-# TODO: work on remote Websites
 
-if [[ ${WEB_DAYS_TO_KEEP} -gt 0 ]]; then
+LOCAL_WEB_DAYS_TO_KEEP="$( settings ".daystokeeplocalwebsite" )"
+if [[ ${LOCAL_WEB_DAYS_TO_KEEP} -gt 0 && $( settings ".uselocalwebsite" ) == "true" ]]; then
 	if [[ ! -d ${ALLSKY_WEBSITE} ]]; then
-		echo -e "${ME}: ${YELLOW}WARNING: 'WEB_DAYS_TO_KEEP' set but no website found in '${ALLSKY_WEBSITE}!${NC}"
-		echo -e 'Set WEB_DAYS_TO_KEEP to ""'
+		echo -e "${ME}: ${YELLOW}WARNING: 'Days to Keep on Pi Website' set but no Website found in '${ALLSKY_WEBSITE}!${NC}"
+		echo -e 'Set "Days to Keep on Pi Website" to ""'
 	else
-		del=$( date --date="${WEB_DAYS_TO_KEEP} days ago" +%Y%m%d )
+		del=$( date --date="${LOCAL_WEB_DAYS_TO_KEEP} days ago" +%Y%m%d )
 		(
 			cd "${ALLSKY_WEBSITE}" || exit 1
 			NUM_DELETED=0
@@ -147,6 +141,14 @@ if [[ ${WEB_DAYS_TO_KEEP} -gt 0 ]]; then
 			done
 		)
 	fi
+fi
+
+REMOTE_WEB_DAYS_TO_KEEP="$( settings ".daystokeepremotewebsite" )"
+if [[ ${REMOTE_WEB_DAYS_TO_KEEP} -gt 0 && $( settings ".useremotewebsite" ) == "true" ]]; then
+	# TODO: work on remote Websites.
+	# Possibly do a curl xxxx?keep=${REMOTE_WEB_DAYS_TO_KEEP}
+	# and pass something so it knows this is a valid request.
+	:
 fi
 
 SHOW_ON_MAP=$( settings ".showonmap" )

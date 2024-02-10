e04911d050006a21db4452e07369df07a93ad57f
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-02-10 04:52:08 -0600

    upload testUpload.sh
    A script to test an upload and provide information on why it failed.

---
+++ scripts/testUpload.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+
+# Run upload.sh using a test file and if it doesn't work, parse the output
+# looking for errors that are easy for users to miss.
+
+# Allow this script to be executed manually, which requires ALLSKY_HOME to be set.
+[[ -z ${ALLSKY_HOME} ]] && export ALLSKY_HOME="$( realpath "$( dirname "${BASH_ARGV0}" )/.." )"
+ME="$( basename "${BASH_ARGV0}" )"
+
+#shellcheck source-path=.
+source "${ALLSKY_HOME}/variables.sh"		|| exit "${EXIT_ERROR_STOP}"
+#shellcheck source-path=scripts
+source "${ALLSKY_SCRIPTS}/functions.sh"		|| exit "${EXIT_ERROR_STOP}"
+
+usage_and_exit()
+{
+	local RET=${1}
+	{
+		[[ ${RET} -ne 0 ]] && echo -en "${RED}"
+		echo "Usage: ${ME} [--help] --website | --server ......."
+		[[ ${RET} -ne 0 ]] && echo -e "${NC}"
+	} >&2
+	exit "${RET}"
+}
+
+DEBUG="false"
+DO_WEBSITE="false"
+DO_SERVER="false"
+while [[ $# -gt 0 ]]; do
+	ARG="${1}"
+	case "${ARG,,}" in
+		--help)
+			HELP="true"
+			;;
+		--debug)
+			DEBUG="true"
+			;;
+		--website)
+			DO_WEBSITE="true"
+			;;
+		--server)
+			DO_SERVER="true"
+			;;
+		-*)
+			echo -e "${RED}Unknown argument '${ARG}'.${NC}" >&2
+			RET=1
+			;;
+		*)
+			break	# done with arguments
+			;;
+	esac
+	shift
+done
+[[ ${HELP} == "true" ]] && usage_and_exit 0
+[[ ${DO_WEBSITE} == "false" && ${DO_SERVER} == "false" ]] && usage_and_exit 1
+
+# Parse the output and provide fixes when possible.
+parse_output()
+{	local OUTPUT="${1}"
+	local TYPE="${2}"
+
+	local PROTOCOL DIR HOST USER S
+
+	if [[ ${TYPE} == "REMOTEWEBSITE" ]]; then
+		PROTOCOL=".remotewebsiteprotocol"
+		DIR=".remotewebsiteimagedir"
+		S="Remote Website Settings"
+	else
+		PROTOCOL=".remoteserverprotocol"
+		DIR=".remoteserverimagedir"
+		S="Remote Server Settings"
+	fi
+	HOST="${TYPE}_HOST"
+	USER="${TYPE}_USER"
+
+	# Parse output.
+	if grep -i --silent "host name resolve timeout" "${OUTPUT}" ; then
+		HOST="$( settings ".${HOST}" "${ENV_FILE}" )"
+		echo -e "\t* Host name '${HOST}' not found."
+		echo -e "\t  FIX: Check the spelling of the server."
+	   	echo -e "\t       Make sure your network is up."
+	   	echo -e "\t       Make sure the network the server is on is up."
+	fi >&2
+
+	if grep -E -i --silent "User cannot log in|Login failed|Login incorrect" "${OUTPUT}" ; then
+		echo -e "\t* Unable to login."
+		echo -e "\t  FIX: Make sure the username and password are correct."
+	fi >&2
+
+	if grep -i --silent "max-retries exceeded" "${OUTPUT}" ; then
+		echo -e "\t* Unable to login for unknown reason."
+		echo -e "\t  FIX: Make sure the port is correct and your network is working."
+		PROTOCOL="$( settings "${PROTOCOL}" )"
+		if [[ ${PROTOCOL} == "sftp" ]]; then
+			HOST="$( settings ".${HOST}" "${ENV_FILE}" )"
+			USER="$( settings ".${USER}" "${ENV_FILE}" )"
+			echo -e "\t       On your Pi, run:  ssh ${USER}@${HOST}"
+			echo -e "\t       When prompted to enter 'yes' or 'no', enter 'yes'."
+			echo -e "\t       You may need to do this if the IP address of your Pi changed."
+		fi
+	fi >&2
+
+	if grep -i --silent "The system cannot find the file specified" "${OUTPUT}" ; then
+		if grep -i --silent "is current directory" "${OUTPUT}" ; then
+			echo -e "\t* Login succeeded but unknown location found."
+		else
+			# This should never happen.
+			# If we can't login we wouldn't know if the location was there.
+			echo -e "\t* Login failed and unknown location found."
+		fi
+		DIR="$( settings "${DIR}" )"
+		if [[ -n ${DIR} ]]; then
+			echo -e "\t  The 'Image Directory' in the WebUI's '${S}' section is '${DIR}'."
+			echo -e "\t  FIX: make sure that directory exists on the server."
+		else
+			echo -e "\t  The 'Image Directory' in the WebUI's '${S}' section is empty."
+			# TODO: can this ever happen?
+			echo -e "\t  FIX: unknown - not sure why this failed."
+		fi
+	fi >&2
+
+	# Certificate-related issues
+	if grep -i --silent "The authenticity of host" "${OUTPUT}" ; then
+		HOST="$( settings ".${HOST}" "${ENV_FILE}" )"
+		USER="$( settings ".${USER}" "${ENV_FILE}" )"
+		PROTOCOL="$( settings "${PROTOCOL}" )"
+		echo -e "\t* The remote machine doesn't know about your Pi."
+		if [[ ${PROTOCOL} == "sftp" ]]; then
+			echo -e "\t  This happens the first time you use Protocol 'sftp' on a new Pi."
+			echo -e "\t  FIX: On your Pi, run:  ssh ${USER}@${HOST}"
+			echo -e "\t       When prompted to enter 'yes' or 'no', enter 'yes'."
+			echo -e "\t       You may need to do this if the IP address of your Pi changed."
+		else
+			echo -e "\t  This error usually only happens when using Protocol 'sftp' on a new Pi."
+			echo -e "\t  You are using Protocol '${PROTOCOL}', so no know fix exists."
+		fi
+	fi >&2
+
+	if grep -i --silent "certificate common name doesn't match requested host name" "${OUTPUT}" ; then
+		echo -e "\t* Certificate verification issue."
+		echo -e "\t  FIX: Do one of the following on your Pi:"
+		echo -e "\t    echo 'set ssl:check-hostname' > ~/.lftprc"
+		echo -e "\t  or"
+		echo -e "\t    In the WebUI's '${S}' section set 'FTP Commands' to 'set ssl:check-hostname'."
+	fi >&2
+
+	if grep -i --silent "Not trusted" "${OUTPUT}" ; then
+		echo -e "\t* Certificate verification issue."
+		echo -e "\t  FIX: o one of the following on your Pi:"
+		echo -e "\t    echo 'set ssl:verify-certificate no' > ~/.lftprc"
+		echo -e "\t  or"
+		echo -e "\t    In the WebUI's '${S}' section set 'FTP Commands' to 'set ssl:verify-certificate no'."
+	fi >&2
+
+
+	if [[ -s ${OUTPUT} || ${DEBUG} == "true" ]]; then
+		echo -e "\n==================== OUTPUT:"
+		indent "${YELLOW}$( < "${OUTPUT}" )${NC}\n"
+	fi >&2
+}
+
+
+# Test an upload.
+do_test()
+{
+	local TYPE="${1}"
+
+	local TEST_FILE bTEST_FILE OUTPUT_FILE HUMAN_TYPE PROTOCOL DIR REMOTE CMD D
+
+	TEST_FILE="/tmp/${ME}.txt"
+	bTEST_FILE="$( basename "${TEST_FILE}" )"
+	echo "Test file for ${TYPE}" > "${TEST_FILE}" || return 1
+
+	OUTPUT_FILE="/tmp/${ME}-${TYPE}.txt"
+
+	if [[ ${TYPE} == "REMOTEWEBSITE" ]]; then
+		HUMAN_TYPE="Remote Website"
+		PROTOCOL=".remotewebsiteprotocol"
+		DIR=".remotewebsiteimagedir"
+		REMOTE="web"
+	else
+		HUMAN_TYPE="Remote Server"
+		PROTOCOL=".remoteserverprotocol"
+		DIR=".remoteserverimagedir"
+		REMOTE="server"
+	fi
+
+	PROTOCOL="$( settings "${PROTOCOL}" )"
+	if [[ $? -ne 0 || -z ${PROTOCOL} ]]; then
+		echo -e "${RED}${ME}: could not find protocol for ${HUMAN_TYPE}; unable to test.${NC}" >&2
+		return 1
+	fi
+
+	DIR="$( settings "${DIR}" )"
+	DIR="${DIR:=null}"
+
+	CMD="${ALLSKY_SCRIPTS}/upload.sh --debug --remote ${REMOTE} ${TEST_FILE} ${DIR} ${bTEST_FILE} ${ME}"
+	[[ ${DEBUG} == "true" ]] && echo -e "Executing:\n\t${CMD}"
+	${CMD} > "${OUTPUT_FILE}" 2>&1
+	RET=$?
+	if [[ ${RET} -eq 0 ]]; then
+		echo -e "${GREEN}Upload to ${HUMAN_TYPE} succeeded.${NC}"
+		if [[ -z ${DIR} || ${DIR} == "null" ]]; then
+			D=""
+		else
+			D="${DIR}/"
+		fi
+		echo -e "\tPlease remove '${D}${bTEST_FILE}' on your server."
+		if [[ -s ${OUTPUT_FILE} && ${DEBUG} == "true" ]]; then
+			echo -e "OUTPUT:"
+			echo -e "${YELLOW}$( < "${OUTPUT_FILE}" )${NC}\n"
+		fi
+	else
+		echo -e "${RED}Upload to ${HUMAN_TYPE} FAILED with RET=${RET}.${NC}"
+		parse_output "${OUTPUT_FILE}" "${TYPE}"
+	fi
+
+	rm -f "${TEST_FILE}"
+	[[ ! -s ${OUTPUT_FILE} ]] && rm -f "${OUTPUT_FILE}"
+	exit "${RET}"
+}
+
+# ========================= main body of program
+[[ ${DO_WEBSITE} == "true" ]] && do_test "REMOTEWEBSITE"
+[[ ${DO_SERVER} == "true" ]] && do_test "REMOTESERVER"
+
+exit 0

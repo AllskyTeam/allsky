32c2760dc519aca482bd37b5477f307fd0b100ea
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-03-09 14:19:07 -0600

    Update postData.sh: Fix: only send to enabled locations
    e.g., local Website, remote Website, remote server.
    Tell upload.sh (via upload_all) not to output any messages (--silent).
    If called from the WebUI, output a single summary line.

--- scripts/postData.sh
+++ scripts/postData.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This script uploads a file to a Website to tell the Website when the user has defined
-# "sunrise" and "sunset".  Use the angle set by the user.
+# "sunrise" and "sunset".
 # A copy of the settings file is also uploaded.
-# By default we upload to both local and remote Websites if they exist.
+# Upload to both local and remote Websites if they are enabled.
 
 # Allow this script to be executed manually or by sudo, which requires several variables to be set.
 [[ -z ${ALLSKY_HOME} ]] && export ALLSKY_HOME="$( realpath "$( dirname "${BASH_ARGV0}" )/.." )"
@@ -60,7 +60,42 @@ while [[ $# -gt 0 ]]; do
 	shift
 done
 [[ ${RET} -ne 0 ]] && usage_and_exit ${RET}
-[[ ${HELP} = "true" ]] && usage_and_exit 0
+[[ ${HELP} == "true" ]] && usage_and_exit 0
+
+
+# If there are no enabled Websites or an enabled remote server, then exit.
+WEBS=""
+SERVER=""
+WHERE_TO=""
+if [[ "$( settings ".uselocalwebsite" )" == "true" ]]; then
+	WEBS+=" --local-web"
+	WHERE_TO="local Website"
+fi
+if [[ "$( settings ".useremotewebsite" )" == "true" ]]; then
+	WEBS+=" --remote-web"
+	[[ -n ${WHERE_TO} ]] && WHERE_TO+=", "
+	WHERE_TO="remote Website"
+fi
+if [[ "$( settings ".useremoteserver" )" == "true" ]]; then
+	SERVER="--remote-server"
+	[[ -n ${WHERE_TO} ]] && WHERE_TO+=", "
+	WHERE_TO="remote server"
+fi
+if [[ -z ${WEBS} && -z ${USE_REMOTE_SERVER} ]]; then
+	if [[ ${ON_TTY} == "true" ]]; then
+		echo -e "\nWARNING: No action taken because no Websites are enabled.\n" >&2
+		exit 1
+	else
+		exit 0
+	fi
+fi
+
+if [[ ${FROM_WEBUI} == "true" ]]; then
+	# Don't want potentially lots of "uploading xxx" messages.
+	SILENT="--silent"
+else
+	SILENT=""
+fi
 
 if [[ ${SETTINGS_ONLY} == "false" ]]; then
 	OK="true"
@@ -138,7 +173,7 @@ function upload_file()
 	local FILE_TO_UPLOAD="${2}"
 	local DIRECTORY="${3}"		# Directory to put file in
 	if [[ ! -f ${FILE_TO_UPLOAD} ]]; then
-		MSG="File to upload '${FILE_TO_UPLOAD}' not found."
+		local MSG="File to upload '${FILE_TO_UPLOAD}' not found."
 		echo -e "${RED}${ME}: ERROR: ${MSG}.${NC}" >&2
 		if [[ ${FROM_WEBUI} == "false" ]]; then
 			"${ALLSKY_SCRIPTS}/addMessage.sh" "error" "${ME}: ${MSG}"
@@ -147,32 +182,33 @@ function upload_file()
 	fi
 
 	#shellcheck disable=SC2086
-	upload_all ${WHERE} "${FILE_TO_UPLOAD}" "${DIRECTORY}" "" "PostData"
+	upload_all ${SILENT} ${WHERE} "${FILE_TO_UPLOAD}" "${DIRECTORY}" "" "PostData"
 	return $?
 }
 
 # These files go in ${VIEW_DIR} so the user can display their settings.
 # This directory is in the root of the Allsky Website.
 # Assume if the first upload fails they all will, so exit.
-WEB_ONLY="--local-web --remote-web"
-upload_file "${WEB_ONLY}" \
+[[ -n ${WEBS} ]] && upload_file "${WEBS}" \
 		"${SETTINGS_FILE}" \
 		"${ALLSKY_WEBSITE_VIEWSETTINGS_DIRECTORY_NAME}" \
 	|| exit $?
 
-if [[ ${ALL_FILES} == "true" ]]; then
-	upload_file "${WEB_ONLY}" "${OPTIONS_FILE}" \
-		"${ALLSKY_WEBSITE_VIEWSETTINGS_DIRECTORY_NAME}"
-	upload_file "${WEB_ONLY}" "${ALLSKY_WEBUI}/includes/allskySettings.php" \
-		"${ALLSKY_WEBSITE_VIEWSETTINGS_DIRECTORY_NAME}"
-	upload_file "${WEB_ONLY}" "${ALLSKY_DOCUMENTATION}/css/custom.css" \
-		"${ALLSKY_WEBSITE_VIEWSETTINGS_DIRECTORY_NAME}"
+if [[ -n ${WEBS} && ${ALL_FILES} == "true" ]]; then
+	for file in \
+			"${OPTIONS_FILE}" \
+			"${ALLSKY_WEBUI}/includes/allskySettings.php" \
+			"${ALLSKY_DOCUMENTATION}/css/custom.css" 
+	do
+		upload_file "${WEBS}" "${file}" "${ALLSKY_WEBSITE_VIEWSETTINGS_DIRECTORY_NAME}"
+	done
 fi
 
 if [[ ${SETTINGS_ONLY} == "false" ]]; then
 	# Some remote servers may want to see this file so upload everywhere.
 	upload_file "" "${OUTPUT_FILE}" ""		# Goes in top-level directory
-	exit $?
 fi
+
+[[ ${FROM_WEBUI} == "true" ]] && echo "Uploaded configuration files to ${WHERE_TO}."
 
 exit 0

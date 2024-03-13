5d36f9dc4e61c00ac086aefce7d43cd41de9a3b1
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-03-13 04:28:32 -0500

    Update postData.sh: Add missing commas

--- scripts/postData.sh
+++ scripts/postData.sh
@@ -159,9 +159,9 @@ if [[ ${SETTINGS_ONLY} == "false" ]]; then
 		echo "{"
 		echo "\"sunrise\": \"${today}T${sunrise_hhmm}:00.000${timezone}\","
 		echo "\"sunset\": \"${today}T${sunset_hhmm}:00.000${timezone}\","
-		echo "\"takedaytimeimages\": \"${D}\""
+		echo "\"streamDaytime\": \"${D}\"",		# TODO: old name - remove in next release
+		echo "\"takedaytimeimages\": \"${D}\"",
 		echo "\"takenighttimeimages\": \"${N}\""
-		echo "\"streamDaytime\": \"${D}\""		# TODO: old name - remove in next release
 		echo "}"
 	} > "${OUTPUT_FILE}"
 fi

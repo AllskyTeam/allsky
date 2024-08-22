707b8c26a02d8a1766419ce3f60acc34ee4e5d0f
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-08-21 23:44:10 -0500

    Update getChecksum.php: check for non-existent files

--- scripts/utilities/getChecksum.php
+++ scripts/utilities/getChecksum.php
@@ -3,6 +3,9 @@
 <?php
 // Determine the checksum for the files listed on stdin.
 while (($file = fgets(STDIN))) {
+	if (! file_exists($file))
+		continue;
+
 	$c = crc32($file);
 	echo "$c\t$file";
 }

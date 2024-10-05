a7e0746a3b5b55d8f8276125b87e1489d7b60168
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-10-04 23:35:27 -0500

    Update uninstall.sh: umount tmp

--- uninstall.sh
+++ uninstall.sh
@@ -21,8 +21,10 @@ if whiptail --title "${TITLE}" --yesno "${MSG}" 10 60 3>&1 1>&2 2>&3; then
 	echo
     echo -e "A few things of note:"
     echo -e "  - To remove ALL traces of 'allsky' (${RED}This cannot be undone!${NC}), run:"
-# TODO: remove everything else, e.g., website if installed, lighttpd, ...
-	echo -e "     ${YELLOW}cd; sudo rm -rf allsky${NC}"
+# TODO: remove everything else, e.g., lighttpd, /var/log/allsky*, ...
+	echo -e "     ${YELLOW}cd${NC}"
+	echo -e "     ${YELLOW}sudo umount tmp${NC}"
+	echo -e "     ${YELLOW}sudo rm -rf allsky${NC}"
 	echo
     echo -e "  - If you wish to only remove config files, run:"
 	echo -e "     ${YELLOW}sudo make remove_configs${NC}"

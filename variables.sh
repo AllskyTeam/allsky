dc866fedccf4d022c4262531dfed75e3201b0b88
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2024-12-18 05:37:53 -0600

    Update variables.sh: Add disable SC2317
    For some reason sourcing in variables.sh causes every subsequent line in every file to give SC2317 errors

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2034		# variable unused
+# shellcheck disable=SC2034,SC2317		# variable unused, statement unreached
 
 # This file is source'd into other files to set some variables used by scripts.
 # This allows us to easily add and change directory names.

d2b6b1e88ee77c6883a659ea2c12fcadfcb174dd
Alex Greenland <alex.greenland@gmail.com>
2024-12-27 16:30:38 +0000

    #4104 Fix shellcheck and add tcs dialog

--- support.sh
+++ support.sh
@@ -7,23 +7,30 @@
 #            issues with the Allsky installation from interfering with the data collection
 #            Some 'standard' variables are hard coded in this script.
 #
-[[ -z ${ALLSKY_HOME} ]] && export ALLSKY_HOME="$( realpath "$( dirname "${BASH_ARGV0}" )" )"
-
 LOG_LINES="all"
-SUPPORT_DATETIME=$(date +"%Y-%m-%d-%H:%M:%S")
 SUPPORT_DATETIME_SHORT=$(date +"%Y%m%d%H%M%S")
 SUPPORT_ZIP_NAME="support-ISSUE-${SUPPORT_DATETIME_SHORT}.zip"
 SUPPORT_DIR="${ALLSKY_HOME}/html/support"
 DIALOG_COMPLETE_MESSAGE="The support information has now been generated and can be found in\n\n
 ${ALLSKY_HOME}/html/support/ZIPNAME\n\n
 Due to the size of the support information the file has been compressed.\n\n
-This file should be attached to the relevant discussion in Github.\n\nIf your webui is functioning the the file can be downloaded using the support menu option
-"
+This file should be attached to the relevant discussion in Github.\n\nIf your webui is functioning the the file can be downloaded using the support menu option"
 GITHUB_ERROR="Error\n\nThe Discussion number must be numberic\n\nIt can be found in the URL of the github issue, 
 for example in this case\n\nhttps://github.com/AllskyTeam/allsky/discussions/4119\n\nThe discussion number would be 4119"
-
+SUPPORT_TCS="\nThis script will collect data from your Raspberry Pi to assist in supporting any issues.\n\n
+No personal information is collected by this script. The following data is collected\n\n
+- Basic system information\n
+- Filesystem/Memory/Network Information (IPV4/6/MAC details are obfuscated)\n
+- Installed system and python packages\n
+- Allsky/lighttpd logs and error logs\n
+- Connected camera details\n
+- i2c bus details\n
+- Running processes\n
+- Allsky config files (obfuscated where required to hide any credentials)\n\n
+Select 'Yes' to agree or 'No' to abort this script
+"
 ####
-# Install prerequisits
+# Install prerequisites
 function init() 
 {
     clear
@@ -58,8 +65,9 @@ function print_sub_heading(){
     printf "%-20s\n" "----------------------------"    
 }
 
-function get_info()
+function collect_support_info()
 {
+    # shellcheck disable=SC1091
     . /etc/os-release
 
     ### Misc information
@@ -72,8 +80,9 @@ function get_info()
     ###
 
     ### OS Information
-    ID="${ID}"
-    VERSION_ID="${VERSION_ID}"
+    OS_ID="${ID}"
+    OS_VERSION_ID="${VERSION_ID}"
+    OS_VERSION_CODENAME="${VERSION_CODENAME}"
     ###
 
     ### Hardware Information
@@ -94,6 +103,10 @@ function get_info()
 
     ### Network Info
     NETWORKS="$(ip a)"
+    # obfuscate IP, MAC and ipv6 addresses
+    NETWORKS=$(echo "${NETWORKS}" | sed -E 's/192\.168\.[0-9]+\.[0-9]+/[PRIVATE]/g')
+    NETWORKS=$(echo "${NETWORKS}" | sed -E 's/([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}/[REDACTED]/g')
+    NETWORKS=$(echo "${NETWORKS}" | sed -E 's/\b([0-9A-Fa-f]{1,4}:){1,7}([0-9A-Fa-f]{1,4}|:)\b/[REDACTED]/g')
     ###
 
     ### File system information
@@ -104,11 +117,7 @@ function get_info()
     ### Allsky file information
     ALLSKY_VERSION="$( head -1 "./version" )"
     DEBUG_LEVEL=$(jq .debuglevel ./config/settings.json)
-    if [[ "${TREE}" == "true" ]]; then
-        ALLSKY_FILES="$(tree -ugp --gitignore --prune -I '.git|__pycache__')"
-    else
-        ALLSKY_FILES=""
-    fi
+    ALLSKY_FILES="$(tree -ugp --gitignore --prune -I '.git|__pycache__')"
 
     # shellcheck source=/dev/null
     source "${ALLSKY_HOME}/venv/bin/activate"
@@ -117,16 +126,9 @@ function get_info()
     ###
 
     ### Devices
-    if [[ "${DEVICES}" == "true" ]]; then
-        DEV=$(sudo ls -alh  /dev)
-    else
-        DEV=""
-    fi
-    if [[ "${FULL_USB}" == "true" ]]; then
-        USB="$(sudo lsusb -v)"
-    else
-        USB="$(sudo lsusb)"
-    fi
+    DEV=$(sudo ls -alh  /dev)
+
+    USB="$(sudo lsusb -v)"
     I2C_ENABLED=$(sudo raspi-config nonint get_i2c)
     I2C_DEVICES=""
     if [[ ${I2C_ENABLED} == "0" ]]; then
@@ -155,75 +157,101 @@ function generate_support_info()
     TEMP_DIR=$(mktemp -d "${TEMP_FOLDER}"/allskyXXXXX)
 
     BASIC_FILE="${TEMP_DIR}/system.txt"
-    print_heading "Misc Info" > "${BASIC_FILE}"
-    print_info "Date and Time": "$(date)" >> "${BASIC_FILE}"
-    print_info "Allsky Version:" "${ALLSKY_VERSION}" >> "${BASIC_FILE}"
-    print_info "Allsky Debug Level:" "${DEBUG_LEVEL}" >> "${BASIC_FILE}"
-    print_info "Uptime:" "${UPTIME}" >> "${BASIC_FILE}"
-    print_info "OS Id:" "${ID}" >> "${BASIC_FILE}"
-    print_info "OS Version:" "${VERSION_ID}" >> "${BASIC_FILE}"
-    print_info "Pi Revision:" "${PI_REVISION}" >> "${BASIC_FILE}"
-    print_info "Pi Model:" "${PI_MODEL}" >> "${BASIC_FILE}"
-    print_info "Total CPU Cores:" "${CPU_TOTAL}" >> "${BASIC_FILE}"
-    print_info "CPU Architecture:" "${CPU_ARCH}" >> "${BASIC_FILE}"
-    print_info "CPU Bits:" "${CPU_BITS}" >> "${BASIC_FILE}"
-    print_info "Total RAM:" "${MEM_TOTAL}" >> "${BASIC_FILE}"
-    print_info "User Name:" "${USER_NAME}" >> "${BASIC_FILE}"
-    print_info "User ID:" "${USER_ID}" >> "${BASIC_FILE}"
+    {
+        print_heading "Misc Info"
+        print_info "Date and Time": "$(date)"
+        print_info "Allsky Version:" "${ALLSKY_VERSION}"
+        print_info "Allsky Debug Level:" "${DEBUG_LEVEL}"
+        print_info "Uptime:" "${UPTIME}"
+        print_info "OS Id:" "${OS_ID}"
+        print_info "OS Version:" "(${OS_VERSION_ID}) ${OS_VERSION_CODENAME}"
+        print_info "Pi Revision:" "${PI_REVISION}"
+        print_info "Pi Model:" "${PI_MODEL}"
+        print_info "Total CPU Cores:" "${CPU_TOTAL}"
+        print_info "CPU Architecture:" "${CPU_ARCH}"
+        print_info "CPU Bits:" "${CPU_BITS}"
+        print_info "Total RAM:" "${MEM_TOTAL}"
+        print_info "User Name:" "${USER_NAME}"
+        print_info "User ID:" "${USER_ID}"
+    } >> "${BASIC_FILE}"
 
     ISSUE_FILE="${TEMP_DIR}/issue.txt"
-    print_heading "Github Issue" > "${ISSUE_FILE}"
-    print "${ISSUE_NUMBER}" >> "${ISSUE_FILE}"
+    {
+        print_heading "Github Issue"
+        print "${ISSUE_NUMBER}" 
+    } >> "${ISSUE_FILE}"
 
     OS_FILE="${TEMP_DIR}/ps.txt"
-    print_heading "Process Information" > "${OS_FILE}"
-    print "${PS}" >> "${OS_FILE}"
+    {
+        print_heading "Process Information"
+        print "${PS}" 
+    } >> "${OS_FILE}"
 
     MEMORY_FILE="${TEMP_DIR}/memory.txt"
-    print_heading "Memory Info" > "${MEMORY_FILE}"
-    print "${MEMORY_INFO}" >> "${MEMORY_FILE}"
+    {
+        print_heading "Memory Info"
+        print "${MEMORY_INFO}"
+    } >> "${MEMORY_FILE}"
 
     NETWORK_FILE="${TEMP_DIR}/network.txt"
-    print_heading "Network Info" > "${NETWORK_FILE}"
-    print "${NETWORKS}" >> "${NETWORK_FILE}"
+    {
+        print_heading "Network Info"
+        print "${NETWORKS}"
+    } >> "${NETWORK_FILE}"
 
     FILESYSTEM_FILE="${TEMP_DIR}/filesystem.txt"
-    print_heading "File Systems" > "${FILESYSTEM_FILE}"
-    print "${FILE_SYSTEMS}" >> "${FILESYSTEM_FILE}"
+    {
+        print_heading "File Systems"
+        print "${FILE_SYSTEMS}"
+    } >> "${FILESYSTEM_FILE}"
 
     if [[ "${DEVICES}" == "true" ]]; then
         DEVICES_FILE="${TEMP_DIR}/devices.txt"
-        print_heading "Devices" > "${DEVICES_FILE}"
-        print "${DEV}" >> "${DEVICES_FILE}"
+        {
+            print_heading "Devices"
+            print "${DEV}"
+        } >> "${DEVICES_FILE}"
     fi
 
     USB_FILE="${TEMP_DIR}/usb.txt"
-    print_heading "USB Devices" > "${USB_FILE}"
-    print "${USB}" >> "${USB_FILE}"
+    {
+        print_heading "USB Devices"
+        print "${USB}"
+    } >> "${USB_FILE}"
 
     LIBCAMERA_FILE="${TEMP_DIR}/libcamera.txt"
-    print_heading "Libcamera Devices" > "${LIBCAMERA_FILE}"
-    print "${PI_CAMERAS}" >> "${LIBCAMERA_FILE}"
+    {
+        print_heading "Libcamera Devices"
+        print "${PI_CAMERAS}"
+    } >> "${LIBCAMERA_FILE}"
 
     i2C_FILE="${TEMP_DIR}/i2c.txt"
-    print_heading "I2C Devices" > "${i2C_FILE}"
-    print "${I2C_DEVICES}" >> "${i2C_FILE}"
+    {
+        print_heading "I2C Devices"
+        print "${I2C_DEVICES}"
+    } >> "${i2C_FILE}"
 
     ALLSKYFILES_FILE="${TEMP_DIR}/allsky_files.txt"
-    print_heading "Allsky Files" > "${ALLSKYFILES_FILE}"
-    print_info "Allsky Version:" "${ALLSKY_VERSION}" >> "${ALLSKYFILES_FILE}"
-    print "${ALLSKY_FILES}" >> "${ALLSKYFILES_FILE}"
+    {
+        print_heading "Allsky Files"
+        print_info "Allsky Version:" "${ALLSKY_VERSION}"
+        print "${ALLSKY_FILES}"
+    } >> "${ALLSKYFILES_FILE}"
 
     ALLSKYVENV_FILE="${TEMP_DIR}/allsky_venv.txt"
-    print_heading "Allsky Venv information" > "${ALLSKYVENV_FILE}"
-    print_info "Python Version:" "${PYTHON_VERSION}" >> "${ALLSKYVENV_FILE}"
-    print "${PYTHON_MODULES}" >> "${ALLSKYVENV_FILE}"
-    print_heading "Package Information" >> "${ALLSKYVENV_FILE}"
-    print "${PYTHON_PACKAGES}" >> "${ALLSKYVENV_FILE}"
+    {
+        print_heading "Allsky Venv information"
+        print_info "Python Version:" "${PYTHON_VERSION}"
+        print "${PYTHON_MODULES}"
+        print_heading "Package Information"
+        print "${PYTHON_PACKAGES}"
+    }  >> "${ALLSKYVENV_FILE}"
 
     APT_FILE="${TEMP_DIR}/apt.txt"
-    print_heading "APT installed packages" > "${APT_FILE}"
-    print "${APT_INSTALLED}" >> "${APT_FILE}"
+    {
+        print_heading "APT installed packages" > "${APT_FILE}"
+        print "${APT_INSTALLED}"
+    } >> "${APT_FILE}"
 
     LIGHTTPD_ERROR_LOG_FILE="${TEMP_DIR}/lighttpd_error.txt"
     LIGHTTPD_ERROR_LOG="/var/log/lighttpd/error.log"
@@ -231,19 +259,19 @@ function generate_support_info()
         cp "${LIGHTTPD_ERROR_LOG}" "${LIGHTTPD_ERROR_LOG_FILE}"
     fi
 
-    if [[ "$INCLUDE_ALLSKY_SCRIPTS" == "true" ]]; then
-        CAMERA_INFO_FILE="${TEMP_DIR}/camera_info.txt"
-        print_heading "Allsky - Supported Cameras" > "${CAMERA_INFO_FILE}"
-        print_sub_heading "Raspberry Pi Cameras" >> "${CAMERA_INFO_FILE}"
-        ./scripts/utilities/show_supported_cameras.sh --rpi >> "${CAMERA_INFO_FILE}"
+    CAMERA_INFO_FILE="${TEMP_DIR}/camera_info.txt"
+    {
+        print_heading "Allsky - Supported Cameras"
+        print_sub_heading "Raspberry Pi Cameras"
+        ./scripts/utilities/show_supported_cameras.sh --rpi 
 
-        print_sub_heading "Raspberry Pi Cameras Attached" >> "${CAMERA_INFO_FILE}"
-        ./scripts/utilities/get_RPi_camera_info.sh >> "${CAMERA_INFO_FILE}"
-        cat ./tmp/camera_data.txt >> "${CAMERA_INFO_FILE}"
+        print_sub_heading "Raspberry Pi Cameras Attached"
+        ./scripts/utilities/get_RPi_camera_info.sh
+        cat ./tmp/camera_data.txt
 
-        print_sub_heading "ZWO Cameras" >> "${CAMERA_INFO_FILE}"
-        ./scripts/utilities/show_supported_cameras.sh --zwo >> "${CAMERA_INFO_FILE}"
-    fi
+        print_sub_heading "ZWO Cameras"
+        ./scripts/utilities/show_supported_cameras.sh --zwo
+    } >> "${CAMERA_INFO_FILE}"
 
     ALLSKY_LOG_FILE="${TEMP_DIR}/allsky_log.txt"
     if [[ "${LOG_LINES}" == "all" ]]; then
@@ -261,14 +289,12 @@ function generate_support_info()
         tail -n "${LOG_LINES}" /var/log/allskyperiodic.log > "${ALLSKYPERIODIC_LOG_FILE}"
     fi
 
-    if [[ "${INCLUDE_ALLSKY_CONFIG}" == "true" ]]; then
-        cp -ar "${ALLSKY_HOME}"/config "${TEMP_DIR}"
+    cp -ar "${ALLSKY_HOME}"/config "${TEMP_DIR}"
 
-        # Truncate the JPL ephemeris files as its hughe and not needed for support
-        truncate -s 0 "${TEMP_DIR}/config/overlay/config/tmp/overlay/de421.bsp"
-        # Truncate all of the module configs until we can obfuscate any sensitive data
-        find "${TEMP_DIR}"/config/modules -type f -exec truncate -s 0 {} +
-    fi
+    # Truncate the JPL ephemeris files as its hughe and not needed for support
+    truncate -s 0 "${TEMP_DIR}/config/overlay/config/tmp/overlay/de421.bsp"
+    # Truncate all of the module configs until we can obfuscate any sensitive data
+    find "${TEMP_DIR}"/config/modules -type f -exec truncate -s 0 {} +
 
     SUPPORT_ZIP_NAME="${SUPPORT_ZIP_NAME//ISSUE/${ISSUE_NUMBER}}"
     DIALOG_COMPLETE_MESSAGE="${DIALOG_COMPLETE_MESSAGE//ZIPNAME/${SUPPORT_ZIP_NAME}}"
@@ -280,44 +306,12 @@ function generate_support_info()
     chmod u+wx "${TEMP_DIR}/${SUPPORT_ZIP_NAME}"
     sudo mv "${TEMP_DIR}/${SUPPORT_ZIP_NAME}" "${SUPPORT_DIR}"
     trap 'rm -rf "${TEMP_DIR}"' EXIT
-
 }
 
 ####
-# Allow user to select the data to include
-function get_options()
+# Allows the user to enter the Github discussion id
+function get_github_discussion_id()
 {
-    if [[ "${TEXT_MODE}" == "false" ]]; then
-        if [[ "${SHOW_OPTIONS}" == "true" ]]; then
-            TEMP_FILE=$(mktemp)
-            dialog --checklist "Select data to include:" 15 50 5 \
-            1 "Allsky File Tree" off \
-            2 "Full USB details" off \
-            3 "Device list" off \
-            4 "Allsky Config Files" off 2> "${TEMP_FILE}"
-
-            choices=$(<"${TEMP_FILE}")
-            rm -f "${TEMP_FILE}"
-
-            if [[ -n $choices ]]; then
-                for choice in $choices; do
-                    if [[ "${choice}" == "1" ]]; then
-                        TREE="true"
-                    fi
-                    if [[ "${choice}" == "2" ]]; then
-                        FULL_USB="true"
-                    fi
-                    if [[ "${choice}" == "3" ]]; then
-                        DEVICES="true"
-                    fi 
-                    if [[ "${choice}" == "4" ]]; then
-                        INCLUDE_ALLSKY_CONFIG="true"
-                    fi                        
-                done
-            fi
-        fi
-    fi
-
     if [[ "${ISSUE_NUMBER}" == "none" ]]; then
         while true; do
             if [[ "${TEXT_MODE}" == "false" ]]; then
@@ -372,6 +366,16 @@ display_complete_dialog()
     fi
 }
 
+display_start_dialog()
+{
+    dialog --title "Generate Support Information" --yesno "${SUPPORT_TCS}" 25 80
+    response=$?
+    if [ $response -eq 1 ]; then
+        exit 1
+    fi
+}
+
+
 kill_running_dialog()
 {
     if [[ "${TEXT_MODE}" == "false" ]]; then
@@ -393,12 +397,6 @@ function usage_and_exit()
 		echo -e "	'--help' displays this message and exits."
 		echo -e "	'--text' Use text mode. Options must be specified on the command line."        
 		echo -e "	'--issue' Include the Githuib issue number."        
-		echo -e "	'--tree' Include full list of ALL Allsky files."
-		echo -e "	'--fullusb' Include full USB device details."
-		echo -e "	'--devices' Include /dev listing."
-		echo -e "	'--config' Include Allsky configuration files."
-		echo -e "	'--showoptions' Allow data to be included to be selected."
-		echo -e "	'--excludeallskyscript' DO not include the Allsky camera scripts output."
 		echo -e "	'--loglines' Number of lines to include from log files, defaults to 'all'. Use 'all' for entire file"
 
 	} >&2
@@ -406,12 +404,6 @@ function usage_and_exit()
 }
 
 OK="true"
-TREE="false"
-FULL_USB="false"
-DEVICES="false"
-INCLUDE_ALLSKY_CONFIG="false"
-INCLUDE_ALLSKY_SCRIPTS="true"
-SHOW_OPTIONS="false"
 TEXT_MODE="false"
 ISSUE_NUMBER="none"
 
@@ -424,30 +416,6 @@ while [[ $# -gt 0 ]]; do
 
 		--text)
 			TEXT_MODE="true"
-			;;
-
-		--tree)
-			TREE="true"
-			;;
-
-		--fullusb)
-			FULL_USB="true"
-			;;
-
-		--devices)
-			DEVICES="true"
-			;;
-
-		--config)
-			INCLUDE_ALLSKY_CONFIG="true"
-			;;
-
-		--showoptions)
-			SHOW_OPTIONS="true"
-			;;
-
-        --excludeallskyscript)
-			INCLUDE_ALLSKY_SCRIPTS="false"
 			;;
 
         --issue)
@@ -477,10 +445,11 @@ done
 [[ ${DO_HELP} == "true" ]] && usage_and_exit 0
 [[ ${OK} == "false" ]] && usage_and_exit 1
 
+display_start_dialog
 init
-get_options
+get_github_discussion_id
 display_running_dialog
-get_info
+collect_support_info
 generate_support_info
 kill_running_dialog
 display_complete_dialog

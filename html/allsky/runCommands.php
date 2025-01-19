94f59a694ca6eb123332777bfe1c139abf126437
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-01-19 00:05:30 -0600

    Update runCommands.php: big changes - see description
    * Cleaned up comments.
    * Deleted unused variables.
    * Added several commands.
    * Parse line into array once.

--- html/allsky/runCommands.php
+++ html/allsky/runCommands.php
@@ -1,30 +1,22 @@
 #!/usr/bin/php
-<?php
-/*
-// Get a list of all files, not including the ones the user creates
-{
-	echo loading.jpg
-	echo allsky-logo.png
-	echo NoThumbnail.png
-	echo allsky-favicon.png
-	# echo image.jpg
-	find . -type f '!' '(' -name '*.jpg' -or -name '*.png' -or -name '*.mp4' ')' |
-		sed 's;^./;;' |
-		grep -E -v "myFiles/|configuration.json"
-} > files.txt
-*/
+<?php		// Make 2nd line in file or else output includes blank lines.
 
+// Run pre-defined "commands".
+// These commands allow us to check sanity of the Website, create missing directories,
+// remove "old" files, and other tasks.
+
+// This file is normally accessed via curl, not a web page,
+// so only use html in output when invoked from a web page.
+// The list of "commands" to run are in $commandFile, one per line,
+// with optional tab-separated arguments.
+
+$checksumsFile = "checksums.txt";
 $commandFile = "commands.txt";
 $delete_commands = true;
 
-// Check sanity of Website and make necessary directories.
-// This file is normally accessed via curl, not a web page,
-// so only use html in output when invoked from a web page.
-// The list of "commands" to run are in $commandFile, one command per line,
-// with optional tab-separated arguments.
-
 $TAB = "\t";
 $NL = "\n";
+$on64 = (PHP_INT_MAX !== 2147483647);	// are we using 64-bit?
 
 if (isset($_SERVER['HTTP_USER_AGENT']) &&
 		strpos($_SERVER['HTTP_USER_AGENT'], "curl") === false) {
@@ -36,8 +28,10 @@ if (isset($_GET["debug"])) {
 } else {
 	$debug = false;
 }
+$debug2 = false;
 
 if (! file_exists($commandFile)) {
+	$delete_commands = false;	// don't try to delete a non-existant file
 	do_error("", "Command file '$commandFile' not found.");
 	exit(1);
 }
@@ -47,26 +41,36 @@ if ($lines === false) {
 	do_error("", "Unable to read '$commandFile': ${last_error["message"]}.");
 	exit(1);
 }
-//echo "<pre>"; var_dump($lines);echo "</pre>";
-
-$startrails_glob = "startrails-*.jpg";
-$keogram_glob = "keogram-*.jpg";
-$timelapse_glob = "allsky-*.mp4";
+// echo "<pre>"; var_dump($lines);echo "</pre>";
 
 $ok = true;
 
+// Each line is:
+//	command [ TAB arg1 [ TAB arg2] ...]
 foreach ($lines AS $line) {
-	if ($debug) do_debug($command, "line=[$line]");
-	$command = strtok($line, "\t");
+	if ($debug2) do_debug("read line", "[$line]");
+	$args = explode("\t", $line);
+	if (count($args) === 0) {
+		do_warning("", "Line '$line' has no arguments.");
+		continue;
+	}
+	$command = $args[0];
+
 	if (substr($command, 0, 1) === "#")
 		continue;
+
+	// Delete command from array.
+	array_splice($args, 0, 1);
+	$numArgs = count($args);
 
 	switch ($command) {
 
 		case "mkdir":
 			// The arguments are the directories to create.
-			if (($args = get_args(-1, $command)) === null)
+			if ($numArgs === 0) {
+				do_error($command, "No arguments given; expected at least one.");
 				break;
+			}
 
 			$dirsCreated = "";
 			$c = 0;
@@ -98,8 +102,10 @@ foreach ($lines AS $line) {
 
 		case "get_numfiles":
 			// Return the number of files of the given type.
-			if (($args = get_args(-1, $command)) === null)
+			if ($numArgs === 0) {
+				do_error($command, "No arguments given; expected at least one.");
 				break;
+			}
 
 			foreach ($args as $filetype) {
 				$numFiles = count_files($filetype);
@@ -110,40 +116,55 @@ foreach ($lines AS $line) {
 
 		case "do_daystokeep":		// filetype TAB number
 			// Only keep up to $daystokeep images/videos
-			if (($args = get_args(2, $command)) === null)
+			if ($numArgs !== 2) {
+				do_error($command, "Expected 2 arguments.");
 				break;
+			}
 
 			$filetype = $args[0];
-				$parent = basename(dirname($filetype));
+			$parent = basename(dirname($filetype));
 			$daystokeep = $args[1];
 
 			$c = count_files($filetype);
 			$numToDelete = $c - $daystokeep;
 			if ($numToDelete > 0) {
 				// "true" to also delete the thumbnail.
-				$deleted = delete_files($filetype, $numToDelete, $command, $args, true);
-				if ($deleted > 0) {
-//x this is redundant with displaying individual file names that were deleted.
-//x					do_return($command, $args, "Deleted $deleted old '$parent' files.");
-				}
-			} else if ($debug) do_debug($command, "Only $c '$parent' files - not deleting any.");
+				delete_files($filetype, $numToDelete, $command, $args, true);
+			} else {
+				do_return($command, $args, "No '$parent' files deleted - $c left.");
+			}
 			break;
 
 
-		case "checksum":
-			// Check the checksum of the specified file.
-			if (($args = get_args(2, $command)) === null)
-				break;
-
-			$file = $args[0];
-			$checksum = $args[1] + 0;
-			if (! file_exists($file)) {
-				do_return($command, $args, "'$file' is missing.");
+		case "check_checksums":
+			// Check the checksums of all the files in the checksum file.
+			if (! file_exists($checksumsFile)) {
+				do_error($command, "Checksums file '$checksumsFile' not found.");
 				break;
 			}
-			if (crc32($file) !== $checksum) {
-				do_return($command, $args, "'$file' is different.");
-			} else if ($debug) do_debug($command, "'$file' is same.");
+			$files = file($checksumsFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+			$numSame = 0;
+			$numDifferent = 0;
+			$allErrors = "";
+			foreach ($files AS $info) {
+				$i = explode("\t", $info);
+				// false == don't output result; we'll do a summary below.
+				if (compareChecksum($command, $i, count($i), false, $error)) {
+					$numSame++;
+				} else {
+					$numDifferent++;
+					if ($allErrors !== "") $allErrors .= ", ";
+					$allErrors .= $error;
+				}
+			}
+			$msg = "Same:${TAB}$numSame${TAB}Different:${TAB}$numDifferent";
+			if ($numDifferent > 0) $msg .= " ${TAB}$allErrors";
+			do_return($command, "", $msg);
+			break;
+
+		case "check_checksum":		// file  TAB  checksum
+			// Check the checksum of the specified file.  true == output result
+			compareChecksum($command, $args, $numArgs, true, $not_used);
 			break;
 
 
@@ -180,37 +201,42 @@ foreach ($lines AS $line) {
 		case "set":
 			// Set a variable to something.
 
-			if (($args = get_args(2, $command)) === null)
+			if ($numArgs != 2) {
+				do_error($command, "Expected 2 arguments.");
 				break;
-
+			}
 			$variable = $args[0];
+			$value = $args[1];
+
 			switch ($variable) {
 				case "debug":
-					$debug = $args[1];
+					$debug = intval($value);
+					break;
+
+				case "debug2":
+					$debug2 = intval($value);
 					break;
 
 				case "delete-commands":
-					// Don't delete the commands file when done.
-					$delete_commands = $args[1];
+					$delete_commands = intval($value);
+					break;
+
+				case "html":
+					if (intval($value)) {
+						// Output as html
+						$TAB = "&nbsp;&nbsp;&nbsp;&nbsp;";
+						$NL = "<br>";
+					} else {
+						// Output as normal text
+						$TAB = "\t";
+						$NL = "\n";
+					}
 					break;
 
 				default:
 					do_warning($command, " variable '$variable' unknown; ignoring.");
 					break;
 			}
-			break;
-
-
-		case "raw":
-			// Output as normal text
-			$TAB = "\t";
-			$NL = "\n";
-			break;
-
-		case "html":
-			// Output as html
-			$TAB = "&nbsp;&nbsp;&nbsp;&nbsp;";
-			$NL = "<br>";
 			break;
 
 
@@ -230,59 +256,33 @@ if (! $ok) exit(1);
 
 
 // Helper functions.
-function do_return($cmd, $a, $msg, $severity="RETURN")
+function do_return($cmd, $a, $msg, $severity="RETURN", $exit=false)
 {
 	global $NL, $TAB;
 	static $deleted = false;
 
 	echo "${severity}${TAB}${cmd}";
 
-	if (gettype($a) === "array") echo " " . implode(" ", $a);
+	if (gettype($a) === "array") {
+		echo $TAB . implode($TAB, $a);
+	} else if ($a !== "") {
+		echo "${TAB}$a";
+	}
+
 	echo "${TAB}${msg}${NL}";
 
-	if (! $deleted) {
-		delete_commands_file();
+	if (! $deleted && $severity !== "DEBUG") {
+		delete_commands_file($cmd);
 		$deleted = true;
 	}
+	if ($exit)
+		exit;
 }
 
-function do_error($cmd, $msg)		{ do_return($cmd, "", $msg, "ERROR"); }
-function do_warning($cmd, $msg)		{ do_return($cmd, "", $msg, "WARNING"); }
-function do_debug($cmd, $msg)		{ do_return($cmd, "", $msg, "DEBUG"); }
-
-// Get all the arguments and put in an array.
-// Make sure there is at least $num arguments:
-//	0 means arguments are optional
-//	negative numbers means AT LEAST that many arguments
-
-$args = null;
-function get_args($num, $cmd) {
-	global $TAB, $NL, $args;
-
-	if ($args !== null)
-		unset($args);	// clear old array
-
-	$args = array();
-
-	$arg = strtok($TAB);
-	$found = 0;
-	while ($arg !== false) {
-		$found++;
-		$args[] = $arg;
-		$arg = strtok($TAB);
-	}
-
-	if ($found < abs($num)) {
-		if ($num < 0)
-			$x = "at least";
-		else
-			$x = "exactly";
-		
-		do_error($cmd, " requires $x $num argument(s); $found were given.");
-		return(null);
-	}
-	return($args);
-}
+function do_error($cmd, $msg, $exit=false)		{ do_return($cmd, "", $msg, "ERROR", $exit); }
+function do_warning($cmd, $msg, $exit=false)	{ do_return($cmd, "", $msg, "WARNING", $exit); }
+function do_info($cmd, $msg, $exit=false)		{ do_return($cmd, "", $msg, "INFO", $exit); }
+function do_debug($cmd, $msg, $exit=false)		{ do_return($cmd, "", $msg, "DEBUG", $exit); }
 
 // Return the number of file of the specified type.
 function count_files($filetype) {
@@ -306,16 +306,17 @@ function delete_files($filesToDelete, $deleteNum, $cmd, $a, $delete_thumbnail) {
 	while ($deleteNum > 0) {
 		$file = $files[$deleteNum -1];		// -1 for 0 index offset
 		$deleteNum--;
+		$msg = "";
 
 		if ($debug) {
-			do_return($cmd, $a, "'$file' NOT deleted due to debug");
+			do_info($cmd, "NOT deleted due to debug: '$file'.");
 		} else {
 			if (! unlink($file)) {
 				$last_error = error_get_last();
 				do_warning("Unable to remove '$file': ${last_error["message"]}.");
 			} else {
 				$c++;
-				do_return($cmd, $a, "'$file' deleted.");
+				$msg = "'$file'";
 
 				if ($filesDeleted !== "") $filesDeleted .= ", ";
 				$filesDeleted .= $file;
@@ -326,8 +327,14 @@ function delete_files($filesToDelete, $deleteNum, $cmd, $a, $delete_thumbnail) {
 				$parent = dirname($file);
 				$fileName = basename($file);
 				$thumb = "$parent/thumbnails/$fileName";
-				if (file_exists($thumb)) unlink($thumb);
+				if (file_exists($thumb)) {
+					$msg .= " and its thumbnail";
+					@unlink($thumb);
+				}
 			}
+
+			if ($debug && $msg !== "")
+				do_debug($cmd, "Deleted: $msg.");
 		}
 	}
 
@@ -374,13 +381,82 @@ function deleteDirectory($dir) {
 
 // Optionally delete the commands file.
 // Except for testing, the file should be deleted.
-function delete_commands_file()
+function delete_commands_file($cmd)
 {
 	global $delete_commands, $commandFile;
 
 	if ($delete_commands) {
-		unlink($commandFile);
+		if (! @unlink($commandFile)) {
+			$delete_commands = false;	// to keep from going in infinate loop
+			$last_error = error_get_last();
+			$err = $last_error["message"];
+			do_warning($cmd, $err, true);
+		}
 	}
+}
+
+function compareChecksum($cmd, $a, $num, $outputResult, &$error)
+{
+	global $debug, $debug2, $TAB, $on64;
+
+	if ($num != 2) {
+		// checksum, file
+		do_error($command, "Expected 2 arguments.");
+		return(false);
+	}
+
+	// Checksums in the file came from the Pi which could be 32- or 64-bit.
+	// If 32-bit, some checksums will be negative numbers.
+	// The machine this code is running on may be 32- or 64-bit.
+	// Convert all numbers to unsigned.
+
+// TODO: I may have complicated this more than it needs to be...
+
+	$checksum = $a[0];		// a string, may represent a negative number
+	$isNegative = (substr($checksum, 0, 1) === "-");
+	if ($isNegative) {
+# echo "BEFORE checksum= $checksum";
+		if ($on64) {
+			// checksum created on 32-bit machine.  Converting to 64-bit hex
+			// prepends too many "ffffffff".
+			$checksum = str_replace("ffffffff", "", dechex($checksum));
+		} else {
+			$checksum = sprintf("%u", $checksum);	// convert to unsigned string
+		}
+# echo ", AFTER  $checksum  <br>\n";
+	}
+
+	$file = $a[1];
+	if (! file_exists($file)) {
+		do_warning($cmd, "Missing:${TAB}$file");
+		return(false);
+	}
+
+	$c = crc32($file);			// may be a negative number
+# echo "BEFORE c= $c";
+	$c = sprintf("%u", $c);		// convert to unsigned as a string
+# echo ", AFTER  $c";
+	if ($on64 && $isNegative) {
+		$c_ = dechex($c);			// convert to hex number
+	} else {
+		$c_ = $c;
+	}
+# echo ", c_=  $c_  <br>\n";
+	if ($c_ !== $checksum) {
+		$msg = "Different${TAB}size=" . filesize($file) . ", c=$c, c_=$c_, checksum=$checksum";
+		$ret = false;
+		$error = $file;
+	} else {
+		$msg = "Same";
+		$ret = true;
+	}
+	if ($outputResult) {
+		do_return($cmd, $file, $msg);
+	} else {
+		if ($debug2) do_debug($cmd, $msg);
+	}
+
+	return($ret);
 }
 
 ?>

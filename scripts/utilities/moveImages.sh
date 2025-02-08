b5dacaa42ac87d84cf0ff517c84e687abbb73cbf
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-02-08 16:53:55 -0700

    Update moveImages.sh: Only stop/start Allsky if it was running

--- scripts/utilities/moveImages.sh
+++ scripts/utilities/moveImages.sh
@@ -60,10 +60,6 @@ while true; do
 
 	if [[ ${NEW_ALLSKY_IMAGES:0:1} != '/' ]]; then
 		echo -e "\nThe name must begin with '/'.  Try again."
-		echo
-	elif [[ ${NEW_ALLSKY_IMAGES} == "${ALLSKY_IMAGES}" ]]; then
-		echo -e "\nImages are already saved there.  Enter a new location of 'q' to quit."
-		echo
 	else
 		break
 	fi
@@ -83,7 +79,7 @@ if [[ ! -d ${NEW_ALLSKY_IMAGES} ]]; then
 	fi
 
 	if ! E="$( sudo mkdir -p "${NEW_ALLSKY_IMAGES}" 2>&1 )" ; then
-		echo -e "\nERROR: Unable to create '${NEW_ALLSKY_IMAGES}': ${E}\n" >&2
+		E_ "\nERROR: Unable to create '${NEW_ALLSKY_IMAGES}': ${E}\n" >&2
 		exit "${EXIT_ERROR_STOP}"
 	fi
 	display_msg --logonly info "Created '${NEW_ALLSKY_IMAGES}'."
@@ -91,6 +87,22 @@ else
 	display_msg --logonly info "Using existing '${NEW_ALLSKY_IMAGES}'."
 fi
 
+# Get current status of Allsky to determine if we have to stop and restart it.
+STATUS="$( get_allsky_status )"
+if [[ ${STATUS} == "${ALLSKY_STATUS_STARTING}" ||
+	  ${STATUS} == "${ALLSKY_STATUS_RESTARTING}" ||
+	  ${STATUS} == "${ALLSKY_STATUS_RUNNING}" ]]; then
+	ALLSKY_RUNNING="true"
+else
+	ALLSKY_RUNNING="false"
+fi
+
+if [[ ${ALLSKY_RUNNING} == "true" ]]; then
+	stop_Allsky
+	display_msg --log success "Allsky stopped."
+fi
+
+# Reset everything that points to old location.
 if [[ ${ALLSKY_IMAGES_MOVED} == "true" ]]; then
 	if [[ ${NEW_ALLSKY_IMAGES} =~ ${ALLSKY_HOME} ]]; then
 		# Restoring to standard location.
@@ -107,7 +119,7 @@ if [[ ${ALLSKY_IMAGES_MOVED} == "true" ]]; then
 		RET=$?
 	fi
 	if [[ ${RET} -ne 0 ]]; then
-		echo -e "\nERROR: Unable to update '${ALLSKY_USER_VARIABLES}; exiting." >&2
+		E_ "\nERROR: Unable to update '${ALLSKY_USER_VARIABLES}; exiting." >&2
 		exit 1
 	fi
 else
@@ -123,11 +135,14 @@ unset ALLSKY_VARIABLE_SET		# forces variables.sh to be re-read
 #shellcheck source-path=.
 source "${ALLSKY_HOME}/variables.sh"					|| exit "${EXIT_ERROR_STOP}"
 
-# Reset everything that points to old location
 display_msg --logonly info "Making configuration changes."
-stop_Allsky
+
 "${ALLSKY_HOME}/install.sh" --function do_change_images
-start_Allsky
+
+if [[ ${ALLSKY_RUNNING} == "true" ]]; then
+	start_Allsky
+	display_msg --log success "Allsky restarted."
+fi
 
 # If there are any existing files, ask the user what to do.
 NUM=$( find "${SAVED_ALLSKY_IMAGES}" -maxdepth 1 2>/dev/null | wc -l )
@@ -136,7 +151,7 @@ if [[ ${NUM} -gt 0 ]]; then
 	display_msg --logonly info "${NUM} item(s) found in '${SAVED_ALLSKY_IMAGES}'."
 	while true; do
 		echo
-		echo "There are items in '${SAVED_ALLSKY_IMAGES}'.  Should they be:"
+		echo "There are ${NUM} objects in '${SAVED_ALLSKY_IMAGES}'.  Should they be:"
 		echo "1. MOVED to the new location,"
 		echo "2. COPIED to the new location,"
 		echo "3. or left as is?"
@@ -145,7 +160,13 @@ if [[ ${NUM} -gt 0 ]]; then
 		case "${ans}" in
 			1)
 				display_msg --logonly info "  User elected to MOVE items to new location."
-				mv "${SAVED_ALLSKY_IMAGES}"/* "${ALLSKY_IMAGES}"
+				ERR="$( mv "${SAVED_ALLSKY_IMAGES}"/* "${ALLSKY_IMAGES}" 2>&1 )"
+				if [[ $? -ne 0 ]]; then
+					MSG="WARNING: some images didn't get copied;"
+					MSG+=" you must copy them manually:\n${ERR}"
+					W_ "${MSG}"
+				fi
+
 				if [[ ${ALLSKY_IMAGES_MOVED} == "true" ]]; then
 					sudo rmdir "${SAVED_ALLSKY_IMAGES}"
 				fi
@@ -165,17 +186,27 @@ if [[ ${NUM} -gt 0 ]]; then
 				exit 0
 				;;
 			*)
-				echo -e "\nInvalid response.  Enter 1, 2, or 3." >&2
+				E_ "\nInvalid response.  Enter 1, 2, or 3." >&2
 				;;
 		esac
 	done
+else
+	display_msg --logonly info "No items found in '${SAVED_ALLSKY_IMAGES}'."
 fi
 
+MSG="Move completed."
+if [[ ${ALLSKY_RUNNING} == "true" ]]; then
+	MSG2=" Allsky restarted."
+else
+	MSG2=""
+fi
+display_msg --log success "${MSG}" "${MSG2}"
 
 echo
 echo "In order for the change to be visible, go to the WebUI in your browser and press"
 echo "    SHIFT-F5"
 echo "to force it to re-read the new settings."
 echo
+display_msg --logonly info "ENDING changeImages"
 
 exit 0

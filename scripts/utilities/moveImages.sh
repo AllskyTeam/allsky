28fcf3a69c5ba08d4a0bd08a43fc7ca2397a0e45
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-02-10 22:26:47 -0700

    Update moveImages.sh: Use do_exit() when exiting
    It logs the "ENDING" message.
    
    Also:
    * Rename ALLSKY_IMAGES_MOVED to better reflect what it's for.

--- scripts/utilities/moveImages.sh
+++ scripts/utilities/moveImages.sh
@@ -11,18 +11,26 @@ source "${ALLSKY_SCRIPTS}/functions.sh"					|| exit "${EXIT_ERROR_STOP}"
 source "${ALLSKY_SCRIPTS}/installUpgradeFunctions.sh"	|| exit "${EXIT_ERROR_STOP}"
 
 # shellcheck disable=SC2034
-DISPLAY_MSG_LOG="${ALLSKY_LOGS}/moveImages.log"		# display_msg() sends log entries here
+DISPLAY_MSG_LOG="${ALLSKY_LOGS}/moveImages.log"		# display_msg() sends log entries here.
 display_msg --logonly info "STARTING moveImages"
 
-ALLSKY_IMAGES_MOVED="false"
-SAVED_ALLSKY_IMAGES="${ALLSKY_IMAGES}"
+ALLSKY_IMAGES_ALREADY_MOVED="false"					# Did the user already move the images?
+SAVED_ALLSKY_IMAGES="${ALLSKY_IMAGES}"				# Where images are currently kept.
+
+function do_exit()
+{
+	local RET="${1}"
+
+	display_msg --logonly info "ENDING moveImages"
+	exit "${RET}"
+}
 
 # See if ALLSKY_IMAGES has already been set by the user.
 if [[ -s ${ALLSKY_USER_VARIABLES} ]]; then
 	display_msg --logonly info "Found '${ALLSKY_USER_VARIABLES}'."
 
 	# Check if ALLSKY_IMAGES was changed.
-	ALLSKY_IMAGES_MOVED="$(
+	ALLSKY_IMAGES_ALREADY_MOVED="$(
 		unset ALLSKY_IMAGES
 		# shellcheck disable=SC1090,SC1091
 		source "${ALLSKY_USER_VARIABLES}"
@@ -32,35 +40,37 @@ if [[ -s ${ALLSKY_USER_VARIABLES} ]]; then
 			echo "false"
 		fi
 	)"
-	if [[ ${ALLSKY_IMAGES_MOVED} == "true" ]]; then
-		display_msg --logonly info "   ALLSKY_IMAGES already set to '${ALLSKY_IMAGES}'"
+	if [[ ${ALLSKY_IMAGES_ALREADY_MOVED} == "true" ]]; then
+		display_msg --logonly info "  > ALLSKY_IMAGES already set to '${ALLSKY_IMAGES}'"
 		echo
 		echo "It appears you have already moved the Allsky images to '${ALLSKY_IMAGES}'."
 		echo
 		read -r -p "Change location? (yes/no): " ans
 		if [[ ${ans:0:1} != "y" ]]; then
-			display_msg --logonly info "  User elected not to continue."
-			exit 0
+			display_msg --logonly info "  > User elected not to continue."
+			do_exit 0
 		fi
 	else
-		display_msg --logonly info "   ALLSKY_IMAGES not in the file."
+		display_msg --logonly info "  > ALLSKY_IMAGES not in the file."
 	fi
 fi
 
 display_msg --logonly info "Prompting for new location."
 while true; do
+	echo
 	echo "Enter the full pathname to where you want the images to go."
 	echo "The name must begin with '/'."
 	echo
 	read -r -p "Enter pathname: " NEW_ALLSKY_IMAGES
 	if [[ ${NEW_ALLSKY_IMAGES} == 'q' ]]; then
-		display_msg --logonly info "  User elected not to continue."
-		exit 0
+		display_msg --logonly info "  > User elected not to continue."
+		do_exit 0
 	fi
 
 	if [[ ${NEW_ALLSKY_IMAGES:0:1} != '/' ]]; then
 		echo -e "\nThe name must begin with '/'.  Try again."
 	else
+		display_msg --logonly info "  > User entered '${NEW_ALLSKY_IMAGES}'."
 		break
 	fi
 done
@@ -70,22 +80,27 @@ if [[ ! -d ${NEW_ALLSKY_IMAGES} ]]; then
 	echo "'${NEW_ALLSKY_IMAGES}' does not exist."
 	read -r -p "Create it? (yes/no): " ans
 	if [[ ${ans} == "q" || ${ans:0:1} != "y" ]]; then
-		display_msg --logonly info "  User elected not to create '${NEW_ALLSKY_IMAGES}."
+		display_msg --logonly info "  > User elected not to create '${NEW_ALLSKY_IMAGES}."
 		echo
 		echo "Exiting, nothing done."
 		echo "The directory must be created in order to proceed."
 		echo
-		exit 0
+		do_exit 0
 	fi
 
 	if ! E="$( sudo mkdir -p "${NEW_ALLSKY_IMAGES}" 2>&1 )" ; then
-		E_ "\nERROR: Unable to create '${NEW_ALLSKY_IMAGES}': ${E}\n" >&2
-		exit "${EXIT_ERROR_STOP}"
+		MSG="Unable to create '${NEW_ALLSKY_IMAGES}'"
+		display_msg --log error "${MSG}:" "${E}"
+		do_exit "${EXIT_ERROR_STOP}"
 	fi
-	display_msg --logonly info "Created '${NEW_ALLSKY_IMAGES}'."
+	display_msg --logonly info "  > Created '${NEW_ALLSKY_IMAGES}'."
 else
-	display_msg --logonly info "Using existing '${NEW_ALLSKY_IMAGES}'."
+	display_msg --logonly info "  > '${NEW_ALLSKY_IMAGES}' already exists - will use it."
 fi
+
+# Do this even if the directory already existed, "just in case".
+sudo chown "${ALLSKY_OWNER}":"${ALLSKY_OWNER}" "${NEW_ALLSKY_IMAGES}"
+chmod 775 "${NEW_ALLSKY_IMAGES}"
 
 # Get current status of Allsky to determine if we have to stop and restart it.
 STATUS="$( get_allsky_status )"
@@ -99,18 +114,18 @@ fi
 
 if [[ ${ALLSKY_RUNNING} == "true" ]]; then
 	stop_Allsky
-	display_msg --log success "Allsky stopped."
+	display_msg --logonly info "Allsky stopped."
 fi
 
 # Reset everything that points to old location.
-if [[ ${ALLSKY_IMAGES_MOVED} == "true" ]]; then
+if [[ ${ALLSKY_IMAGES_ALREADY_MOVED} == "true" ]]; then
 	if [[ ${NEW_ALLSKY_IMAGES} =~ ${ALLSKY_HOME} ]]; then
 		# Restoring to standard location.
 		sed -i -e '/^ALLSKY_IMAGES=/d' "${ALLSKY_USER_VARIABLES}"
 		RET=$?
 		if [[ ! -s ${ALLSKY_USER_VARIABLES} ]]; then
 			display_msg --logonly info "'${ALLSKY_USER_VARIABLES}' is now empty so removing"
-			sudo rm "${ALLSKY_USER_VARIABLES}"
+			rm "${ALLSKY_USER_VARIABLES}"
 		fi
 	else
 		sed -i \
@@ -119,16 +134,13 @@ if [[ ${ALLSKY_IMAGES_MOVED} == "true" ]]; then
 		RET=$?
 	fi
 	if [[ ${RET} -ne 0 ]]; then
-		E_ "\nERROR: Unable to update '${ALLSKY_USER_VARIABLES}; exiting." >&2
-		exit 1
+		display_msg --log error "Unable to update '${ALLSKY_USER_VARIABLES}; exiting."
+		do_exit 1
 	fi
 else
 	echo "ALLSKY_IMAGES='${NEW_ALLSKY_IMAGES}'" >> "${ALLSKY_USER_VARIABLES}"
 	chmod 664 "${ALLSKY_USER_VARIABLES}"
 fi
-
-sudo chown "${ALLSKY_OWNER}":"${ALLSKY_OWNER}" "${NEW_ALLSKY_IMAGES}"
-chmod 775 "${NEW_ALLSKY_IMAGES}"
 
 # If the new location is in ${ALLSKY_HOME}, assume
 unset ALLSKY_VARIABLE_SET		# forces variables.sh to be re-read
@@ -167,7 +179,7 @@ if [[ ${NUM} -gt 0 ]]; then
 					W_ "${MSG}"
 				fi
 
-				if [[ ${ALLSKY_IMAGES_MOVED} == "true" ]]; then
+				if [[ ${ALLSKY_IMAGES_ALREADY_MOVED} == "true" ]]; then
 					sudo rmdir "${SAVED_ALLSKY_IMAGES}"
 				fi
 				break
@@ -183,7 +195,7 @@ if [[ ${NUM} -gt 0 ]]; then
 				;;
 			"q")
 				display_msg --logonly info "  User elected not to continue."
-				exit 0
+				do_exit 0
 				;;
 			*)
 				E_ "\nInvalid response.  Enter 1, 2, or 3." >&2
@@ -207,6 +219,5 @@ echo "In order for the change to be visible, go to the WebUI in your browser and
 echo "    SHIFT-F5"
 echo "to force it to re-read the new settings."
 echo
-display_msg --logonly info "ENDING moveImages"
 
-exit 0
+do_exit 0

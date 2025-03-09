caee438d39d5c6959730d7eb9dbf6582564febc0
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-03-08 19:10:19 -0700

    Update badImagesInfo.sh: Fix ALLSKY_HOME location
    And put setting name in quotes.

--- scripts/utilities/badImagesInfo.sh
+++ scripts/utilities/badImagesInfo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-[[ -z ${ALLSKY_HOME} ]] && export ALLSKY_HOME="$( realpath "$( dirname "${BASH_ARGV0}" )" )"
+[[ -z ${ALLSKY_HOME} ]] && export ALLSKY_HOME="$( realpath "$(dirname "${BASH_ARGV0}")/../.." )"
 ME="$( basename "${BASH_ARGV0}" )"
 
 #shellcheck source-path=.
@@ -61,7 +60,7 @@ echo "$INFO" | gawk '
 		}
 	}
 	END {
-		name = "Remove Bad Images Threshold"
+		name = "\"Remove Bad Images Threshold\""
 		if (low_count > 0) {
 			printf("%d image", low_count);
 			if (low_count > 1) printf("s");

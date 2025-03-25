e597c299297045bd3a85c2384a958982288a1286
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-03-25 00:15:56 -0600

    Update support.sh: Use dialog_box() instead of "dialog".
    * Use showSupportedCameras.sh instead of hard-coding what it does.
    * dialog_box() is used by installRemoteWebsite.sh and displays colors and is simplier to use.
    * enter_yes_no() is no longer needed with dialog_box().
    * Had to rename TEXT_MODE to TEXT_ONLY and AUTO_MODE to AUTO_CONFIRM to work with dialog_box().
    * Added set_messages() to create the dialog messages that work in text mode or normal mode.
    * Make sure ALLSKY_CONFIG exists before using it.
    * Use ">" to create files instead of ">>" in case the files already exist.

--- support.sh
+++ support.sh
@@ -17,32 +17,61 @@ ME="$( basename "${BASH_ARGV0}" )"
 source "${ALLSKY_HOME}/variables.sh"					|| exit "${EXIT_ERROR_STOP}"
 #shellcheck source-path=scripts
 source "${ALLSKY_SCRIPTS}/functions.sh"					|| exit "${EXIT_ERROR_STOP}"
+#shellcheck source-path=scripts
+source "${ALLSKY_SCRIPTS}/installUpgradeFunctions.sh"	|| exit "${EXIT_ERROR_STOP}"
 
-LOG_LINES="all"
 SUPPORT_DATETIME_SHORT="$( date +"%Y%m%d%H%M%S" )"
 SUPPORT_ZIP_NAME="support-XX_ISSUE_XX-${SUPPORT_DATETIME_SHORT}.zip"
-SUPPORT_DIR="${ALLSKY_WEBUI}/support"
-DIALOG_COMPLETE_MESSAGE="The support information has now been generated and can be found in\n\n
-${SUPPORT_DIR}/XX_ZIPNAME_XX\n\n
-Due to the size of the support information, the file has been compressed.\n\n
-This file should be attached to the relevant Discussion in Github.\n\n
-If your WebUI is functioning the the file can be downloaded using the 'Getting Support' page."
-GITHUB_ERROR="ERROR: The Discussion number must be numberic.\n\n
-It can be found in the URL of the Github issue, for example in this case\n\n
-${GITHUB_ROOT}/${GITHUB_ALLSKY_PACKAGE}/discussions/4119\n\n
-The discussion number would be 4119."
-SUPPORT_TCS="\nThis script will collect the following non-personal data from\n
-your Raspberry Pi to assist in supporting problems:\n\n
-- Basic system information\n
-- Filesystem, memory, and network information (IP and MAC details are obfuscated)\n
-- Installed system and python packages\n
-- Allsky and web logs\n
-- Connected camera details\n
-- i2c bus details\n
-- Running processes\n
-- Allsky config files (obfuscated where required to hide any credentials)\n\n
-Select 'Yes' to agree or 'No' to abort this script.
-"
+ALLSKY_SUPPORT_DIR="${ALLSKY_WEBUI}/support"
+
+
+############################################## functions
+
+function set_dialog_info()
+{
+	# Get DIALOG_WIDTH and DIALOG_HEIGHT
+	calc_d_sizes
+
+	if [[ ${TEXT_ONLY} == "true" ]]; then
+		DIALOG_ERROR="${cERROR}"
+		DIALOG_BLUE="${cBLUE}"
+		DIALOG_BOLD="${cBOLD}"
+		DIALOG_NC="${cNC}"
+	fi
+}
+
+function set_messages()
+{
+	DIALOG_COMPLETE_MESSAGE="\nThe support information is in:\n"
+	DIALOG_COMPLETE_MESSAGE+="\n"
+	DIALOG_COMPLETE_MESSAGE+="    ${DIALOG_BLUE}${ALLSKY_SUPPORT_DIR}/XX_ZIPNAME_XX${DIALOG_NC}\n"
+	DIALOG_COMPLETE_MESSAGE+="\n"
+	DIALOG_COMPLETE_MESSAGE+="This file should be attached to the relevant Discussion in Github.\n"
+	DIALOG_COMPLETE_MESSAGE+="\n"
+	DIALOG_COMPLETE_MESSAGE+="If your WebUI is functioning, the file can be downloaded using the"
+	DIALOG_COMPLETE_MESSAGE+=" ${DIALOG_BOLD}Getting Support${DIALOG_NC} page."
+
+	GITHUB_ERROR="${DIALOG_ERROR}\nERROR: The Discussion number must be numeric.${DIALOG_NC}\n"
+	GITHUB_ERROR+="\n"
+	GITHUB_ERROR+="It can be found in the URL of the Github post, for example if the URL is:\n"
+	GITHUB_ERROR+="\n"
+	GITHUB_ERROR+="    ${DIALOG_BLUE}${GITHUB_ROOT}/${GITHUB_ALLSKY_PACKAGE}/discussions/4119${DIALOG_NC}\n"
+	GITHUB_ERROR+="\n"
+	GITHUB_ERROR+="the Discussion number is ${DIALOG_BOLD}4119${DIALOG_NC}."
+
+	SUPPORT_TCS="\n${DIALOG_BOLD}This script collects the following data from your Raspberry Pi to"
+	SUPPORT_TCS+=" assist in supporting problems (all private data is hidden):${DIALOG_NC}\n"
+	SUPPORT_TCS+="\n"
+	SUPPORT_TCS+="- Basic system information\n"
+	SUPPORT_TCS+="- Filesystem, memory, and network information\n"
+	SUPPORT_TCS+="- Installed system and python packages\n"
+	SUPPORT_TCS+="- Allsky and web logs\n"
+	SUPPORT_TCS+="- Connected camera details\n"
+	SUPPORT_TCS+="- i2c bus details\n"
+	SUPPORT_TCS+="- Running processes\n"
+	SUPPORT_TCS+="- Allsky configuration files\n"
+	SUPPORT_TCS+="\n"
+}
 
 function print_info()
 {
@@ -67,33 +96,6 @@ function print_heading()
 	printf "%-20s\n" "============================"
 }
 
-############################################## functions
-
-# Prompt the user to enter (y)/(yes) or (n)/(no).
-# This function is only used when running in text (--text) mode.
-function enter_yes_no()
-{
-	local TEXT="${1}"
-	local RESULT=1
-	local ANSWER
-
-	while true; do
-		echo -e "${TEXT}"
-		read -r -p "Do you want to continue? (y/n): " ANSWER
-		ANSWER="${ANSWER,,}"	# convert to lowercase
-
-		if [[ ${ANSWER} == "y" || ${ANSWER} == "yes" ]]; then
-			return 0
-		elif [[ ${ANSWER} == "n" || ${ANSWER} == "no" ]]; then
-			return 1
-		else
-			E_ "\nInvalid response. Please enter y/yes or n/no."
-		fi
-	done
-
-	return "${RESULT}"
-}
-
 function print_sub_heading(){
 	local LABEL="${1}"
 
@@ -112,12 +114,12 @@ function collect_support_info()
 	###
 
 	### Misc information
-	UPTIME=$( uptime )
+	UPTIME="$( uptime )"
 	###
 
 	### User information
-	USER_NAME=$( id -un )
-	USER_ID=$( id -u )
+	USER_NAME="$( id -un )"
+	USER_ID="$( id -u )"
 	###
 
 	### Hardware Information
@@ -148,12 +150,8 @@ function collect_support_info()
 
 	### File system information
 	FILE_SYSTEMS="$( df -h )"
-	#TODO: GET AS image dir sizes
+	# TODO: GET AS image dir sizes
 	###
-
-	### Allsky file information
-	cd "${ALLSKY_HOME}" || null
-	ALLSKY_FILES="$( tree -ugp --gitignore --prune -I '.git|__pycache__' "${ALLSKY_HOME}" )"
 
 	X="${ALLSKY_PYTHON_VENV}/bin/activate"
 	if [[ -s ${X} ]]; then
@@ -184,7 +182,7 @@ function collect_support_info()
 	###
 
 	### pi Camera stuff
-	PI_CAMERAS="$( sudo libcamera-still --list-cameras )"
+	PI_CAMERAS="$( libcamera-still --list-cameras 2> /dev/null )"
 	###
 
 	### get installed package information
@@ -196,8 +194,7 @@ function collect_support_info()
 
 function generate_support_info()
 {
-	local TEMP_FOLDER="${TMPDIR:-/tmp}"
-	local TEMP_DIR=$( mktemp -d "${TEMP_FOLDER}"/allskyXXXXX )
+	local TEMP_DIR="$( mktemp -d "${TMPDIR:-/tmp}/allsky_XXX" )"
 
 	local BASIC_FILE="${TEMP_DIR}/system.txt"
 	{
@@ -216,68 +213,67 @@ function generate_support_info()
 		print_info "Total RAM:" "${MEM_TOTAL}"
 		print_info "User Name:" "${USER_NAME}"
 		print_info "User ID:" "${USER_ID}"
-	} >> "${BASIC_FILE}"
+	} > "${BASIC_FILE}"
 
 	local ISSUE_FILE="${TEMP_DIR}/issue.txt"
 	{
 		print_heading "Github Issue"
 		print "${ISSUE_NUMBER}"
-	} >> "${ISSUE_FILE}"
+	} > "${ISSUE_FILE}"
 
 	local OS_FILE="${TEMP_DIR}/ps.txt"
 	{
 		print_heading "Process Information"
 		print "${PS}"
-	} >> "${OS_FILE}"
+	} > "${OS_FILE}"
 
 	local MEMORY_FILE="${TEMP_DIR}/memory.txt"
 	{
 		print_heading "Memory Info"
 		print "${MEMORY_INFO}"
-	} >> "${MEMORY_FILE}"
+	} > "${MEMORY_FILE}"
 
 	local NETWORK_FILE="${TEMP_DIR}/network.txt"
 	{
 		print_heading "Network Info"
 		print "${NETWORKS}"
-	} >> "${NETWORK_FILE}"
+	} > "${NETWORK_FILE}"
 
 	local FILESYSTEM_FILE="${TEMP_DIR}/filesystem.txt"
 	{
 		print_heading "File Systems"
 		print "${FILE_SYSTEMS}"
-	} >> "${FILESYSTEM_FILE}"
+	} > "${FILESYSTEM_FILE}"
 
 	local DEVICES_FILE="${TEMP_DIR}/devices.txt"
 	{
 		print_heading "Devices"
 		print "${DEV}"
-	} >> "${DEVICES_FILE}"
+	} > "${DEVICES_FILE}"
 
 	local USB_FILE="${TEMP_DIR}/usb.txt"
 	{
 		print_heading "USB Devices"
 		print "${USB}"
-	} >> "${USB_FILE}"
+	} > "${USB_FILE}"
 
 	local LIBCAMERA_FILE="${TEMP_DIR}/libcamera.txt"
 	{
 		print_heading "Libcamera Devices"
 		print "${PI_CAMERAS}"
-	} >> "${LIBCAMERA_FILE}"
+	} > "${LIBCAMERA_FILE}"
 
 	local i2C_FILE="${TEMP_DIR}/i2c.txt"
 	{
 		print_heading "I2C Devices"
 		print "${I2C_DEVICES}"
-	} >> "${i2C_FILE}"
+	} > "${i2C_FILE}"
 
 	local ALLSKYFILES_FILE="${TEMP_DIR}/allsky_files.txt"
 	{
-		print_heading "Allsky Files"
-		print_info "Allsky Version:" "${ALLSKY_VERSION}"
-		print "${ALLSKY_FILES}"
-	} >> "${ALLSKYFILES_FILE}"
+		print_heading "Files in ${ALLSKY_HOME}"
+		tree -ugp --gitignore --prune -I '.git|__pycache__' "${ALLSKY_HOME}"
+	} > "${ALLSKYFILES_FILE}"
 
 	local ALLSKYVENV_FILE="${TEMP_DIR}/allsky_venv.txt"
 	{
@@ -286,13 +282,13 @@ function generate_support_info()
 		print "${PYTHON_MODULES}"
 		print_heading "Package Information"
 		print "${PYTHON_PACKAGES}"
-	}  >> "${ALLSKYVENV_FILE}"
+	}  > "${ALLSKYVENV_FILE}"
 
 	local APT_FILE="${TEMP_DIR}/apt.txt"
 	{
 		print_heading "APT installed packages"
 		print "${APT_INSTALLED}"
-	} >> "${APT_FILE}"
+	} > "${APT_FILE}"
 
 	local LIGHTTPD_ERROR_LOG_FILE="${TEMP_DIR}/lighttpd_error.txt"
 	local LIGHTTPD_ERROR_LOG="/var/log/lighttpd/error.log"
@@ -308,34 +304,8 @@ function generate_support_info()
 		# sudo "${ALLSKY_UTILITIES}/getRPiCameraInfo.sh"
 		# sudo cat ./tmp/camera_data.txt
 
-		print_sub_heading "Pi Cameras"
-# TODO: FIX: use utility
-		gawk -F'\t' '
-			BEGIN {
-				printf("%-25s Sensor\n", "Camera Name");
-				printf("%-25s-------\n", "--------------------------");
-			}
-			{
-				if ($1 == "camera") {
-					sensor = $2;
-					compare_length = $3
-					model = $4;
-					if (compare_length > 0)
-						other = " and related sensors";
-					else
-						other = "";
-					printf("%-25s %s%s\n", model, sensor, other);
-				}
-			}' "${RPi_SUPPORTED_CAMERAS}"
-
-		print_sub_heading "ZWO Cameras"
-# TODO: FIX: use utility
-		strings "${ALLSKY_HOME}/src/lib/armv7/libASICamera2.a" |
-			grep '_SetResolutionEv$' | \
-			sed -e 's/^.*CameraS//' -e 's/17Cam//' -e 's/_SetResolutionEv//' | \
-			sort -u
-
-	} >> "${SUPPORTED_CAMERAS_FILE}"
+		"${ALLSKY_UTILITIES}/showSupportedCameras.sh" --rpi --zwo
+	} > "${SUPPORTED_CAMERAS_FILE}"
 
 	local ALLSKY_LOG_FILE="${TEMP_DIR}/allsky_log.txt"
 	if [[ -f ${ALLSKY_LOG} ]]; then
@@ -358,20 +328,24 @@ function generate_support_info()
 	[[ -d ${ALLSKY_CONFIG} ]] && cp -ar "${ALLSKY_CONFIG}" "${TEMP_DIR}"
 
 	# Truncate large files not needed for support.
-	truncate -s 0 "${TEMP_DIR}/config/overlay/config/tmp/overlay/de421.bsp"
+	local X="${TEMP_DIR}/config/overlay/config/tmp/overlay/de421.bsp"
+	[[ -s ${X} ]] && truncate -s 0 "${X}"
+	X="${TEMP_DIR}/config/overlay/system_fonts"
+	[[ -d ${X} ]] && find "${TEMP_DIR}/config/overlay/system_fonts" -type f -exec truncate -s 0 {} +
+
 	# Truncate all of the module configs until we can obfuscate any sensitive data.
-	find "${TEMP_DIR}/config/modules" -type f -exec truncate -s 0 {} +
-	find "${TEMP_DIR}/config/overlay/system_fonts" -type f -exec truncate -s 0 {} +
+	X="${TEMP_DIR}/config/modules"
+	[[ -d ${X} ]] && find "${TEMP_DIR}/config/modules" -type f -exec truncate -s 0 {} +
 
 	SUPPORT_ZIP_NAME="${SUPPORT_ZIP_NAME//XX_ISSUE_XX/${ISSUE_NUMBER}}"
 	DIALOG_COMPLETE_MESSAGE="${DIALOG_COMPLETE_MESSAGE//XX_ZIPNAME_XX/${SUPPORT_ZIP_NAME}}"
 
-	cd "${TEMP_DIR}" || exit 1
-	zip -r "${SUPPORT_ZIP_NAME}" ./* > /dev/null 2>&1
-	sudo chown "${USER_NAME}":www-data "${SUPPORT_ZIP_NAME}"
+	SUPPORT_ZIP_NAME="${TEMP_DIR}/${SUPPORT_ZIP_NAME}"
+	zip -r "${SUPPORT_ZIP_NAME}" "${TEMP_DIR}/"* > /dev/null 2>&1
+	sudo chown "${USER_NAME}:${WEBSERVER_OWNER}" "${SUPPORT_ZIP_NAME}"
 	sudo chmod g+wx "${SUPPORT_ZIP_NAME}"
 	sudo chmod u+wx "${SUPPORT_ZIP_NAME}"
-	sudo mv "${SUPPORT_ZIP_NAME}" "${SUPPORT_DIR}"
+	sudo mv "${SUPPORT_ZIP_NAME}" "${ALLSKY_SUPPORT_DIR}"
 	trap 'rm -rf "${TEMP_DIR}"' EXIT
 }
 
@@ -381,25 +355,19 @@ function get_github_discussion_id()
 {
 	local ISSUE_NUMBER_TEMP
 
-	if [[ ${AUTO_MODE} == "false"  && ${ISSUE_NUMBER} == "none" ]]; then
+	if [[ ${AUTO_CONFIRM} == "false"  && ${ISSUE_NUMBER} == "none" ]]; then
+		local MSG="${DIALOG_BOLD}Enter the Github Discussion Number.${DIALOG_NC}\nPress 'Enter' if you don't know it: "
+		local DIALOG_TITLE="Github Discussion Number"
 		while true; do
-			if [[ ${TEXT_MODE} == "false" ]]; then
-				ISSUE_NUMBER_TEMP="$( dialog --inputbox "Github Discussion Number:\nIf you don't know it just hit Enter." 8 50 2>&1 >/dev/tty )"
-			else
-				echo -e "Enter the Github Discussion number:\nIf you don't know it just hit Enter.\n"
-				read -r -p "Github Discussion number " ISSUE_NUMBER_TEMP
-			fi
+			# The prompt is written to stdout and the answer to stderr, so switch them.
+			ISSUE_NUMBER_TEMP="$( display_box "--inputbox" "${DIALOG_TITLE}" "\n${MSG}" --no-cancel 3>&1 1>&2 2>&3 )"
 
 			if [[ -n ${ISSUE_NUMBER_TEMP} ]]; then
 				if [[ ${ISSUE_NUMBER_TEMP} =~ ^[+-]?[0-9]+$ ]]; then
 					ISSUE_NUMBER="${ISSUE_NUMBER_TEMP}"
 					break
 				else
-					if [[ ${TEXT_MODE} == "false" ]]; then
-						dialog --msgbox "${GITHUB_ERROR}" 15 70
-					else
-						E_ "${GITHUB_ERROR}\n\n"
-					fi
+					display_box "--msgbox" "${DIALOG_TITLE}" "${GITHUB_ERROR}"
 				fi
 			else
 				break
@@ -411,44 +379,28 @@ function get_github_discussion_id()
 PROCESS_DIALOG=""
 display_running_dialog()
 {
-	if [[ ${AUTO_MODE} == "false" && ${TEXT_MODE} == "false" ]]; then
-		(
-			dialog --title "Processing" --msgbox "\n\n\nPlease wait while the support info is collected..." 10 60
-		) &
-		PROCESS_DIALOG=$!		# Global
-	else
-		echo "Please wait while the support info is collected..."
+	if [[ "${AUTO_CONFIRM}" == "false" ]]; then
+		DIALOG_TITLE="Processing"
+		DIALOG_TEXT="${DIALOG_BOLD}Please wait while the support info is collected...${DIALOG_NC}"
+		display_box "--infobox" "${TITLE}" "\n\n${DIALOG_TEXT}\n"
 	fi
 }
 
 display_complete_dialog()
 {
-	if [[ ${AUTO_MODE} == "false" ]]; then
-		local DIALOG_MESSAGE="${DIALOG_COMPLETE_MESSAGE}"
-		local DIALOG_HEIGHT=20
-
-		if [[ ${TEXT_MODE} == "false" ]]; then
-			dialog --title "Complete" --msgbox "${DIALOG_MESSAGE}" "${DIALOG_HEIGHT}" 70
-		else
-			echo -e "${DIALOG_MESSAGE}"
-		fi
+	if [[ ${AUTO_CONFIRM} == "false" ]]; then
+		display_box "--msgbox" "Complete" "${DIALOG_COMPLETE_MESSAGE}"
 	fi
 }
 
 display_start_dialog()
 {
-	if [[ ${AUTO_MODE} == "false" ]]; then
-		local RESULT
+	if [[ ${AUTO_CONFIRM} == "false" ]]; then
 		clear
-		if [[ ${TEXT_MODE} == "false" ]]; then
-			dialog --title "Generate Support Information" --yesno "${SUPPORT_TCS}" 25 80
-			RESULT=$?
-		else
-			enter_yes_no "${SUPPORT_TCS}"
-			RESULT=$?
+		if [[ ${TEXT_ONLY} == "false" ]]; then
+			SUPPORT_TCS+="Do you want to continue?\n"
 		fi
-
-		if [[ ${RESULT} -eq 1 ]]; then
+		if ! display_box "--yesno" "Generate Support Information" "${SUPPORT_TCS}" ; then
 			exit 1
 		fi
 	fi
@@ -482,10 +434,14 @@ function usage_and_exit()
 	exit "${RET}"
 }
 
+
+############################################## main body
+
 OK="true"
-TEXT_MODE="false"
+TEXT_ONLY="false"
 ISSUE_NUMBER="none"
-AUTO_MODE="false"
+AUTO_CONFIRM="false"
+LOG_LINES="all"
 
 while [[ $# -gt 0 ]]; do
 	ARG="${1}"
@@ -495,11 +451,11 @@ while [[ $# -gt 0 ]]; do
 			;;
 
 		--text)
-			TEXT_MODE="true"
+			TEXT_ONLY="true"
 			;;
 
 		--auto)
-			AUTO_MODE="true"
+			AUTO_CONFIRM="true"
 			;;
 
 		--issue)
@@ -513,9 +469,7 @@ while [[ $# -gt 0 ]]; do
 				LOG_LINES="${LOG_LINES_TEMP}"
 			else
 				LOG_LINES_TEMP="${LOG_LINES_TEMP,,}"
-				if [[ ${LOG_LINES_TEMP} == "all" ]]; then
-					LOG_LINES="all"
-				else
+				if [[ ${LOG_LINES_TEMP} != "all" ]]; then
 					E_ "ERROR: ${ARG} must be numeric or 'all'." >&2
 					OK="false"
 				fi
@@ -529,6 +483,8 @@ done
 [[ ${DO_HELP} == "true" ]] && usage_and_exit 0
 [[ ${OK} == "false" ]] && usage_and_exit 1
 
+set_dialog_info
+set_messages
 display_start_dialog
 get_github_discussion_id
 display_running_dialog

64bea394d7f8be9c8d428ce00d52fe6fb5772cbc
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-06-12 17:27:23 -0500

    Update supportutils.php: Return status
    Don't assume everything worked and return "ok".
    
    Also:
    * Replace spaces with tabs.

--- html/includes/supportutils.php
+++ html/includes/supportutils.php
@@ -7,207 +7,222 @@ include_once('authenticate.php');
 
 class SUPPORTUTIL
 {
-    private $request;
-    private $method;
-    private $jsonResponse = false;
-    private $issueDir;
+	private $request;
+	private $method;
+	private $jsonResponse = false;
+	private $issueDir;
 
-    function __construct() {
-        $this->issueDir = ALLSKY_SUPPORT_DIR;
+	function __construct() {
+		$this->issueDir = ALLSKY_SUPPORT_DIR;
 		if (! is_dir($this->issueDir)) {
-			$msg = "ERROR - Directory '" . $this->issueDir . "' not found!\n";
-			$this->send500($msg);
+			$msg = "Directory '" . $this->issueDir . "' not found!";
+			$this->send404($msg);
 		}
-    }
+	}
 
-    public function run()
-    {
-        $this->checkXHRRequest();
-        $this->sanitizeRequest();
-        $this->runRequest();
-    }
+	public function run()
+	{
+		$this->checkXHRRequest();
+		$this->sanitizeRequest();
+		$this->runRequest();
+	}
 
-    private function checkXHRRequest()
-    {
-        $var = "HTTP_X_REQUESTED_WITH";
+	private function checkXHRRequest()
+	{
+		$var = "HTTP_X_REQUESTED_WITH";
 		$val = "";
-        if (empty($_SERVER[$var])) {
-            $this->send404("$var not set");
+		if (empty($_SERVER[$var])) {
+			$this->send404("$var not set");
 		} else {
 			$val = strtolower($_SERVER[$var]);
 			$v = "xmlhttprequest";
 			if ($val != $v) {
-            	$this->send404("$var ($val) != $v");
+				$this->send404("$var ($val) != $v");
 			}
-        }
-    }
-
-    private function sanitizeRequest()
-    {
-        $this->request = $_GET['request'];
-        $this->method = strtolower($_SERVER['REQUEST_METHOD']);
-
-        $accepts = $_SERVER['HTTP_ACCEPT'];
-        if (stripos($accepts, 'application/json') !== false) {
-            $this->jsonResponse = true;
-        }
-    }
-
-    private function send404($msg = null)
-    {
-        header('HTTP/1.0 404 Not Found');
-		if ($msg !== null) {
-        	header("Content-Description: $msg");
 		}
-        die();
-    }
+	}
 
-    private function send500($msg = null)
-    {
-        header('HTTP/1.0 500 Internal Server Error');
-		if ($msg !== null) {
-        	header("Content-Description: $msg");
-        	// TODO $this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
+	private function sanitizeRequest()
+	{
+		$this->request = $_GET['request'];
+		$this->method = strtolower($_SERVER['REQUEST_METHOD']);
+
+		$accepts = $_SERVER['HTTP_ACCEPT'];
+		if (stripos($accepts, 'application/json') !== false) {
+			$this->jsonResponse = true;
 		}
-        die();
-    }
+	}
 
-    private function sendResponse($response = 'ok')
-    {
-        echo ($response);
-        die();
-    }
+	private function send404($msg = null)
+	{
+		if ($msg !== null) {
+			$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
+			die();
+		}
+		header('HTTP/1.0 404 Not Found');
+		if ($msg !== null) {
+			header("Content-Description: $msg");
+		}
+		die();
+	}
 
-    private function runRequest()
-    {
-        $action = $this->method . $this->request;
-        if (is_callable(array('SUPPORTUTIL', $action))) {
-            call_user_func(array($this, $action));
-        } else {
-            $this->send404($this->request . " is not callable.");
-        }
-    }
+	private function send500($msg = null)
+	{
+		header('HTTP/1.0 500 Internal Server Error');
+		if ($msg !== null) {
+			header("Content-Description: $msg");
+		}
+		die();
+	}
 
-    private function humanReadableFileSize($bytes, $decimals = 2) {
-        $sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
-        $factor = floor((strlen($bytes) - 1) / 3);
-        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . ' ' . $sizes[$factor];
-    }
+	private function sendResponse($response = 'ok')
+	{
+		echo ($response);
+		die();
+	}
 
-    public function postDownloadLog() {
-        $logId = $_POST['logId'];
-        $logId = basename($logId);
-        $fromFile = $this->issueDir . DIRECTORY_SEPARATOR . $logId;
+	private function runRequest()
+	{
+		$action = $this->method . $this->request;
+		if (is_callable(array('SUPPORTUTIL', $action))) {
+			call_user_func(array($this, $action));
+		} else {
+			$this->send404($this->request . " is not callable.");
+		}
+	}
+
+	private function humanReadableFileSize($bytes, $decimals = 2) {
+		$sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+		$factor = floor((strlen($bytes) - 1) / 3);
+		return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . ' ' . $sizes[$factor];
+	}
+
+	public function postDownloadLog() {
+		$logId = $_POST['logId'];
+		$logId = basename($logId);
+		$fromFile = $this->issueDir . DIRECTORY_SEPARATOR . $logId;
 // TODO: check if $fromFile exists
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="' . basename($fromFile) . '"');
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($fromFile));
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="' . basename($fromFile) . '"');
+		header('Content-Transfer-Encoding: binary');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($fromFile));
 		if ( ! readfile($fromFile)) {
 			echo "ERROR: Unable to read '$fromFile'";
 		}
-        exit;
-    }
+		exit;
+	}
 
-    public function postChangeGithubId() {
-        $logId = $_POST['logId'];
-        $logId = basename($logId);
+	public function postChangeGithubId() {
+		$logId = $_POST['logId'];
+		$logId = basename($logId);
 
-        $githubId = $_POST['githubid'];
+		$githubId = $_POST['githubid'];
 
-        $nameParts = explode('-', $logId);
-        $newLogId = $nameParts[0] . '-' . $githubId . '-' . $nameParts[2];
+		$nameParts = explode('-', $logId);
+		$newLogId = $nameParts[0] . '-' . $githubId . '-' . $nameParts[2];
 
-        $fromFile = $this->issueDir . DIRECTORY_SEPARATOR . $logId;
-        $newFile = $this->issueDir . DIRECTORY_SEPARATOR . $newLogId;
+		$fromFile = $this->issueDir . DIRECTORY_SEPARATOR . $logId;
+		$newFile = $this->issueDir . DIRECTORY_SEPARATOR . $newLogId;
 
 		if ( ! file_exists($fromFile)) {
-        	$msg = "'$fromFile' does not exist.";
-        	$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
+			$msg = "'$fromFile' does not exist.";
+			$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
 		} else if (rename($fromFile, $newFile)) {
-        	$this->sendResponse(json_encode(array("status"=>"ok")));
+			$this->sendResponse(json_encode(array("status"=>"ok")));
 		} else {
-        	$msg = "Could not rename($fromFile, $newFile)";
-        	$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
+			$msg = "Could not rename($fromFile, $newFile)";
+			$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
 		}
-    }
+	}
 
-    public function postDeleteLog() {
-        $logId = $_POST['logId'];
-        $logId = basename($logId);
-        
-        $fileToDelete = $this->issueDir . DIRECTORY_SEPARATOR . $logId;
-        if (@unlink($fileToDelete)) {
-        	$this->sendResponse(json_encode(array("status"=>"ok")));
+	public function postDeleteLog() {
+		$logId = $_POST['logId'];
+		$logId = basename($logId);
+
+		$fileToDelete = $this->issueDir . DIRECTORY_SEPARATOR . $logId;
+		if (@unlink($fileToDelete)) {
+			$this->sendResponse(json_encode(array("status"=>"ok")));
 		} else {
-        	$msg = "Could not unlink($fileToDelete)";
-        	$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
+			$msg = "Could not unlink($fileToDelete)";
+			$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
 		}
-    }
+	}
 
-    public function getSupportFilesList() {
+	public function getSupportFilesList() {
 
-        $data = array();
-        
+		$data = array();
+
 		if ( ! is_dir($this->issueDir)) {
-        	$sg = "Directory '" . $this->issueDir . "' does not exist.";
-        	$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
+			$sg = "Directory '" . $this->issueDir . "' does not exist.";
+			$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
 			return;
 		}
-        $files = scandir($this->issueDir);
+		$files = scandir($this->issueDir . "x");
 		if ($files === false) {
-        	$msg = "Could not scandir({$this->issueDir})";
-        	$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
-			return;
+			$msg = "scandir({$this->issueDir}) failed.";
+			// TODO  $data = array("status"=>"ERROR", "message"=>"$msg");
+			$data[] = [
+				"filename" => "",
+				"sortfield" => "",
+				"date" => "<br><p class='errorMsgBox errorMsg'>$msg</p>",
+				"ID" => "",
+				"size" => "",
+				"actions" => ""
+			];
+		} else {
+			// file format:  support-none-20250612160748.zip
+			foreach ($files as $file) {
+				if (strpos($file, '.') === 0) {
+					continue;
+				}
+				$fileBits = explode("-", $file);
+				$problemType = $fileBits[0];
+				if ($problemType !== "support" && $problemType !== "Discussion" && $problemType !== "Issue") {
+					continue;
+				}
+
+				$GitHubID = $fileBits[1];					// GitHub ID
+				$date = explode(".", $fileBits[2])[0];	// date
+				$year = substr($date, 0, 4);
+				$month = substr($date, 4, 2);
+				$day = substr($date, 6, 2);
+				$hour = substr($date, 8, 2);
+				$minute = substr($date, 10, 2);
+				$second = substr($date, 12, 2);
+			   	$timestamp = mktime($hour, $minute, $second, $month, $day, $year);
+			   	$formattedDate = strftime("%A %d %B %Y, %H:%M", $timestamp);
+
+			   	$size = filesize($this->issueDir . DIRECTORY_SEPARATOR . $file);
+			   	$hrSize = $this->humanReadableFileSize($size);
+
+			   	$data[] = [
+				   	"filename" => $file,
+				   	"sortfield" => $year.$month.$day.$hour.$minute.$second,
+				   	"date" => $formattedDate,
+				   	"ID" => $GitHubID,
+				   	"size" => $hrSize,
+				   	"actions" => ""
+			   	];
+			}
 		}
+		$this->sendResponse(json_encode($data));
+	}
 
-        foreach ($files as $file) {
-            if (strpos($file, '.') !== 0) {
-
-                $fileBits = explode("-", $file);
-                $issue = $fileBits[1];
-                $date = explode(".", $fileBits[2])[0];
-                $year = substr($date, 0, 4);
-                $month = substr($date, 4, 2);
-                $day = substr($date, 6, 2);
-                $hour = substr($date, 8, 2);
-                $minute = substr($date, 10, 2);
-                $second = substr($date, 12, 2);
-
-                $timestamp = mktime($hour, $minute, $second, $month, $day, $year);
-                $formattedDate = strftime("%A %d %B %Y, %H:%M", $timestamp);
-
-                $size = filesize($this->issueDir . DIRECTORY_SEPARATOR . $file);
-                $hrSize = $this->humanReadableFileSize($size);
-
-                $data[] = [
-                    "filename" => $file,
-                    "sortfield" => $year.$month.$day.$hour.$minute.$second,
-                    "date" => $formattedDate,
-                    "issue" => $issue,
-                    "size" => $hrSize,
-                    "actions" => ""                    
-                ];
-            }
-        }
-        $this->sendResponse(json_encode($data));
-    }
-
-    public function getGenerateLog() {
-        $command = 'export ALLSKY_HOME=' . ALLSKY_HOME . '; export SUDO_OK="true"; ';
+	public function getGenerateLog() {
+		$command = 'export ALLSKY_HOME=' . ALLSKY_HOME . '; export SUDO_OK="true"; ';
 		$command .= ALLSKY_HOME . '/support.sh --auto 2>&1';
 		exec($command, $result, $ret_code);
-        if ($ret_code === 0) {
-        	$this->sendResponse(json_encode(array("status"=>"ok")));
+		if ($ret_code === 0) {
+			$this->sendResponse(json_encode(array("status"=>"ok")));
 		} else {
 			$msg = "[$cmd] failed: " . implode(" ", $result);
-        	$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
+			$this->sendResponse(json_encode(array("status"=>"ERROR", "message"=>"$msg")));
 		}
-    }
+	}
 
 }
 

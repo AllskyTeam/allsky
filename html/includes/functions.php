e8e2bd016ca033ad2164bd8a7d4c85971c089d9f
Alex <alex.greenland@gmail.com>
2025-07-07 21:27:07 +0100

    #3689 Bug fixes from _04 merge

--- html/includes/functions.php
+++ html/includes/functions.php
@@ -1190,6 +1190,28 @@ function getNewestAllskyVersion(&$changed=null)
 	return($version_array);
 }
 
+function getUptime() {
+	$uparray = explode(" ", exec("cat /proc/uptime"));
+	$seconds = round($uparray[0], 0);
+	$minutes = $seconds / 60;
+	$hours = $minutes / 60;
+	$days = floor($hours / 24);
+	$hours = floor($hours - ($days * 24));
+	$minutes = floor($minutes - ($days * 24 * 60) - ($hours * 60));
+	$uptime = '';
+	if ($days != 0) {
+		$uptime .= $days . ' day' . (($days > 1) ? 's ' : ' ');
+	}
+	if ($hours != 0) {
+		$uptime .= $hours . ' hour' . (($hours > 1) ? 's ' : ' ');
+	}
+	if ($minutes != 0) {
+		$uptime .= $minutes . ' minute' . (($minutes > 1) ? 's ' : ' ');
+	}
+
+	return $uptime;
+}
+
 function getCPULoad() 
 {
 	$secs = 2; $q = '"';

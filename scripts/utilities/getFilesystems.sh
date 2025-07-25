4a7c158d433db3abd2a99e5ed0bff51c33c1ca96
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-07-24 20:51:21 -0500

    Update getFilesystems.sh: Move --help message here

--- scripts/utilities/getFilesystems.sh
+++ scripts/utilities/getFilesystems.sh
@@ -12,7 +12,45 @@ source "${ALLSKY_HOME}/variables.sh"					|| exit "${EXIT_ERROR_STOP}"
 #shellcheck source-path=scripts
 source "${ALLSKY_SCRIPTS}/functions.sh"					|| exit "${EXIT_ERROR_STOP}"
 
+usage_and_exit()
+{
+	local RET=${1}
+	exec >&2
+	echo
+	local USAGE="Usage: ${ME} [--help] [--debug]"
+	if [[ ${RET} -ne 0 ]]; then
+		E_ "${USAGE}"
+	else
+		echo -e "${USAGE}"
+	fi
+
+	echo
+	echo "This command helps determine the path to a storage device like an SSD."
+	echo
+
+	exit "${RET}"
+}
+OK="true"
 DEBUG="false"
+DO_HELP="false"
+while [[ $# -gt 0 ]]; do
+	ARG="${1}"
+	case "${ARG,,}" in
+		--help)
+			DO_HELP="true"
+			;;
+		--debug)
+			DEBUG="true"
+			;;
+		-*)
+			E_ "Unknown argument '${ARG}'." >&2
+			OK="false"
+			;;
+	esac
+	shift
+done
+[[ ${DO_HELP} == "true" ]] && usage_and_exit 0
+[[ ${OK} == "false" ]] && usage_and_exit 1
 
 function outputData()
 {

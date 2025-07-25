6ff258f0f4da5b3157dd66475245785e512a6162
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-07-24 20:47:00 -0500

    Update checkPostData.sh: Move --help msg here

--- scripts/utilities/checkPostData.sh
+++ scripts/utilities/checkPostData.sh
@@ -13,6 +13,45 @@ source "${ALLSKY_HOME}/variables.sh"					|| exit "${EXIT_ERROR_STOP}"
 #shellcheck source-path=scripts
 source "${ALLSKY_SCRIPTS}/functions.sh"					|| exit "${EXIT_ERROR_STOP}"
 
+usage_and_exit()
+{
+	local RET=${1}
+	exec >&2
+	echo
+	local USAGE="Usage: ${ME} [--help]"
+	if [[ ${RET} -ne 0 ]]; then
+		E_ "${USAGE}"
+	else
+		echo -e "${USAGE}"
+	fi
+
+	echo
+	echo "This command helps determine why you get the"
+	echo "    data.json is X days old"
+	echo "message.  If possible, a solution is proposed."
+	echo
+
+	exit "${RET}"
+}
+
+OK="true"
+DO_HELP="false"
+while [[ $# -gt 0 ]]; do
+	ARG="${1}"
+	case "${ARG,,}" in
+		--help)
+			DO_HELP="true"
+			;;
+		-*)
+			E_ "Unknown argument '${ARG}'." >&2
+			OK="false"
+			;;
+	esac
+	shift
+done
+[[ ${DO_HELP} == "true" ]] && usage_and_exit 0
+[[ ${OK} == "false" ]] && usage_and_exit 1
+
 CMD="postData.sh"
 # Get all settings we're going to use.
 #shellcheck disable=SC2119

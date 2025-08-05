0a06b2bbdd0ef35210c39fc688412bef8d56f95d
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-08-05 09:39:37 -0500

    Update startrails_settings.php: Set defaults
    Also:
    Pass data to script.

--- html/helpers/startrails_settings.php
+++ html/helpers/startrails_settings.php
@@ -2,19 +2,13 @@
 
 function startrailsSettings() {
 
-// defaults
-$cmd = "AM_ALLSKY_CONFIG compare_startrails --html";
+// Defaults.  Ideally should match what's in compareStartrails.sh.
+$num_images = 20;
+$input_directory = "";
+$thresholds = "0.10  0.15  0.20  0.25  0.30  0.35  0.40  0.45  0.50";
+$verbose = "false";
 
-// TODO: on form submittal, use javascript to append to 'id' the form fields:
-//	--count c
-//	--thresholds t		(replacing spaces with "_")
-// then delete these lines:
-$count = 5;
-$input_directory = ALLSKY_IMAGES . "/from_allsky";
-$thresholds = "0.50   0.55";
-$cmd .= " --input from_allsky";
-$cmd .= " --num-images $count";
-$cmd .= " --thresholds " . str_replace(" ", "_", $thresholds);
+$cmd = "AM_ALLSKY_CONFIG compare_startrails --html";
 
 ?>
 <div class="row">
@@ -23,6 +17,19 @@ $cmd .= " --thresholds " . str_replace(" ", "_", $thresholds);
 			Help Determine <strong>Startrails Settings</strong>
 		</div>
 		<div class="panel-body">
+			<p>
+			This page helps you determine what startrails-related settings to use
+			to get the most number of images included in your startrails
+			and hence the longest trails possible.
+			<br>
+			Multiple startrails images will be created using the settings you enter
+			and you can then compare them to find the best image.
+			<p>
+			See the
+			<a external="true" allsky="true" href="/documentation/explanations/startrails.html#brightnessthreshold">startrails settings</a>
+			for an explanation of the startrails settings.
+			</p><br><br>
+
 			<form role="form" action="execute.php" method="POST" onsubmit="return add_args()">
 <script>
 function add_args() {
@@ -31,11 +38,14 @@ function add_args() {
 	input_directory = document.getElementById('input_directory').value;
 	if (input_directory != "") id += " --input " + input_directory;
 
-	count = document.getElementById('count').value;
-	if (count != "") id += " --num-images " + count;
+	num_images = document.getElementById('num_images').value;
+	if (num_images != "") id += " --num-images " + num_images;
 
 	thresholds = document.getElementById('thresholds').value;
 	if (thresholds != "") id += " --thresholds " + thresholds.replace(/ /g, "_");
+
+	verbose = document.getElementById('verbose').value;
+	if (verbose != "") id += " --verbose " + verbose;
 
 	document.getElementById('id').value = id;
 
@@ -47,25 +57,41 @@ function add_args() {
 				<input type="hidden" name="day" value="test_startrails">
 
 				<div class="row">
-					<div class="form-group col-md-4">
+					<div class="form-group col-md-8">
 						<label for="input_directory">Use images from</label>
+						<br>Enter the directory where the source images reside.
+						<br>This is typically a directory in '<?php echo ALLSKY_IMAGES ?>'.
+						<br>If you don't enter anything then yesterday's images are used.
 						<input type="text" class="form-control" name="input_directory"
 							id="input_directory" value="<?php echo $input_directory; ?>"/>
 					</div>
 				</div>
 				<div class="row">
-					<div class="form-group col-md-4">
-						<label for="count">Number of images to include in each startrail</label>
-						<input type="text" class="form-control" name="count"
-							id="count" value="<?php echo $count; ?>"/>
+					<div class="form-group col-md-8">
+						<label for="num_images">Number of images to include in each startrail</label>
+						<br>More images take more time to process but produce longer trailed stars.
+						<input type="text" class="form-control" name="num_images"
+							id="num_images" value="<?php echo $num_images; ?>"/>
 					</div>
 				</div>
 				<div class="row">
-					<div class="form-group col-md-4">
-						<label for="thresholds">Thresholds to use</label>
+					<div class="form-group col-md-8">
+						<label for="thresholds"><span class="WebUISetting">Brightness Threshold</span>s to use</label>
 						<br>Enter one or more space-separated Brightness Thresholds.
+						<br>The more values you have the longer it will take to process.
 						<input type="text" class="form-control" name="thresholds"
 							id="thresholds" value="<?php echo $thresholds; ?>"/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col-md-8">
+						<label for="verbose">Verbose Output?</label>
+						<br>Check the box to output summary information for each startrails created.
+						<br><input type="checkbox" class="form-control" name="verbose"
+							id="verbose" value="<?php echo $verbose; ?>"
+							<?php if ($verbose == "true") echo "checked"; ?>
+							style="width: 10%"
+							/>
 					</div>
 				</div>
 

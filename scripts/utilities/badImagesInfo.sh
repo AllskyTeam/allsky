64352607a5de8957abf5a9299a608d67f5b35dba
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-08-06 21:08:51 -0500

    Update badImagesInfo.sh: Be HTML aware

--- scripts/utilities/badImagesInfo.sh
+++ scripts/utilities/badImagesInfo.sh
@@ -22,7 +22,7 @@ usage_and_exit()
 	else
 		echo -e "${USAGE}"
 	fi
-	echo "Where:"
+	echo "Arguments:"
 	echo "   --help              Displays this message and exits."
 	echo "   --show_bad_images   Displays a list of 'bad' images that were removed."
 	echo
@@ -37,12 +37,16 @@ usage_and_exit()
 
 OK="true"
 DO_HELP="false"
+HTML="false"
 SHOW_BAD_IMAGES="0"
 while [[ $# -gt 0 ]]; do
 	ARG="${1}"
 	case "${ARG,,}" in
 		--help)
 			DO_HELP="true"
+			;;
+		--html)
+			HTML="true"
 			;;
 		--show_bad_images)
 			SHOW_BAD_IMAGES="1"
@@ -74,18 +78,33 @@ fi
 #	'image-20250209104345' MEAN of 0.167969 is below low threshold of 0.5
 #	$1                     $2   $3 $4       $5 $6    $7  $8        $9 $10
 
+if [[ ${HTML} == "true" ]]; then
+	echo -e "<div style='font-size: 125%'>\n";
+else
+	echo		# adds space at top to make it easier to read on a terminal
+fi
+
+# Pass in Bold ON, Bold Off, Highlight ON, Highlight Off
 echo "$INFO" | gawk -v showBadImages="${SHOW_BAD_IMAGES}" \
+		-v BR="${wBR}" -v BON="${wBOLD}" -v BOFF="${wNBOLD}" -v HLON="${wINFO}" -v HLOFF="${wNC}" \
 		-v singleQuote="'" -v settingName="${SETTING_NAME}" '
 	BEGIN {
 		low_count = 0; low_min = 1; low_max = 0; low_threshold = 0; low_mean_total = 0;
 		high_count = 0; high_min = 0; high_max = 0; high_threshold = 0; high_mean_total = 0;
+		low_settingName = BON settingName " Low" BOFF;
+		high_settingName = BON settingName " High" BOFF;
 	}
 	{
 		fileName = $1; gsub(singleQuote, "", fileName);
 		mean = $4;
 		below_above = $6;
 		threshold = $10;
-		if (showBadImages) printf("%s: mean of %f is %s threshold of %f.\n", fileName, mean, below_above, threshold);
+
+		if (showBadImages) {
+			printf("%s: mean of %0.4f is %s threshold of %0.4f.%s",
+				fileName, mean, below_above, threshold, BR);
+		}
+
 		if (below_above == "below") {	# below low
 			if (threshold != low_threshold) {
 				if (low_threshold != 0) {
@@ -120,39 +139,49 @@ echo "$INFO" | gawk -v showBadImages="${SHOW_BAD_IMAGES}" \
 	}
 	END {
 		if (low_count > 0) {
-			printf("\n");
-			printf("%d image", low_count);
-			if (low_count > 1) printf("s");
-			printf(" had a mean brightness below the \"%s Low\" setting of %f so were deleted.\n",
-				settingName, low_threshold);
+			printf("%s%d%s image%s", HLON, low_count, HLOFF, low_count > 1 ? "s" : "");
+			printf(" had a mean brightness below the %s setting of %0.4f so were deleted.%s",
+				low_settingName, low_threshold, BR);
+
 			if (low_min == low_max) {
-				printf("The lowest mean was %f.\n", low_min);
-				printf("FIX: Consider lowering your \"%s Low\" to less than %0.3f\n",
-					settingName, low_min);
-				printf("or increasing your exposure and/or gain.\n");
+				printf("The lowest mean was %0.4f.%s", low_min, BR);
+				printf("%sFIX: Consider lowering your %s to less than %0.4f%s",
+					HLON, low_settingName, low_min, HLOFF, BR);
+				printf("or increasing your exposure and/or gain.%s", BR);
+
 			} else {
 				ave = low_mean_total / low_count;
-				printf("The lowest mean was %f and the highest %f with an average of %f.\n",
-					low_min, low_max, ave);
-				printf("FIX: Consider lowering your \"%s Low\" to around %0.3f.\n", settingName, ave);
+				printf("The lowest mean was %0.4f and the highest %0.4f with an average of %0.4f.%s",
+					low_min, low_max, ave, BR);
+				printf("%sFIX: Consider lowering your %s to around %0.4f.%s%s",
+					HLON, low_settingName, ave, HLOFF, BR);
 			}
 		}
+
 		if (high_count > 0) {
-			printf("\n");
-			printf("%d image", high_count);
-			if (high_count > 1) printf("s");
-			printf(" had a mean brightness above the \"%s High\" of %f so were deleted.\n",
-				settingName, high_threshold);
+			if (low_count > 0) printf("%s", BR);	# Separator
+
+			printf("%s%d%s image%s", HLON, high_count, HLOFF, high_count > 1 ? "s" : "");
+			printf(" had a mean brightness above the %s%s High%s of %0.4f so were deleted.%s",
+				BON, settingName, BOFF, high_threshold, BR);
+
 			if (high_min == high_max) {
-				printf("The highest mean was %f.\n", high_min);
-				printf("FIX: Consider raising your \"%s High\" to more than %0.3f\n",
-					settingName, high_min);
-				printf("or decreasing your exposure and/or gain.\n");
+				printf("The highest mean was %0.4f.%s", high_min, BR);
+				printf("%sFIX: Consider raising your %s to more than %0.4f%s",
+					HLON, high_settingName high_min, HLOFF, BR);
+				printf("or decreasing your exposure and/or gain.%s", BR);
 			} else {
 				ave = high_mean_total / high_count;
-				printf("The lowest mean was %f and the highest %f with and average of %f.\n",
-					high_min, high_max, ave);
-				printf("FIX: Consider raising your \"%s High\" to around %0.3f.\n", settingName, ave);
+				printf("The lowest mean was %0.4f and the highest %0.4f with and average of %0.4f.%s",
+					high_min, high_max, ave, BR);
+				printf("%sFIX: Consider raising your %s to around %0.4f.%s%s",
+					HLON, high_settingName, ave, HLOFF, BR);
 			}
 		}
 	}'
+
+if [[ ${HTML} == "true" ]]; then
+	echo -e "</div>\n";
+else
+	echo		# adds space at bottom to make it easier to read on a terminal
+fi

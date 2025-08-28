38e9733bd4245d3c6e2064306400a510f1ed1ee8
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-08-28 14:43:46 -0500

    Update execute.php: Don't cache pages
    Since the output is generally created in real-time.

--- html/execute.php
+++ html/execute.php
@@ -42,6 +42,9 @@ if ($use_TEXT) {
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
 
 	<link href="documentation/css/custom.css" rel="stylesheet">
 	<link href="documentation/css/light.css" rel="stylesheet">

01d38e2e3a183fcd7089666c55a82df3d2748654
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-10-05 16:38:56 -0500

    Update darkCapture.sh: Convert temp to int

--- scripts/darkCapture.sh
+++ scripts/darkCapture.sh
@@ -17,13 +17,16 @@ fi
 # The extension on ${CURRENT_IMAGE} may not be ${ALLSKY_EXTENSION}.
 DARK_EXTENSION="${CURRENT_IMAGE##*.}"
 
-mkdir -p "${ALLSKY_DARKS}"
+[[ ! -d ${ALLSKY_DARKS} ]] && mkdir -p "${ALLSKY_DARKS}"
+
 if [[ -z ${AS_TEMPERATURE_C} ]]; then
 	# The camera doesn't support temperature so we'll keep overwriting the file until
 	# AS_TEMPERATURE_C is set.
 	# This allows users to continually look for a new dark file and rename it manually.
 	MOVE_TO_FILE="${ALLSKY_DARKS}/$( basename "${CURRENT_IMAGE}" )"
 else
+	# If the temp is a float, round and convert to int.
+	AS_TEMPERATURE_C="$( echo ${AS_TEMPERATURE_C} | gawk '{ printf("%d", $1+0.5); }' )"
 	MOVE_TO_FILE="${ALLSKY_DARKS}/${AS_TEMPERATURE_C}.${DARK_EXTENSION}"
 fi
 mv "${CURRENT_IMAGE}" "${MOVE_TO_FILE}" || exit 3

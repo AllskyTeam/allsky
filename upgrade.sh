7a36b027bd0081d21f52a5a8bbe031cfdbfc6fc9
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2025-11-22 20:26:44 -0600

    Update upgrade.sh: minor tweaks

--- upgrade.sh
+++ upgrade.sh
@@ -86,7 +86,7 @@ check_for_oldest()
 
 	if [[ -d ${OLDEST_DIR} ]]; then
 		local MSG="Directory '${OLDEST_DIR}' already exist."
-		local MSG2="\n\nIf you want to upgrade to the newest release, either remove '${OLDEST_DIR}'"
+		local MSG2="\n\nIf you want to upgrade to the newest Allsky release, either remove '${OLDEST_DIR}'"
 		MSG2+=" or rename it to something else, then re-run this upgrade."
 		whiptail --title "${TITLE}" --msgbox "${MSG}${MSG2}" 25 "${WT_WIDTH}" 3>&1 1>&2 2>&3
 		display_msg --log info "${MSG}"
@@ -97,11 +97,11 @@ check_for_oldest()
 	mv "${PRIOR_ALLSKY_DIR}" "${OLDEST_DIR}"
 }
 
-
+# Currently not used...
 restore_directories()
 {
 	display_msg --log info "Renaming '${PRIOR_ALLSKY_DIR}' back to '${ALLSKY_HOME}'."
-	echo mv "${PRIOR_ALLSKY_DIR}" "${ALLSKY_HOME}"
+	mv "${PRIOR_ALLSKY_DIR}" "${ALLSKY_HOME}"
 	if [[ -d ${OLDEST_DIR} ]]; then
 		display_msg --log info "Renaming '${OLDEST_DIR}' back to '${PRIOR_ALLSKY_DIR}'."
 		mv "${OLDEST_DIR}" "${PRIOR_ALLSKY_DIR}"
@@ -109,7 +109,6 @@ restore_directories()
 }
 
 
-#
 function usage_and_exit()
 {
 	local RET=${1}
@@ -118,7 +117,7 @@ function usage_and_exit()
 	echo
 	local USAGE="Usage: ${ME} [--help] [--debug] [--branch branch]"
 	if [[ ${RET} -eq 0 ]]; then
-		echo "Upgrade the Allsky software to a newer version."
+		echo "Upgrade the Allsky software to the newest version."
 		echo -e "\n${USAGE}"
 	else
 		E_ "${USAGE}"
@@ -141,7 +140,7 @@ OK="true"
 HELP="false"
 DEBUG="false"; DEBUG_ARG=""
 # shellcheck disable=SC2119
-BRANCH="$( get_branch )"
+BRANCH="$( get_branch "" )"
 [[ -z ${BRANCH} ]] && BRANCH="${GITHUB_MAIN_BRANCH}"
 while [[ $# -gt 0 ]]; do
 	ARG="${1}"
@@ -229,7 +228,7 @@ if ! ERR="$( git clone --depth=1 --recursive --branch "${BRANCH}" "${R}" 2>&1 )"
 fi
 
 cd "${ALLSKY_HOME}" || exit "${EXIT_ERROR_STOP}"
-#
+
 # --doUpgrade tells it to use prior version without asking and to not display header,
 # change messages to say "upgrade", not "install", etc.
 # shellcheck disable=SC2086,SC2291

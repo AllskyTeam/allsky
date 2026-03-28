6b2a24632c1514fa3126e3175f89fa31631c854f
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2026-03-28 17:36:13 -0500

    Update logutil.php: Add /var/log/allsky.log.1

--- html/includes/logutil.php
+++ html/includes/logutil.php
@@ -40,6 +40,12 @@ class LOGUTIL extends UTILBASE
                     'default' => true,
                 ],
                 [
+                    'id' => 'allsky1',
+                    'description' => 'Allsky Log (prior copy)',
+                    'location' => '/var/log/allsky.log.1',
+                    'default' => false,
+                ],
+                [
                     'id' => 'allskyperiodic',
                     'description' => 'Allsky Periodic Log',
                     'location' => '/var/log/allskyperiodic.log',

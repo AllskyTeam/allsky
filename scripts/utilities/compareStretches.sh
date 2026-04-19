cadf7341e019297a211a53ce36273dc4b1298df8
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2026-04-19 12:16:55 -0500

    Update compareStretches.sh: Use external='true'

--- scripts/utilities/compareStretches.sh
+++ scripts/utilities/compareStretches.sh
@@ -4,11 +4,11 @@
 ME="$( basename "${BASH_ARGV0}" )"
 
 #shellcheck source-path=.
-source "${ALLSKY_HOME}/variables.sh"					|| exit "${EXIT_ERROR_STOP}"
+source "${ALLSKY_HOME}/variables.sh"					|| exit "${ALLSKY_EXIT_ERROR_STOP}"
 #shellcheck source-path=scripts
-source "${ALLSKY_SCRIPTS}/functions.sh"					|| exit "${EXIT_ERROR_STOP}"
+source "${ALLSKY_SCRIPTS}/functions.sh"					|| exit "${ALLSKY_EXIT_ERROR_STOP}"
 #shellcheck source-path=scripts
-source "${ALLSKY_SCRIPTS}/installUpgradeFunctions.sh"	|| exit "${EXIT_ERROR_STOP}"
+source "${ALLSKY_SCRIPTS}/installUpgradeFunctions.sh"	|| exit "${ALLSKY_EXIT_ERROR_STOP}"
 
 usage_and_exit()
 {
@@ -283,7 +283,7 @@ if [[ ${NUM_CREATED} -gt 0 ]]; then
 		DAY="$( basename "${OUT_DIRECTORY}" )"
 		echo -n "Click <a href='/helpers/show_images.php?_ts=${RANDOM}"
 		echo -n "&day=${DAY}&pre=stretch_&type=Test Stretch"
-		echo    "'>here</a> to see the results."
+		echo    "' external='true'>here</a> to see the results."
 	else
 		echo -e "\nThe ${NUM_CREATED} stretched image(s) are in '${OUT_DIRECTORY}'.\n"
 	fi

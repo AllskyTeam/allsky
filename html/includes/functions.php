17fe177926a2361a755b53bef67a13c307866165
Alex <alex.greenland@gmail.com>
2026-04-22 22:58:34 +0100

    Pollyfills for php7

--- html/includes/functions.php
+++ html/includes/functions.php
@@ -56,6 +56,44 @@ if ($variablesJsonOk === false) {
 	die(1);
 }
 
+if (!function_exists('str_contains')) {
+	function str_contains(string $haystack, string $needle): bool
+	{
+		if ($needle === '') {
+			return true;
+		}
+
+		return strpos($haystack, $needle) !== false;
+	}
+}
+
+if (!function_exists('str_starts_with')) {
+	function str_starts_with(string $haystack, string $needle): bool
+	{
+		if ($needle === '') {
+			return true;
+		}
+
+		return strncmp($haystack, $needle, strlen($needle)) === 0;
+	}
+}
+
+if (!function_exists('str_ends_with')) {
+	function str_ends_with(string $haystack, string $needle): bool
+	{
+		if ($needle === '') {
+			return true;
+		}
+
+		$needleLength = strlen($needle);
+		if ($needleLength > strlen($haystack)) {
+			return false;
+		}
+
+		return substr($haystack, -$needleLength) === $needle;
+	}
+}
+
 // Read and decode a json file, returning the decoded results or null.
 // On error, display the specified error message.
 // If we're being run by the user it's likely on a tty so don't use html.

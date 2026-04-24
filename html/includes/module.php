9e4e25bccaff13ce82ad0ed12327a8b31f689e18
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2026-04-24 13:41:19 -0500

    Update module.php: Use doHelpLink()

--- html/includes/module.php
+++ html/includes/module.php
@@ -84,11 +84,7 @@ function DisplayModule()
 <div class="panel panel-allsky">
 	<div class="panel-heading clearfix">
         <span><i class="<?php echo $pageIcon ?>"></i> <?php echo $pageHeaderTitle ?></span>
-<?php if (!empty($pageHelp)) { ?>
-        <a class="pull-right" href="<?php echo $pageHelp; ?>" target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-container="body" data-placement="left" title="Help">
-            <i class="fa-solid fa-circle-question"></i> Help
-        </a>
-<?php } ?>
+		<?php if (!empty($pageHelp)) { doHelpLink($pageHelp); } ?>
     </div>
     <div class="panel-body">
         <nav class="navbar navbar-default">

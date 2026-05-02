453a3967069cbdd8089414c6c17f76e99ecbb9ef
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2026-05-01 21:59:17 -0500

    Update support.sh: shellcheck fix

--- support.sh
+++ support.sh
@@ -409,6 +409,7 @@ function generate_support_info()
 	{
 		local ZIP_FILE="${1}"
 		local SOURCE="${2}"
+		# shellcheck disable=SC2086
 		zip -r "${ZIP_FILE}" ${SOURCE} > /dev/null 2>&1
 		get_mb "${ZIP_FILE}"
 		rm -f "${ZIP_FILE}"

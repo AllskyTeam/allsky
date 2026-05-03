c6e87afddcbfe7e2311952063c94775001b4d6c1
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2026-05-03 16:43:09 -0500

    Update upgrade.sh: Fix typo in variable name

--- upgrade.sh
+++ upgrade.sh
@@ -237,14 +237,14 @@ IN_PLACE="true"		# TODO: XXXXXXXXXXXXXXXX prompt for it
 		display_msg --log progress "Stopping Allsky"
 		sudo systemctl stop allsky
 
-		cd "${ALLSKY_HOME}"	|| exit "${ALLSKY_EXIT_STOP}"
+		cd "${ALLSKY_HOME}"	|| exit "${ALLSKY_EXIT_ERROR_STOP}"
 
 		display_msg --log progress "Getting new files from GitHub"
 		X="$( git pull 2>&1 )"
 		if [[ $? -ne 0 ]]; then
 			MSG="Unable to get new files: ${X}"
 			display_msg --log error "${MSG}" "Contact the Allsky Team"
-			exit "${ALLSKY_EXIT_STOP}"
+			exit "${ALLSKY_EXIT_ERROR_STOP}"
 		fi
 
 		# This script may have been updated so re-run it.
@@ -252,7 +252,7 @@ IN_PLACE="true"		# TODO: XXXXXXXXXXXXXXXX prompt for it
 		exec "${ALLSKY_HOME}/${ME}" --doUpgrade --in-place		# should not return
 
 		display_msg --log error "Unable to continue the upgrade."
-		exit "${ALLSKY_EXIT_STOP}"
+		exit "${ALLSKY_EXIT_ERROR_STOP}"
 
 	else		# move ${ALLSKY_HOME}
 		#	Check for prior Allsky versions:

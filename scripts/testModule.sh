76ae7042fc88f9c2c2000c74b20d45f7b3c50061
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2026-05-03 14:48:27 -0500

    Update testModule.sh: Prepend ALLSKY_

--- scripts/testModule.sh
+++ scripts/testModule.sh
@@ -6,9 +6,9 @@
 ME="$( basename "${BASH_ARGV0}" )"
 
 #shellcheck disable=SC1091 source-path=.
-source "${ALLSKY_HOME}/variables.sh"		|| exit "${EXIT_ERROR_STOP}"
+source "${ALLSKY_HOME}/variables.sh"		|| exit "${ALLSKY_EXIT_ERROR_STOP}"
 #shellcheck source-path=scripts
-source "${ALLSKY_SCRIPTS}/functions.sh"		|| exit "${EXIT_ERROR_STOP}"
+source "${ALLSKY_SCRIPTS}/functions.sh"		|| exit "${ALLSKY_EXIT_ERROR_STOP}"
 
 export CURRENT_IMAGE=""
 export DAY_OR_NIGHT="${1}"

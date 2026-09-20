005e7d9d02cc95e7609d4e7451482aade67202c8
Carlos G <ea1ii@hotmail.com>
2026-09-20 23:53:30 +0100

    fixed action rows

--- html/includes/days.php
+++ html/includes/days.php
@@ -83,8 +83,8 @@ function ListDays()
 				</div>
 				<div class="col-sm-8 days-actions-col">
 					<div class="row days-grid-actions">
-						<div class="col-xs-3 text-center days-action-col"><span class="days-grid-label">Images</span></div>
-						<div class="col-xs-3 text-center days-action-col"><span class="days-grid-label">Timelapse</span></div>
+						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-label">Images</span></div>
+						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-label">Timelapse</span></div>
 						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-label">Keogram</span></div>
 						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-label">Startrails</span></div>
 						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-label">Meteors</span></div>
@@ -101,8 +101,8 @@ function ListDays()
 				<div class="col-sm-2 hidden-xs hidden-sm"><span class="days-grid-value">-</span></div>
 				<div class="col-sm-8 days-actions-col">
 					<div class="row days-grid-actions">
-						<div class="col-xs-3 text-center days-action-col"><span class="days-grid-value"><span title="There are too many total images to view on one page.">-</span></span></div>
-						<div class="col-xs-3 text-center days-action-col"><span class="days-grid-value"><?php insertHref("list_videos", "All"); ?></span></div>
+						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-value"><span title="There are too many total images to view on one page.">-</span></span></div>
+						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-value"><?php insertHref("list_videos", "All"); ?></span></div>
 						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-value"><?php insertHref("list_keograms", "All"); ?></span></div>
 						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-value"><?php insertHref("list_startrails", "All"); ?></span></div>
 						<div class="col-xs-2 text-center days-action-col"><span class="days-grid-value"><span title="View meteors one day at a time.">-</span></span></div>
@@ -218,8 +218,8 @@ foreach ($days as $day) {
 	echo "    <div class='col-sm-2 hidden-xs hidden-sm'><span class='days-grid-value'>" . htmlspecialchars($displayDate) . "</span></div>\n";
 	echo "    <div class='col-sm-8 days-actions-col'>\n";
 	echo "      <div class='row days-grid-actions'>\n";
-	echo "        <div class='col-xs-3 text-center days-action-col'><span class='days-grid-value'>$imagesHtml</span></div>\n";
-	echo "        <div class='col-xs-3 text-center days-action-col'><span class='days-grid-value'>$timelapseHtml</span></div>\n";
+	echo "        <div class='col-xs-2 text-center days-action-col'><span class='days-grid-value'>$imagesHtml</span></div>\n";
+	echo "        <div class='col-xs-2 text-center days-action-col'><span class='days-grid-value'>$timelapseHtml</span></div>\n";
 	echo "        <div class='col-xs-2 text-center days-action-col'><span class='days-grid-value'>$keogramHtml</span></div>\n";
 	echo "        <div class='col-xs-2 text-center days-action-col'><span class='days-grid-value'>$startrailsHtml</span></div>\n";
 	echo "        <div class='col-xs-2 text-center days-action-col'><span class='days-grid-value'>$meteorsHtml</span></div>\n";
